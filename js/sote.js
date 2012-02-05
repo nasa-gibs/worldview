b29cd4257ea2d70610aa769e6da124a698c975f7
Ryan Boller <ryan.a.boller@nasa.gov>
2012-02-05 17:44:11 +0000

    Forcing a white background for IE warning message

--- js/sote.js
+++ js/sote.js
@@ -3,7 +3,7 @@ window.onload = function(){// Initialize "static" vars
 	// Test for IE and show warning, if necessary
 	if (/MSIE (\d+\.\d+);/.test(navigator.userAgent))
 	{
-		var ieWarningOverlay = new YAHOO.widget.Panel("iewarning", { zIndex:1020, visible:false } );
+		var ieWarningOverlay = new YAHOO.widget.Panel("iewarning", { zIndex:1020, visible:false, underlay:"matte" } );
 		var msg = "<div>" + 
 			"<h4>Internet Explorer is not currently supported</h4>"+
 			"<br /><p>Worldview uses a set of browser technologies that are not currently supported by Internet Explorer.  " +
