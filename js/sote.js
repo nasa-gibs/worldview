cfe3399d17fb95c549e4c330c99d22eef59b0c12
Ryan Boller <ryan.a.boller@nasa.gov>
2012-12-27 00:38:38 +0000

    updated default polar coastline layers to use the locally-cached versions

--- js/sote.js
+++ js/sote.js
@@ -26,7 +26,7 @@ window.onload = function(){// Initialize "static" vars
 	// Create map 
 	var m = new SOTE.widget.MapSote("map",{baseLayer:"MODIS_Terra_CorrectedReflectance_TrueColor"});
 	var ss = new SOTE.widget.Switch("switch",{dataSourceUrl:"a",selected:"geographic"});
-	var a = new SOTE.widget.Bank("products",{dataSourceUrl:"ap_products.php",title:"My Layers",selected:{antarctic:"baselayers.MODIS_Terra_CorrectedReflectance_TrueColor~overlays.polarview:coastS10", arctic:"baselayers.MODIS_Terra_CorrectedReflectance_TrueColor~overlays.polarview:coastArctic10",geographic:"baselayers.MODIS_Terra_CorrectedReflectance_TrueColor~overlays.sedac_bound"},categories:["Base Layers","Overlays"],callback:this.showSelector,selector:this.selector});
+	var a = new SOTE.widget.Bank("products",{dataSourceUrl:"ap_products.php",title:"My Layers",selected:{antarctic:"baselayers.MODIS_Terra_CorrectedReflectance_TrueColor~overlays.antarctic_coastlines", arctic:"baselayers.MODIS_Terra_CorrectedReflectance_TrueColor~overlays.arctic_coastlines",geographic:"baselayers.MODIS_Terra_CorrectedReflectance_TrueColor~overlays.sedac_bound"},categories:["Base Layers","Overlays"],callback:this.showSelector,selector:this.selector});
 	var s = new SOTE.widget.Selector("selectorbox",{dataSourceUrl:"ap_products.php",categories:["Base Layers","Overlays"]});
 	//var h = new SOTE.widget.MenuPicker("hazard",{dataSourceUrl:"data/mp_hazard.php"});
 	//var tr = new SOTE.widget.MenuPicker("transition",{dataSourceUrl:"data/mp_transition.php"});
