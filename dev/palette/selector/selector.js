3c25ed469abcfa45ccdf1145779a2bda63b891af
Mike McGann <mike.mcgann@nasa.gov>
2013-02-07 10:37:55 -0500

    Initial work on the palette selector.

---
+++ dev/palette/selector/selector.js
@@ -0,0 +1,9 @@
+$(function() {
+   
+   var ns = Worldview.Visual;
+   
+   ns.STOCK_PALETTE_ENDPOINT = "../../../data/palettes";
+   
+   ns.StockPaletteSelector({selector: "#palettes"});
+    
+});
