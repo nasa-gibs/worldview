3738714d5c17bd04faf8e74a783cf59515f80aac
Mike McGann <mike.mcgann@nasa.gov>
2013-04-10 10:29:30 -0400

    Fixed regression where code was not assuming that invalid dates throw an exception

--- js/Worldview/Widget/WorldviewMap.js
+++ js/Worldview/Widget/WorldviewMap.js
@@ -94,10 +94,12 @@ Worldview.Widget.WorldviewMap = function(containerId, config) {
                 if ( state.time === undefined ) {
                     state.time = today.toISOStringDate();
                 }
-                var date = Date.parseISOString(state.time);
-                if ( isNaN(date.getTime()) ) {
-                    log.warn("Invalid time: " + state.time + 
-                            ", using today: " + today.toISOStringDate());
+                var date;
+                try {
+                    date = Date.parseISOString(state.time);
+                } catch ( error ) {
+                    log.warn("Invalid time: " + state.time + ", reason: " + 
+                            error + "; using today: " + today.toISOStringDate());
                     state.time = today.toISOStringDate();
                     date = today;                   
                 }
