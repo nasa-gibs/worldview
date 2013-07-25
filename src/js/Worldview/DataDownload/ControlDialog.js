bbdbe532d637ecc5ef3b84779118c1c649ade0be
Mike McGann <mike.mcgann@nasa.gov>
2013-07-25 11:03:43 -0400

    Fixed some merge conflicts that seem to be related to the restructure

---
+++ src/js/Worldview/DataDownload/ControlDialog.js
@@ -0,0 +1,61 @@
+/*
+ * NASA Worldview
+ * 
+ * This code was originally developed at NASA/Goddard Space Flight Center for
+ * the Earth Science Data and Information System (ESDIS) project. 
+ *
+ * Copyright (C) 2013 United States Government as represented by the 
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
+Worldview.namespace("DataDownload");
+
+Worldview.DataDownload.ControlDialog = function(model) {
+    
+    var WIDTH = 345;
+    var HEIGHT = 265;
+    var X_OFFSET = 50;
+    var Y_OFFSET = 75;
+    var dialog;
+    
+    var self = {};
+    self.events = Worldview.Events();
+    
+    var init = function() {
+        var properties = {
+            width: "" + WIDTH + "px",
+            height: "" + HEIGHT + "px",
+            visible: false,
+            autofillheight: "body",
+            contraintoviewport: true,
+            x: Math.ceil($(window).width() - WIDTH - X_OFFSET),
+            y: Y_OFFSET
+        };
+        dialog = new YAHOO.widget.Panel("dataDownload_controlDialog", 
+                properties);
+        dialog.setHeader("Select Download Products");
+        dialog.setBody([
+            "<form>",
+                "<div class='productSelector'></div>",
+            "</form>"
+        ].join("\n"));
+        
+        dialog.hideEvent.subscribe(function() {
+            setTimeout(function() {
+                dialog.destroy();
+                self.events.fire("close");    
+            }, 5)
+        });
+        
+        dialog.render(document.body);
+        
+        Worldview.DataDownload.ProductSelector(model,
+                "#dataDownload_controlDialog .productSelector");
+                
+        dialog.show();        
+    }
+    
+    init();
+    
+    return self;
+}
---
+++ src/js/Worldview/DataDownload/ProductSelector.js
@@ -0,0 +1,52 @@
+/*
+ * NASA Worldview
+ * 
+ * This code was originally developed at NASA/Goddard Space Flight Center for
+ * the Earth Science Data and Information System (ESDIS) project. 
+ *
+ * Copyright (C) 2013 United States Government as represented by the 
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
+Worldview.namespace("DataDownload");
+
+Worldview.DataDownload.ProductSelector = function(model, selector) {
+    
+    var self = {};
+    
+    var init = function() {
+        $(selector).html("<select size='1'></select>");
+        $(selector + " select").change(function() {
+            onSelect();
+        });
+        
+        model.events.on("layerUpdate", onLayerUpdate)
+        model.events.on("layerSelect", onSelectUpdate);
+        
+        onLayerUpdate();
+    };    
+
+    var onLayerUpdate = function() {
+        console.log(model.layers);
+        var options = [];
+        $.each(model.layers, function(index, layer) {
+            var option = "<option value='" + layer.id + "'>" + layer.name + 
+                "; " + layer.description + "</option>";
+            options.push(option);
+        })
+        $(selector + " select").html(options.join("\n"));   
+    };
+    
+    var onSelect = function() {
+        var layer = $(selector + " select option:selected").val();
+        model.selectLayer(layer);
+    };
+    
+    var onSelectUpdate = function(selectedLayer) {
+        $(selector + " select").val(selectedLayer)        
+    };
+    
+    init();
+    return self;
+
+}
