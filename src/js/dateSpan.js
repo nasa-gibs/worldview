3408989cd17b79c96b1cbf391cb8b253028690da
Mike McGann <mike.mcgann@nasa.gov>
2013-10-08 06:41:28 -0400

    Removed console.log

--- src/js/dateSpan.js
+++ src/js/dateSpan.js
@@ -35,7 +35,6 @@ SOTE.widget.DateSpan = function(containerId, config){
 	for ( var year = 2000; year <= Worldview.now().getUTCFullYear(); year++ ) {
 	    this.sliders["Year"].push(year);
 	}
-	console.log(this.sliders["Year"]);
 	this.sliders["Month"] = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 	this.sliders["Day"] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
  	this.months = [31,28,31,30,31,30,31,31,30,31,30,31];
