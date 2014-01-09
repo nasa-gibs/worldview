95727636b9aabe7b309fe1b136dc9e11d4bbba7d
Mike McGann <mike.mcgann@nasa.gov>
2014-01-09 09:35:17 -0500

    Lint recommendations

--- src/js/util/wv.util.browser.js
+++ src/js/util/wv.util.browser.js
@@ -359,13 +359,13 @@ if (!String.prototype.endsWith) {
  * Mobile device quirks.
  */
 (function() {
-    if( navigator.userAgent.match(/Android/i)
-     || navigator.userAgent.match(/webOS/i)
-     || navigator.userAgent.match(/iPhone/i)
-     || navigator.userAgent.match(/iPad/i)
-     || navigator.userAgent.match(/iPod/i)
-     || navigator.userAgent.match(/BlackBerry/i)
-     || navigator.userAgent.match(/Windows Phone/i)
+    if( navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
      ){
         mobile = true;
         if (!(window.orientation == 90 || window.orientation == -90)){
