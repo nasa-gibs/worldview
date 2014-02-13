ed07a92756b03c6c3cac45a254ec50e6a3318fc1
Mike McGann <mike.mcgann@nasa.gov>
2014-02-13 14:33:28 -0500

    Added test for IE10

--- test/util/wv.util.browser-test.js
+++ test/util/wv.util.browser-test.js
@@ -42,6 +42,13 @@ buster.testCase("wv.util.browser", {
         buster.assert(wv.util.browser.tests.ie());
     },
 
+    "Is Internet Explorer 10": function() {
+        this.stub(wv.util.browser.tests, "navigator").returns({
+            userAgent: "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C)"
+        });
+        buster.assert(wv.util.browser.tests.ie());
+    },
+
     "Is not Internet Explorer": function() {
         this.stub(wv.util.browser.tests, "navigator").returns({
             userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:26.0) Gecko/20100101 Firefox/26.0"
