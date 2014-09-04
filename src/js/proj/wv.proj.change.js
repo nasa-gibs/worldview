74084d70dd322e561c0ed9180b57ee586e97e050
Mike McGann <mike.mcgann@nasa.gov>
2014-09-04 09:45:37 -0400

    Updated projection change text

--- src/js/proj/wv.proj.change.js
+++ src/js/proj/wv.proj.change.js
@@ -90,12 +90,15 @@ wv.proj.change = wv.proj.change || function(models) {
         notified = true;
         var message = [
             "On " + wv.util.toISOStringDate(PROJECTION_CHANGE_DATE) +
-            " the polar projections changed as follows:" ,
+            " the polar projections for some layers changed as follows:" ,
             "<br/><br/>",
             "The <b>Arctic projection</b> changed from Arctic Polar ",
             "Stereographic (EPSG:3995, \"Greenwich down\") to NSIDC Polar ",
-            "Stereographic North (EPSG:3413, \"Greenland down\").",
+            "Stereographic North (EPSG:3413, \"Greenland down\"). ",
+            "Coastlines and Graticule in the older projection ",
+            "can be found in the Add Layer tab by searching for \"EPSG:3995\". ",
             "<br/><br/>" +
+
             "The <b>Antarctic projection</b> changed from being projected onto ",
             "a sphere with radius of 6371007.181 meters to being projected ",
             "onto the WGS84 ellipsoid. The projection is now the correct ",
@@ -105,9 +108,7 @@ wv.proj.change = wv.proj.change || function(models) {
             "<br/><br/>",
 
             "Imagery before this date has not yet been reprocessed to the ",
-            "new projection. Coastlines and Graticule in the older projection ",
-            "can be found in the Add Layer tab by searching for \"EPSG:3995\". ",
-            "In addition, the \"Population Density\" ",
+            "new projection. In addition, the \"Population Density\" ",
             "layer can no longer be displayed properly ",
             "in the older projection.",
             "<br/><br/>",
