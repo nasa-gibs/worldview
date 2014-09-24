985758ae9fb071f59ac22b257f260221b7d6d9b4
Ryan Boller <ryan.a.boller@nasa.gov>
2014-09-24 13:32:44 -0400

    Updated URL path to reflect new build structure

--- web/js/data/wv.data.echo.js
+++ web/js/data/wv.data.echo.js
@@ -24,7 +24,7 @@ wv.data.echo.client = wv.data.echo.client || function(spec) {
     var ns = wv.data.echo.client;
 
     var ajaxOptions = {
-        url: wv.brand.url("service/echo.cgi"),
+        url: wv.brand.url("service/data/echo.cgi"),
         traditional: true,
         dataType: "json",
         timeout: QUERY_TIMEOUT
