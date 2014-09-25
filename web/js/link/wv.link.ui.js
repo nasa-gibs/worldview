f42bcd4baa660f8d9e86e9623fecc08da031e957
Mike McGann <mike.mcgann@nasa.gov>
2014-09-25 15:30:49 -0400

    WV-915: Auto url permalink back in, please report problems promptly

--- web/js/link/wv.link.ui.js
+++ web/js/link/wv.link.ui.js
@@ -48,15 +48,12 @@ wv.link.ui = wv.link.ui || function(models, config) {
         models.link.events.on("update", replaceHistoryState);
     };
 
-    var replaceHistoryState = function() {};
-    /*
     var replaceHistoryState = _.throttle(function() {
         if ( wv.util.browser.history ) {
             window.history.replaceState("", "@OFFICIAL_NAME@",
                     "?" + models.link.toQueryString());
         }
     }, 250, {trailing: true});
-    */
 
     self.show = function() {
         var $dialog = wv.ui.getDialog();
