59b0ad184f0513ccc2d27db8eb322e1693c00ebe
Mike McGann <mike.mcgann@nasa.gov>
2014-11-04 14:37:36 -0500

    Layer lists refresh when expanded, default font now sans-serif

--- web/js/layers/wv.layers.add.js
+++ web/js/layers/wv.layers.add.js
@@ -41,6 +41,7 @@ wv.layers.add = wv.layers.add || function(models, ui, config) {
             .on("add", onLayerAdded)
             .on("remove", onLayerRemoved);
         models.proj.events.on("select", onProjectionChange);
+        models.wv.events.on("sidebar-expand", resize);
         ui.sidebar.events.on("select", function(tab) {
             if ( tab === "add" ) {
                 resize();
