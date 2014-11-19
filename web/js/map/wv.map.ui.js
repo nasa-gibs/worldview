42d1069f0294417b01f81a064d25dc77df2c134d
Mike McGann <mike.mcgann@nasa.gov>
2014-11-19 10:01:46 -0500

    WV-1124: Checking for active palette when creating a new layer now done via isActive()

--- web/js/map/wv.map.ui.js
+++ web/js/map/wv.map.ui.js
@@ -299,14 +299,17 @@ wv.map.ui = wv.map.ui || function(models, config) {
     };
 
     var updatePalette = function(layerId) {
+        console.log("updatePalette", layerId);
         var def = config.layers[layerId];
         var key = layerKey(def);
         var mapLayer = _.find(self.selected.layers, { key: key });
         var palette = models.palettes.get(layerId);
         if ( !mapLayer ) {
+            console.log("no map layer");
             updateLayer(def);
             //updateMap();
         } else if ( palette.lookup ) {
+            console.log("still has lookup")
             mapLayer.lookupTable = palette.lookup;
             _.each(mapLayer.grid, function(row) {
                 _.each(row, function(tile) {
@@ -397,7 +400,7 @@ wv.map.ui = wv.map.ui || function(models, config) {
             tileSize: new OpenLayers.Size(matrixSet.tileSize[0],
                                           matrixSet.tileSize[1])
         };
-        if ( models.palettes.active[def.id] ) {
+        if ( models.palettes.isActive(def.id) ) {
             param.tileClass = wv.map.palette.canvasTile;
             param.lookupTable = models.palettes.active[def.id].lookup;
         }
