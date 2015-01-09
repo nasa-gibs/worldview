2014eee05101f5ea5b5b0bddfe3a87add6da6512
Mike McGann <mike.mcgann@nasa.gov>
2015-01-09 11:07:23 -0500

    GC documents now only fetch upon request

--- Gruntfile.js
+++ Gruntfile.js
@@ -256,6 +256,10 @@ module.exports = function(grunt) {
                 command: "find build -type d -empty -delete"
             },
 
+            fetch: {
+                command: "PATH=python/bin:${PATH} FETCH_GC=1 bin/wv-options-build"
+            },
+
             rpmbuild: {
                 command: 'rpmbuild --define "_topdir $PWD/build/rpmbuild" ' +
                              '-ba build/rpmbuild/SPECS/worldview.spec'
@@ -591,6 +595,8 @@ module.exports = function(grunt) {
         "copy:dist_config_versioned"
     ]);
 
+    grunt.registerTask("fetch", ["exec:fetch"])
+    
     grunt.registerTask("site", [
         "load_branding",
         "remove:build_site",
