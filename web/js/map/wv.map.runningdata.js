94a544c51133cbff01daf161d9a8630eaa5c452a
Benjamin <benking4@gmail.com>
2016-07-14 12:32:37 -0400

    matched color value to position on palette

--- web/js/map/wv.map.runningdata.js
+++ web/js/map/wv.map.runningdata.js
@@ -23,7 +23,6 @@ wv.map.runningdata = wv.map.runningdata || function(models) {
                     if(paletteInfo) {
                         self.setLayerValue(palette.id, paletteInfo);
                     }
-
                     self.activeLayers.push(palette.id +'_palette');
                 };
               };
@@ -32,43 +31,37 @@ wv.map.runningdata = wv.map.runningdata || function(models) {
             self.updateRunners(self.LayersToRemove(self.oldLayers, self.activeLayers));
         }
     };
-    self.setLayerValue = function(id, data) {
-        var $paletteCase;
-        var $paletteWidth;
-        var labelWidth;
-        var percent;
-        var labelMargin;
-        var location;
-
-        percent = data.percent;
-        $paletteCase = self.getPalette(id).parent();
-        $paletteWidth = $paletteCase.width();
-        $paletteLabel = $paletteCase.find('.wv-running-label');
-        $paletteBar = $paletteCase.find('.wv-running-bar');
-        location = $paletteWidth * percent;
-
-        $paletteLabel.text(data.label);
-        labelWidth = $paletteLabel.width();
-
-        labelMargin = self.getLabelMarginLeft(labelWidth, $paletteWidth, location);
-
-        $paletteLabel.attr('style', 'left:' + Math.round(labelMargin) + 'px;');
-        $paletteBar.attr('style', 'left:' + Math.round(location) + 'px;');
-        $paletteCase.addClass('wv-running');
-    }
+    self.getDataLabel = function(scale, hex) {
+        for(var i = 0, len = scale.colors.length; i < len; i++)  {
+            if(scale.colors[i] === hex) {
+                return {label:scale.labels[i], len: len, index:i};
+            };
+        };
+        return undefined;
+    };
     self.getLabelMarginLeft = function(labelWidth, caseWidth, location) {
-
-      if(location + (labelWidth / 2) > caseWidth) {
-          return (caseWidth - labelWidth);
-      } else if (location - (labelWidth / 2) < 0) {
-          return 0;
-      } else {
-          return (location - (labelWidth / 2));
-      }
-
+        if(location + (labelWidth / 2) > caseWidth) {
+            return (caseWidth - labelWidth);
+        } else if (location - (labelWidth / 2) < 0) {
+            return 0;
+        } else {
+            return (location - (labelWidth / 2));
+        }
     }
     self.getPalette = function(id) {
         return $('#' + id + '_palette');
+    }
+    self.getPercent = function(len, index, caseWidth) {
+
+      var segmentWidth;
+      var location;
+        if(len < 250) {
+            segmentWidth = (caseWidth / (len));
+            location = ((segmentWidth * index) + (0.5 * segmentWidth));
+            return (location / caseWidth);
+        } else {
+          return (index / len);
+        }
     }
     self.LayersToRemove = function(oldArray, newArray) {
         return _.difference(old, newArray);
@@ -82,12 +75,36 @@ wv.map.runningdata = wv.map.runningdata || function(models) {
         var $paletteCase = $('#' + id).parent();
         $paletteCase.removeClass('wv-running');
     }
-    self.getDataLabel = function(scale, hex) {
-        for(var i = 0, len = scale.colors.length; i < len; i++)  {
-            if(scale.colors[i] === hex) {
-                return {label:scale.labels[i], percent: Number(i/len)};
-            }
-        };
-        return undefined;
-    };
+    self.setLayerValue = function(id, data) {
+        var $palette;
+        var $paletteCase;
+        var $paletteWidth;
+        var labelWidth;
+        var percent;
+        var labelMargin;
+        var location;
+        var margin;
+
+        
+        $palette = self.getPalette(id);
+        $paletteCase = $palette.parent();
+        $paletteWidth = $palette.width();
+        $paletteCaseWidth = $paletteCase.outerWidth();
+        $paletteLabel = $paletteCase.find('.wv-running-label');
+        $paletteBar = $paletteCase.find('.wv-running-bar');
+
+        percent = self.getPercent(data.len, data.index, $paletteWidth);
+        margin = (($paletteCaseWidth - $paletteWidth) / 2)
+        location = ($paletteWidth * percent + margin);
+
+
+        $paletteLabel.text(data.label);
+        labelWidth = $paletteLabel.width();
+
+        labelMargin = self.getLabelMarginLeft(labelWidth, $paletteWidth, location);
+
+        $paletteLabel.attr('style', 'left:' + Math.round(labelMargin) + 'px;');
+        $paletteBar.attr('style', 'left:' + Math.round(location) + 'px;');
+        $paletteCase.addClass('wv-running');
+    }
 };
