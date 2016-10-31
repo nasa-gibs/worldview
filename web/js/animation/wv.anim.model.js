0e406060152b9eb2f9abf27bded662ac21d88623
Benjamin <benjamin.a.king@nasa.gov>
2016-10-31 16:27:56 -0400

    adjust permalinks to not break when adjusted

--- web/js/animation/wv.anim.model.js
+++ web/js/animation/wv.anim.model.js
@@ -41,14 +41,32 @@ wv.anim.model = wv.anim.model || function(models, config) {
     self.load = function(state, errors) {
         //is running twice on page load?????
         //console.log('loading');
+
         self.rangeState = self.rangeState || {};
-        self.rangeState.playing = false;
-        if(state.a) {
-            self.rangeState.state = 'on';
-            attributes = state.a.attributes;
-            attributes.forEach(function(attr) {
-                self.rangeState[attr.id] = attr.value;
-            });
+        // self.rangeState.playing = false;
+        // if(state.a) {
+        //     self.rangeState.state = 'on';
+        //     attributes = state.a.attributes;
+        //     attributes.forEach(function(attr) {
+        //         self.rangeState[attr.id] = attr.value;
+        //     });
+        // }
+
+        if(state.ab) {
+            self.rangeState.state = state.ab;
+        }
+        if(state.as && state.ae) {
+
+            if(state.ae.length == 10 && state.as.length == 10) {
+                self.rangeState.startDate = state.as;
+                self.rangeState.endDate = state.ae;
+            }
+        }
+        if(state.av) {
+            self.rangeState.speed = state.av;
+        }
+        if(state.al) {
+            self.rangeState.loop = Boolean(state.al);
         }
     };
 
@@ -81,11 +99,11 @@ wv.anim.model = wv.anim.model || function(models, config) {
      */
 
     self.add = function(arra) {
-        var updatedState = _.clone(self.state);
-        arra.forEach(function(prop) {
-            updatedState[prop.id] = prop.value;
-        });
-        self.animationState = updatedState;
+        // var updatedState = _.clone(self.state);
+        // arra.forEach(function(prop) {
+        //     updatedState[prop.id] = prop.value;
+        // });
+        // self.animationState = updatedState;
     };
 
     /*
@@ -104,29 +122,35 @@ wv.anim.model = wv.anim.model || function(models, config) {
         var newState;
 
         rangeState = self.rangeState;
-        state.a = state.a || [];
-        newState = {id: rangeState.state};
-        newState.attributes = [];
-
-        newState.attributes.push(
-            {
-                id:'startDate',
-                value: rangeState.startDate
-            },
-            {
-                id:'endDate',
-                value: rangeState.endDate
-            },
-            {
-                id:'speed',
-                value: rangeState.speed
-            },
-            {
-                id:'loop',
-                value: rangeState.loop
-            }
-        );
-        state.a.push(newState);
+        // state.a = state.a || [];
+        // newState = {id: rangeState.state};
+        // newState.attributes = [];
+        if(rangeState.state) {
+            state.ab = rangeState.state;
+            state.as = rangeState.startDate;
+            state.ae = rangeState.endDate;
+            state.av = String(rangeState.speed);
+            state.al = String(rangeState.loop);
+        }
+        // newState.attributes.push(
+        //     {
+        //         id:'startDate',
+        //         value: rangeState.startDate
+        //     },
+        //     {
+        //         id:'endDate',
+        //         value: rangeState.endDate
+        //     },
+        //     {
+        //         id:'speed',
+        //         value: rangeState.speed
+        //     },
+        //     {
+        //         id:'loop',
+        //         value: rangeState.loop
+        //     }
+        // );
+        // state.a.push(newState);
     };
     return self;
 };
