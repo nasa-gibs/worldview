07057285be15c1f1061a6f20f7580732dafa03cc
Benjamin <benjamin.a.king@nasa.gov>
2017-03-21 11:12:17 -0400

    e2e testing updates

--- e2e/globals.js
+++ e2e/globals.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: 'https://worldview.sit.earthdata.nasa.gov/'
-}
+  //url: 'https://uat.gibs.earthdata.nasa.gov/worldview-dev/v1.4.0/release/'
+  url: 'http://127.0.0.1/~baking1/worldview/web/'
+};
