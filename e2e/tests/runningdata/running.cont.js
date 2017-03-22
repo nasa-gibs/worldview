92061d5540c940dcf9eeb45dbd722b7daae0234e
Benjamin <benjamin.a.king@nasa.gov>
2017-03-22 12:46:31 -0400

    added comment RD e2e test #195

--- e2e/tests/runningdata/running.cont.js
+++ e2e/tests/runningdata/running.cont.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'Running Data for normal continuous layer' : function (browser) {
+    'Running Data: Continuous layer' : function (browser) {
     	var height, width;
         browser
             .url(browser.globals.url + '?p=geographic&l=MODIS_Terra_Brightness_Temp_Band31_Day,Coastlines(hidden)&t=2015-05-25&z=2&v=-42.148380855752734,42.13121723408824,22.122734950093943,85.16225953076464')
@@ -12,7 +12,7 @@ module.exports = {
         
 	        browser
 	            .useCss()
-	            .moveToElement('.ol-viewport',  width - (width / 2)  ,  height - (height / 2))
+	            .moveToElement('.ol-viewport',  width - (width / 2)  ,  height - (height / 2)) // clicking the very middle point of page
 	            .mouseButtonDown(0)
 	            .pause(10);
 			browser.getText('.wv-running-label', function(result){
