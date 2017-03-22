55f5f49d0e26d41111424941041d557748dad2da
Benjamin <benjamin.a.king@nasa.gov>
2017-03-22 12:15:40 -0400

    removed browser resize from e2e RD test, took to long #195

--- e2e/tests/runningdata/running.cont.js
+++ e2e/tests/runningdata/running.cont.js
@@ -3,8 +3,7 @@ module.exports = {
     	var height, width;
         browser
             .url(browser.globals.url + '?p=geographic&l=MODIS_Terra_Brightness_Temp_Band31_Day,Coastlines(hidden)&t=2015-05-25&z=2&v=-42.148380855752734,42.13121723408824,22.122734950093943,85.16225953076464')
-            .pause(500)
-            .resizeWindow(800, 600);
+            .pause(500);
 
         browser.getElementSize("#wv-map-geographic .ol-viewport", function(result) {
 
@@ -29,7 +28,6 @@ module.exports = {
 		        	this.assert.equal(result.value, '260.9 K');
 		        });
 		    });
-
 		});
 		browser.end();
     }
