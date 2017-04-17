cc4d0c3ddb517cdb6825d17c1be24088ab79cd62
Benjamin <benjamin.a.king@nasa.gov>
2017-04-17 10:19:46 -0400

    added cmr request file

---
+++ web/js/data/wv.data.cmr.request.js
@@ -0,0 +1,30 @@
+/*
+ * NASA Worldview
+ *
+ * This code was originally developed at NASA/Goddard Space Flight Center for
+ * the Earth Science Data and Information System (ESDIS) project.
+ *
+ * Copyright (C) 2013 - 2014 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
+
+/**
+ * @module wv.data
+ */
+var wv = wv || {};
+wv.data = wv.data || {};
+wv.data.cmr = wv.data.cmr || {};
+
+wv.data.cmr.request = wv.data.cmr.request || function(spec) {
+
+    
+    self = {};
+    var init = function() {};
+    var queryCMR = function(url, options, xml) {
+
+    };
+    var createXML = function() {
+        
+    };
+};
