3ed9ea4daa09e0724faada49c56c4798ec0c8b2a
ZachTRice <contact@zachtrice.com>
2017-07-07 14:04:20 -0400

    Updated gruntfile and globals to include comments about Windows config

--- e2e/globals.js
+++ e2e/globals.js
@@ -1,4 +1,5 @@
 module.exports = {
-  //url: 'https://uat.gibs.earthdata.nasa.gov/worldview-dev/v1.4.0/release/'
-  url: 'http://127.0.0.1/~baking1/worldview/web/'
+  url: 'https://uat.gibs.earthdata.nasa.gov/worldview-dev/v1.4.7-test/release/'
+  // url: 'http://localhost:3000'
+  // url: 'http://127.0.0.1/~baking1/worldview/web/'
 };
