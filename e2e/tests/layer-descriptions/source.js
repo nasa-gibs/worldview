603dfb82d87c26b54f40debf473d8b934d467577
ZachTRice <contact@zachtrice.com>
2017-07-10 10:15:58 -0400

    Formatted code (2 spaces), commented sidebar.js, added waitForElement... - Formatted code to use 2 space indents - Provided further comments within sidebar.js file - Added waitForElementVisible function to search.js to ensure elements are visible before performing next set of actions / checks.

--- e2e/tests/layer-descriptions/source.js
+++ e2e/tests/layer-descriptions/source.js
@@ -1,15 +1,13 @@
 module.exports = {
-    'Layer Descriptions - Measurment source ("Add Layers" Categories / All / Aerosol Optical Depth / Terra/MODIS)' : function (browser) {
-        browser
-            .url(browser.globals.url)
-            .pause(1000);
-        browser.waitForElementVisible('#skipTour', 10000, function (el) {
-            browser.click('#skipTour');
+  'Layer Descriptions - Measurment source ("Add Layers" Categories / All / Aerosol Optical Depth / Terra/MODIS)' : function (browser) {
+    browser.url(browser.globals.url).pause(1000);
+    browser.waitForElementVisible('#skipTour', 10000, function (el) {
+      browser.click('#skipTour');
 
-            // Check the 'Add Layers' Categories / All / Aerosol Optical Depth / Terra/MODIS
-            // for the presence of a description box.
+      // Check the 'Add Layers' Categories / All / Aerosol Optical Depth / Terra/MODIS
+      // for the presence of a description box.
 
-        });
-        browser.end();
-    }
+    });
+    browser.end();
+  }
 };
