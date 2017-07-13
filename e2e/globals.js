864ed8a176bd47c04d10c479d25701070ef875b6
ZachTRice <contact@zachtrice.com>
2017-07-13 09:51:01 -0400

    Changed global url parameter to use localhost

--- e2e/globals.js
+++ e2e/globals.js
@@ -1,5 +1,3 @@
 module.exports = {
-  url: 'https://uat.gibs.earthdata.nasa.gov/worldview-dev/v1.4.7-test/release/'
-  // url: 'http://localhost:3000'
-  // url: 'http://127.0.0.1/~baking1/worldview/web/'
+  url: 'http://localhost:3000'
 };
