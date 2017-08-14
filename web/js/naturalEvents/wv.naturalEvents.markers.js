8e8eff352cfa0b97d4d4767f3be986dbe32786e4
Josiah Sprague <josiah.sprague@gmail.com>
2017-08-14 14:43:56 -0400

    Cleanup and reorganize

--- web/js/naturalEvents/wv.naturalEvents.markers.js
+++ web/js/naturalEvents/wv.naturalEvents.markers.js
@@ -1,57 +1,28 @@
-/*
- * NASA Worldview
- *
- * This code was originally developed at NASA/Goddard Space Flight Center for
- * the Earth Science Data and Information System (ESDIS) project.
- *
- * Copyright (C) 2013 - 2017 United States Government as represented by the
- * Administrator of the National Aeronautics and Space Administration.
- * All Rights Reserved.
- */
+var wv = wv || {};
+wv.naturalEvents = wv.naturalEvents || {};
+wv.naturalEvents.markers = wv.naturalEvents.markers || function(models, maps, config) {
 
-var naturalEventsMarkers = function(models, maps, config) {
-  var self = {}, marker, markerEl, boundingBox, map;
+  var self = {},
+    boundingBox,
+    map,
+    marker;
+
   map = map || maps.selected;
 
-  self.draw = function(location) {
-    var hasPolygon = Array.isArray(location[0]);
+  self.draw = function(coordinates) {
+    var hasPolygon = Array.isArray(coordinates[0]);
     self.remove();
 
-    if (!marker) {
-      markerEl = document.createElement('div');
-      markerEl.className = 'map-marker';
-      markerEl.appendChild(document.createTextNode('x'));
-      marker = new ol.Overlay({
-        element: markerEl
-      });
-    }
-
     if (hasPolygon) {
-      boundingBox = new ol.layer.Vector({
-        source: new ol.source.Vector({
-          features: [new ol.Feature({
-            geometry: new ol.geom.Polygon(location),
-            name: 'NaturalEvent'
-          })],
-          wrapX: false
-        }),
-        style: new ol.style.Style({
-          fill: new ol.style.Fill({
-            color: [255, 255, 255, 0.1]
-          }),
-          stroke: new ol.style.Stroke({
-            color: [212, 85, 0, 0.8],
-            width: 2
-          })
-        })
-      });
+      boundingBox = createBoundingBox(coordinates);
       map.addLayer(boundingBox);
     } else {
-      marker.setPosition(location);
+      marker = marker || createMarker();
+      marker.setPosition(coordinates);
       map.addOverlay(marker);
     }
 
-    self.activeMarker = location;
+    self.activeMarker = coordinates;
   };
 
   self.remove = function() {
@@ -62,6 +33,32 @@ var naturalEventsMarkers = function(models, maps, config) {
   return self;
 };
 
-var wv = wv || {};
-wv.naturalEvents = wv.naturalEvents || {};
-wv.naturalEvents.markers = wv.naturalEvents.markers || naturalEventsMarkers;
+var createMarker = function(){
+  markerEl = document.createElement('div');
+  markerEl.className = 'map-marker';
+  markerEl.appendChild(document.createTextNode('x'));
+  return new ol.Overlay({
+    element: markerEl
+  });
+};
+
+var createBoundingBox = function(coordinates){
+  return new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: [new ol.Feature({
+        geometry: new ol.geom.Polygon(coordinates),
+        name: 'NaturalEvent'
+      })],
+      wrapX: false
+    }),
+    style: new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: [255, 255, 255, 0.1]
+      }),
+      stroke: new ol.style.Stroke({
+        color: [212, 85, 0, 0.8],
+        width: 2
+      })
+    })
+  });
+};
--- web/js/naturalEvents/wv.naturalEvents.ui.js
+++ web/js/naturalEvents/wv.naturalEvents.ui.js
@@ -1,14 +1,3 @@
-/*
- * NASA Worldview
- *
- * This code was originally developed at NASA/Goddard Space Flight Center for
- * the Earth Science Data and Information System (ESDIS) project.
- *
- * Copyright (C) 2013 - 2017 United States Government as represented by the
- * Administrator of the National Aeronautics and Space Administration.
- * All Rights Reserved.
- */
-
 var wv = wv || {};
 wv.naturalEvents = wv.naturalEvents || {};
 
