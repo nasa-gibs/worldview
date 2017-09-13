b2922af1aa0c9249b03b51329b708b88f0b5acb7
ZachTRice <contact@zachtrice.com>
2017-09-13 11:05:56 -0400

    Added check for package.json name; will only git pull if matches our options repo.

--- options-check.sh
+++ options-check.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+PACKAGE_NAME=$(sed -nE 's/^\s*"name": "(.*?)",$/\1/p' options/package.json)
 changed=0
-cd options && git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
-if [ $changed = 1 ]; then
-    git pull && cd -
-    echo "Updated successfully";
+if [ $PACKAGE_NAME = 'worldview-options-eosdis' ]; then
+    cd options && git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+    if [ $changed = 1 ]; then
+      git pull && cd -
+      echo "Updated successfully";
+    else
+      cd -
+      echo "Up-to-date"
+    fi
 else
-    cd -
-    echo "Up-to-date"
+    echo "Worldview options not found.";
 fi
