8e773abd41e7be87ae4467c1af7c1dec74448bf1
Jo Sprague <josiah.sprague@gmail.com>
2017-12-07 16:15:05 -0500

    Modularize layer tests
    Lodash was mistakenly used before instead of underscore

--- test/main.js
+++ test/main.js
@@ -1,9 +1,13 @@
 // This file is an entry point used to build a testable bundle of the WV core
 // It exposes some internal methods on a global `wv` var for testing purposes
+import underscore from 'underscore';
 import 'babel-polyfill'; // Polyfill fixes a bug in Phantom 2.1.x
 import brand from '../web/js/brand';
 import { parse as dateParser } from '../web/js/date/date';
 import { dateModel } from '../web/js/date/model';
+import { parse as layerParser } from '../web/js/layers/layers';
+import { layersModel } from '../web/js/layers/model';
+import { projectionModel } from '../web/js/projection/model';
 import util from '../web/js/util/util';
 import fixtures from './fixtures.js';
 
@@ -13,7 +17,15 @@ window.wv = {
     parse: dateParser,
     model: dateModel
   },
+  layers: {
+    parse: layerParser,
+    model: layersModel
+  },
+  proj: {
+    model: projectionModel
+  },
   util: util
 };
 
 window.fixtures = fixtures;
+window._ = underscore;
