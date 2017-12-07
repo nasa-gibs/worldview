ba2a2a2a30aed09a4181059607a53a53e76d6741
Jo Sprague <josiah.sprague@gmail.com>
2017-12-07 13:41:44 -0500

    Modularize dateModel tests

--- test/main.js
+++ test/main.js
@@ -2,13 +2,15 @@
 // It exposes some internal methods on a global `wv` var for testing purposes
 import brand from '../web/js/brand';
 import { parse as dateParser } from '../web/js/date/date';
+import { dateModel } from '../web/js/date/model';
 import util from '../web/js/util/util';
 import fixtures from './fixtures.js';
 
 window.wv = {
   brand: brand,
   date: {
-    parse: dateParser
+    parse: dateParser,
+    model: dateModel
   },
   util: util
 };
