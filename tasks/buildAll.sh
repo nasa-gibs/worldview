74f6864f49ca7e6c072cccdb1996f55576414acd
Jo Sprague <josiah.sprague@gmail.com>
2018-01-11 09:32:42 -0500

    Add --production=false flag to npm install
    Makes sure to install devDependencies even when NODE_ENV is set to production

--- tasks/buildAll.sh
+++ tasks/buildAll.sh
@@ -5,7 +5,7 @@
 # Reset directories that have build or install artifacts
 rm -rf build dist node_modules .python web/build
 
-npm install # Install dependencies
+npm install --production=false # Install dependencies, set production false so devDependencies are installed even though NODE_ENV is set to production
 npm run build # Build the app
 
 # Prepare rpm sources
