d868a040d6c29fc18b55bf86591185d683365f96
Benjamin King <benjamin.a.king@nasa.gov>
2018-04-04 21:45:15 -0400

    Create tests that res limits disable download #819

--- e2e/step_definitions/utility.js
+++ e2e/step_definitions/utility.js
@@ -32,14 +32,19 @@ defineSupportCode(({ Given, Then, When }) => {
     return client.useCss().click(selectors[key] || key);
   });
 
+  // Input a value into a keyed element
+  When('I input {string} in the {string}', (value, key) => {
+    return client.useCss().setValue(selectors[key] || key, value);
+  });
+
   // Click a Worldview tab
   When('I click the {string} tab', (name) => {
     return client.useCss().click('[data-tab="' + name + '"]');
   });
 
-  // Input a value into a keyed element
-  When('I input {string} in the {string}', (value, key) => {
-    return client.useCss().setValue(selectors[key] || key, value);
+  // Check value of input
+  When('I see a value of {string} in the {string}', (value, key) => {
+    return client.assert.value(selectors[key] || key, value);
   });
 
   // Pause for n seconds
@@ -92,6 +97,20 @@ defineSupportCode(({ Given, Then, When }) => {
     return client.useCss().assert.containsText(selectors[key] || key, text);
   });
 
+  // check for a class in an element
+  Then('I see the class {string} in the {string}', (text, key) => {
+    return client.assert.cssClassPresent(selectors[key] || key, text);
+  });
+
+  // check if a class is disabled
+  Then('I see {string} is disabled', (key) => {
+    return client.expect.element(selectors[key] || key).to.not.be.enabled;
+  });
+
+  // check if an element has a css property
+  Then('I see {string} has style property {string} equal to {string}', (key, prop, value) => {
+    return client.assert.cssProperty(selectors[key] || key, prop, value);
+  });
   // Check that an element is visible by predefined selector
   Then('I see the {string}', (key) => {
     return client.useCss().expect.element(selectors[key] || key).to.be.visible;
