638d00eafd26fc0c68c602a66e0f9a29a0212bb1
Benjamin King <benjamin.a.king@nasa.gov>
2018-05-10 15:19:33 -0400

    replace resizeWindow with normalizeViewport #689

--- e2e/reuseables/skip-tour.js
+++ e2e/reuseables/skip-tour.js
@@ -1,12 +1,13 @@
+const normalizeViewport = require('./normalize-viewport.js').normalizeViewport;
 module.exports = {
   loadAndSkipTour: function(client, wait) {
-    client.resizeWindow(1200, 1024);
+    normalizeViewport(client, 1000, 1024);
     client.url(client.globals.url)
       .execute(function() {
         return !(window.localStorage.getItem('hideSplash')); // See if there should be a tour
       }, [], function(result) {
-        const hasTour = result.value
-        if(hasTour) {
+        const hasTour = result.value;
+        if (hasTour) {
           client.waitForElementVisible('#skipTour', wait, function() {
             client.click('#skipTour');
             client.pause(1000);
