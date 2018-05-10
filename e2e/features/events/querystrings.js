e4525e4dbf374d328e1b516c91073b174543e0c5
Benjamin King <benjamin.a.king@nasa.gov>
2018-05-10 15:15:50 -0400

    Remove cucumber tests and fix linting #930

--- e2e/features/events/querystrings.js
+++ e2e/features/events/querystrings.js
@@ -1,4 +1,5 @@
 module.exports = {
   eventsTabActive: '?e=true',
   mockEvents: '?p=geographic&l=VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor,Reference_Labels(hidden),Reference_Features(hidden),Coastlines&t=2018-05-02-T00%3A00%3A00Z&z=3&v=-409.00147812273656,-205.62883007565202,270.5880270080828,219.11461063111003&e=true&mockEvents=20170530'
-}
+};
+
--- e2e/features/layers/permalinks.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  multipleDataLayers: '?p=geographic&l=MODIS_Terra_Aerosol,MODIS_Terra_Brightness_Temp_Band31_Day&t=2017-03-22&z=3&v=136.07019188386334,14.722152527011556,155.59817576644127,24.312819167567586',
-  continuousDataLayers: '?p=geographic&l=MODIS_Terra_Brightness_Temp_Band31_Day&t=2015-05-25&z=2&v=-42.148380855752734,42.13121723408824,22.122734950093943,85.16225953076464'
-};
