c562bb375c4f5ba1cd06b7d317cbee13f3094ba8
Benjamin King <benjamin.a.king@nasa.gov>
2018-05-10 15:21:06 -0400

    Remove global selectors that have been replaced by local selectors #930

--- e2e/globals.js
+++ e2e/globals.js
@@ -4,13 +4,6 @@ module.exports = {
   selectors: {
     'eventsTab': '[data-tab="events"]',
     'dataTab': '[data-tab="download"]',
-    'infoButton': '.wv-layers-info',
-    'optionsButton': '.wv-layers-options',
-    'infoDialog': '[aria-describedby="wv-layers-info-dialog"]',
-    'optionsDialog': '[aria-describedby="wv-layers-options-dialog"]',
-    'layersSearchField': '#layers-search-input',
-    'sourceInfoIcon': '.layers-all-layer .fa-info-circle',
-    'sourceMetadataCloseButton': '.source-metadata .metadata-more',
     'yearlyResolutionTooltip': '#zoom-years',
     'timelineSetToYears': '#current-zoom.zoom-years',
     'modalCloseButton': '.ui-dialog.ui-draggable.ui-resizable .ui-dialog-titlebar-close',
