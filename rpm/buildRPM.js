3d7816bb59cf46992f39c63336bba5773ca130e0
Mike McGann <mike.mcgann@nasa.gov>
2018-07-18 09:50:20 -0400

    as much of the rpm install as possible right now, finish the rest on linux

--- rpm/buildRPM.js
+++ rpm/buildRPM.js
@@ -5,20 +5,26 @@ const pkg = require('../package.json');
 const moment = require('moment');
 
 let buildNumber = moment.utc().format('YYMMDDHHmmss');
+const homedir = require('os').homedir();
+
+// https://github.com/docker/for-mac/issues/2296
+
+shell.exec('npm run dist');
 
 console.log('Preparing RPM build');
-shell.mkdir('-p', 'build/rpm/SOURCES');
-shell.cp('dist/worldview.tar.gz', 'build/rpm/SOURCES');
-shell.cp('rpm/httpd.conf', 'build/rpm/SOURCES');
-shell.mkdir('-p', 'build/rpm/SPECS');
-shell.cp('rpm/worldview.spec', 'build/rpm/SPECS');
+shell.rm('-rf', `${homedir}/rpmbuild`);
+shell.mkdir('-p', `${homedir}/rpmbuild/SOURCES`);
+shell.cp('dist/worldview.tar.gz', `${homedir}/rpmbuild/SOURCES`);
+shell.cp('rpm/httpd.conf', `${homedir}/rpmbuild/SOURCES`);
+shell.mkdir('-p', `${homedir}/rpmbuild/SPECS`);
+shell.cp('rpm/worldview.spec', `${homedir}/rpmbuild/SPECS`);
 
 console.log('Branding');
-let applyTo = 'build/rpm/SPECS/worldview.spec';
+let applyTo = `${homedir}/rpmbuild/SPECS/worldview.spec`;
 shell.sed('-i', /@WORLDVIEW@/g, 'worldview', applyTo);
 shell.sed('-i', /@BUILD_VERSION@/g, pkg.version, applyTo);
 shell.sed('-i', /@BUILD_NUMBER@/g, buildNumber, applyTo);
 
 console.log('Building RPM');
-let pwd = process.env.PWD;
-shell.exec(`rpmbuild --define \\"_topdir ${pwd}/build/rpm\\" -ba build/rpm/SPECS/worldview.spec`);
+shell.exec(`rpmbuild -ba ${homedir}/rpmbuild/SPECS/worldview.spec`);
+shell.cp(`${homedir}/rpmbuild/RPMS/noarch/*`, 'dist');
