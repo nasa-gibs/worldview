3e8b07bfb4e86aa30527930a484252b8b29c324e
Mike McGann <mike.mcgann@nasa.gov>
2018-07-19 13:17:38 -0400

    distclean not required in bamboo build

--- tasks/buildAll.sh
+++ tasks/buildAll.sh
@@ -2,13 +2,10 @@
 
 # Bash commands to run in Bamboo to prepare an rpm for deployment
 
-# Reset directories that have build or install artifacts
-npm run distclean
-
 npm install --production=false # Install dependencies, set production false so devDependencies are installed even though NODE_ENV is set to production
 npm run dist
 node rpm/buildRPM.js
 
 # This is where bamboo expects to find artifacts
-mv ~/rpmbuild build/rpm 
+mv ~/rpmbuild build/rpm
 
