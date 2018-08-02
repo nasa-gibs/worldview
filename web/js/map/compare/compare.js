1b27edfc8ad12d97f29e18503d016f8c8585e2b9
Benjamin King <benking4@gmail.com>
2018-08-02 11:54:04 -0400

    Add touchEvents to swipe #1118

--- web/js/map/compare/compare.js
+++ web/js/map/compare/compare.js
@@ -1,24 +1,36 @@
-import { Swipe } from './swipe';
-import { Opacity } from './opacity';
-import { Spy } from './spy';
-import util from '../../util/util';
+import { Swipe } from "./swipe";
+import { Opacity } from "./opacity";
+import { Spy } from "./spy";
+import util from "../../util/util";
+
+const TOUCH_EVENT = {
+  start: "touchstart",
+  move: "touchmove",
+  end: "touchend"
+};
+const MOUSE_EVENT = {
+  start: "mousedown",
+  move: "mousemove",
+  end: "mouseup"
+};
 export function mapCompare(models, config) {
   var self = {};
   var comparison = null;
-  var mode = 'swipe';
-  var proj = '';
+  var mode = "swipe";
+  var proj = "";
   self.events = util.events();
   self.swipe = Swipe;
   self.opacity = Opacity;
   self.spy = Spy;
   self.active = false;
   self.dragging = false;
+  self.EventTypeObject = util.browser.touchDevice ? TOUCH_EVENT : MOUSE_EVENT;
   var init = function() {
     self.events
-      .on('mousedown', () => {
+      .on(self.EventTypeObject.start, () => {
         self.dragging = true;
       })
-      .on('mouseup', () => {
+      .on(self.EventTypeObject.end, () => {
         self.dragging = false;
       });
   };
@@ -31,21 +43,23 @@ export function mapCompare(models, config) {
       comparison = new self[compareMode](
         map,
         models.compare.isCompareA,
-        self.events
+        self.events,
+        self.EventTypeObject
       ); // e.g. new self.swipe()
     } else {
       mode = compareMode;
       comparison = new self[compareMode](
         map,
         models.compare.isCompareA,
-        self.events
+        self.events,
+        self.EventTypeObject
       ); // e.g. new self.swipe()
     }
     self.active = true;
     proj = models.proj.selected;
   };
   self.getOffset = function() {
-    if (mode === 'swipe') {
+    if (mode === "swipe") {
       return comparison.getSwipeOffset();
     } else {
       return null;
--- web/js/map/compare/swipe.js
+++ web/js/map/compare/swipe.js
@@ -1,4 +1,4 @@
-import lodashEach from 'lodash/each';
+import lodashEach from "lodash/each";
 
 var swipeOffset = null;
 var line = null;
@@ -7,8 +7,10 @@ var topLayers = [];
 var map;
 var events;
 var mapCase;
+var listenerObj;
 export class Swipe {
-  constructor(olMap, isAactive, compareEvents) {
+  constructor(olMap, isAactive, compareEvents, eventListenerStringObj) {
+    listenerObj = eventListenerStringObj;
     map = olMap;
     events = compareEvents;
     this.create();
@@ -34,60 +36,60 @@ export class Swipe {
 }
 
 var addLineOverlay = function(map) {
-  var lineCaseEl = document.createElement('div');
-  var draggerEl = document.createElement('div');
-  var iconEl = document.createElement('i');
-  var firstLabel = document.createElement('span');
-  var secondLabel = document.createElement('span');
-  mapCase = document.getElementById('wv-map');
-  firstLabel.className = 'ab-swipe-span left-label';
-  secondLabel.className = 'ab-swipe-span right-label';
-  firstLabel.appendChild(document.createTextNode('A'));
-  secondLabel.appendChild(document.createTextNode('B'));
+  var lineCaseEl = document.createElement("div");
+  var draggerEl = document.createElement("div");
+  var iconEl = document.createElement("i");
+  var firstLabel = document.createElement("span");
+  var secondLabel = document.createElement("span");
+  mapCase = document.getElementById("wv-map");
+  firstLabel.className = "ab-swipe-span left-label";
+  secondLabel.className = "ab-swipe-span right-label";
+  firstLabel.appendChild(document.createTextNode("A"));
+  secondLabel.appendChild(document.createTextNode("B"));
 
-  iconEl.className = 'fa fa-arrows-h';
-  draggerEl.className = 'ab-swipe-dragger';
-  lineCaseEl.className = 'ab-swipe-line';
+  iconEl.className = "fa fa-arrows-h";
+  draggerEl.className = "ab-swipe-dragger";
+  lineCaseEl.className = "ab-swipe-line";
   lineCaseEl.appendChild(firstLabel);
   lineCaseEl.appendChild(secondLabel);
   draggerEl.appendChild(iconEl);
   lineCaseEl.appendChild(draggerEl);
   mapCase.appendChild(lineCaseEl);
   swipeOffset = swipeOffset || mapCase.offsetWidth / 2;
-  lineCaseEl.style.transform = 'translateX( ' + swipeOffset + 'px)';
+  lineCaseEl.style.transform = "translateX( " + swipeOffset + "px)";
 
   [lineCaseEl, draggerEl].forEach(el => {
-    el.addEventListener('mousedown', evt => {
-      events.trigger('mousedown');
+    el.addEventListener(listenerObj.start, evt => {
+      events.trigger(listenerObj.start);
       evt.preventDefault();
       evt.stopPropagation();
       function move(evt) {
         evt.preventDefault();
         evt.stopPropagation();
         swipeOffset = evt.clientX;
-        lineCaseEl.style.transform = 'translateX( ' + swipeOffset + 'px)';
+        lineCaseEl.style.transform = "translateX( " + swipeOffset + "px)";
 
         map.render();
       }
       function end(evt) {
-        events.trigger('mouseup');
-        window.removeEventListener('mousemove', move);
-        window.removeEventListener('mouseup', end);
+        events.trigger(listenerObj.end);
+        window.removeEventListener(listenerObj.move, move);
+        window.removeEventListener(listenerObj.end, end);
       }
-      window.addEventListener('mousemove', move);
-      window.addEventListener('mouseup', end);
+      window.addEventListener(listenerObj.move, move);
+      window.addEventListener(listenerObj.end, end);
     });
   });
   return lineCaseEl;
 };
 var applyLayerListeners = function(layer) {
-  layer.on('precompose', clip);
-  layer.on('postcompose', restore);
+  layer.on("precompose", clip);
+  layer.on("postcompose", restore);
   bottomLayers.push(layer);
 };
 var applyReverseLayerListeners = function(layer) {
-  layer.on('precompose', reverseClip);
-  layer.on('postcompose', restore);
+  layer.on("precompose", reverseClip);
+  layer.on("postcompose", restore);
   topLayers.push(layer);
 };
 
@@ -115,19 +117,19 @@ var restore = function(event) {
 };
 var removeListenersFromBottomLayers = function(layers) {
   lodashEach(layers, layer => {
-    layer.un('precompose', reverseClip);
-    layer.un('postcompose', restore);
+    layer.un("precompose", reverseClip);
+    layer.un("postcompose", restore);
   });
 };
 var removeListenersFromLayers = function(layers) {
   lodashEach(layers, layer => {
-    layer.un('precompose', clip);
-    layer.un('postcompose', restore);
+    layer.un("precompose", clip);
+    layer.un("postcompose", restore);
   });
 };
 
 var applyEventsToBaseLayers = function(layer, map, callback) {
-  var layers = layer.get('layers');
+  var layers = layer.get("layers");
   if (layers) {
     lodashEach(layers.getArray(), layer => {
       applyEventsToBaseLayers(layer, map, callback);
