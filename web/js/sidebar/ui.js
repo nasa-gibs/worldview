db2eb77dcb59b2fe2ba0194a17fb584dc277e4c6
Benjamin King <benking4@gmail.com>
2018-08-02 13:22:08 -0400

    Remove debouncers that are causing more problems than solving #1118

--- web/js/sidebar/ui.js
+++ web/js/sidebar/ui.js
@@ -1,80 +1,73 @@
-import $ from 'jquery';
-import 'jquery-ui/dialog';
-import lodashFind from 'lodash/find';
-import lodashDebounce from 'lodash/debounce';
-import util from '../util/util';
-import Sidebar from '../components/sidebar/sidebar';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { getCompareObjects } from '../compare/util';
-import { getCheckerboard } from '../palettes/util';
-import { layersOptions } from '../layers/options';
-import { layersInfo } from '../layers/info';
-import palettes from '../palettes/palettes';
-import { getZotsForActiveLayers } from '../layers/util';
-import { timelineDataHightlight } from '../date/util';
-import wvui from '../ui/ui';
+import $ from "jquery";
+import "jquery-ui/dialog";
+import lodashFind from "lodash/find";
+import lodashDebounce from "lodash/debounce";
+import util from "../util/util";
+import Sidebar from "../components/sidebar/sidebar";
+import React from "react";
+import ReactDOM from "react-dom";
+import { getCompareObjects } from "../compare/util";
+import { getCheckerboard } from "../palettes/util";
+import { layersOptions } from "../layers/options";
+import { layersInfo } from "../layers/info";
+import palettes from "../palettes/palettes";
+import { getZotsForActiveLayers } from "../layers/util";
+import { timelineDataHightlight } from "../date/util";
+import wvui from "../ui/ui";
 
 export function sidebarUi(models, config, ui) {
   var isCollapsed = false;
-  var activeTab = 'layers';
+  var activeTab = "layers";
   var self = {};
   var model = models.layers;
   var compareObj = {};
-  var compareModeType = 'swipe';
+  var compareModeType = "swipe";
 
   self.events = util.events();
 
   var init = function() {
     self.reactComponent = ReactDOM.render(
       React.createElement(Sidebar, getInitialProps()),
-      document.getElementById('wv-sidebar')
+      document.getElementById("wv-sidebar")
     );
-    var debounceLayerUpdates = lodashDebounce(updateLayers, 300, {
-      leading: true,
-      trailing: true
-    });
+
     var debounceUpdateData = lodashDebounce(updateData, 300, { leading: true });
     var debounceUpdateEventsList = lodashDebounce(updateEventsList, 300, {
       leading: true,
       trailing: true
     });
-    var debounceLayerAdd = lodashDebounce(
-      layer => {
-        updateLayers();
-        updateState('zotsObject', getZotsForActiveLayers(config, models, ui));
-      },
-      300,
-      { leading: true, trailing: true }
-    );
+    var layerAdd = function(layer) {
+      updateLayers();
+      updateState("zotsObject", getZotsForActiveLayers(config, models, ui));
+    };
 
     // Set Event Listeners
     models.data.events
-      .on('activate', () => {
-        self.selectTab('download');
+      .on("activate", () => {
+        self.selectTab("download");
         debounceUpdateData();
       })
-      .on('productSelect', onProductSelect)
-      .on('layerUpdate', debounceUpdateData)
-      .on('granuleSelect', debounceUpdateData)
-      .on('granuleUnselect', debounceUpdateData);
+      .on("productSelect", onProductSelect)
+      .on("layerUpdate", debounceUpdateData)
+      .on("granuleSelect", debounceUpdateData)
+      .on("granuleUnselect", debounceUpdateData);
     models.layers.events
-      .on('add', debounceLayerAdd)
-      .on('remove', debounceLayerUpdates)
-      .on('update', debounceLayerUpdates);
+      .on("add", layerAdd)
+      .on("remove", updateLayers)
+      .on("update", updateLayers);
 
     models.palettes.events
-      .on('set-custom', debounceLayerUpdates)
-      .on('clear-custom', debounceLayerUpdates)
-      .on('range', debounceLayerUpdates)
-      .on('update', debounceLayerUpdates);
+      .on("set-custom", updateLayers)
+      .on("clear-custom", updateLayers)
+      .on("range", updateLayers)
+      .on("update", updateLayers);
     models.naturalEvents.events
-      .on('activate', () => self.selectTab('events'))
-      .on('list-change', debounceUpdateEventsList)
-      .on('selected-event', selected => {
+      .on("activate", () => self.selectTab("events"))
+      .on("list-change", debounceUpdateEventsList)
+      .on("selected-event", selected => {
         self.reactComponent.setState({ selectedEvent: selected });
       })
-      .on('hasData', () => {
+      .on("hasData", () => {
         self.reactComponent.setState({
           eventsData: models.naturalEvents.data,
           selectEvent: ui.naturalEvents.selectEvent,
@@ -83,24 +76,24 @@ export function sidebarUi(models, config, ui) {
         });
       });
     models.compare.events
-      .on('toggle', () => {
-        updateState('isCompareMode');
-        updateState('layerObjects');
-        updateState('layers');
+      .on("toggle", () => {
+        updateState("isCompareMode");
+        updateState("layerObjects");
+        updateState("layers");
       })
-      .on('toggle-state', () => {
-        updateState('isCompareA');
+      .on("toggle-state", () => {
+        updateState("isCompareA");
       });
-    models.date.events.on('select', debounceLayerUpdates);
-    models.proj.events.on('select', debounceLayerUpdates);
-    models.map.events.on('data-running', runningLayers => {
+    models.date.events.on("select", updateLayers);
+    models.proj.events.on("select", updateLayers);
+    models.map.events.on("data-running", runningLayers => {
       self.reactComponent.setState({ runningLayers: runningLayers });
     });
     $(window).resize(resize);
     ui.map.events.on(
-      'zooming',
+      "zooming",
       lodashDebounce(() => {
-        updateState('zotsObject', getZotsForActiveLayers(config, models, ui));
+        updateState("zotsObject", getZotsForActiveLayers(config, models, ui));
       }, 300)
     );
   };
@@ -108,10 +101,10 @@ export function sidebarUi(models, config, ui) {
   var getInitialProps = function() {
     var compareModel;
     activeTab = models.naturalEvents.active
-      ? 'events'
+      ? "events"
       : models.data.active
-        ? 'download'
-        : 'layers';
+        ? "download"
+        : "layers";
     if (config.features.compare) {
       compareModel = models.compare;
       if (models.compare.active) {
@@ -124,7 +117,7 @@ export function sidebarUi(models, config, ui) {
       isCompareMode:
         compareModel && compareModel.active ? compareModel.active : false,
       isCollapsed: isCollapsed,
-      layers: model.get({ group: 'all' }),
+      layers: model.get({ group: "all" }),
       onTabClick: self.selectTab,
       toggleSidebar: toggleSidebar,
       toggleLayerVisibility: toggleLayerVisibility,
@@ -178,11 +171,11 @@ export function sidebarUi(models, config, ui) {
   };
   var updateLayers = function() {
     if (models.compare.active) {
-      updateState('layerObjects');
-      updateState('zotsObject', getZotsForActiveLayers(config, models, ui));
+      updateState("layerObjects");
+      updateState("zotsObject", getZotsForActiveLayers(config, models, ui));
     } else {
-      updateState('layers');
-      updateState('zotsObject', getZotsForActiveLayers(config, models, ui));
+      updateState("layers");
+      updateState("zotsObject", getZotsForActiveLayers(config, models, ui));
     }
   };
   var onProductSelect = function(product) {
@@ -213,7 +206,7 @@ export function sidebarUi(models, config, ui) {
   };
   var onAddLayerCLick = function() {
     wvui.closeDialog();
-    $('#layer-modal').dialog('open');
+    $("#layer-modal").dialog("open");
   };
 
   // Need to rethink what is going on here
@@ -249,29 +242,29 @@ export function sidebarUi(models, config, ui) {
   };
   var updateState = function(type, value) {
     switch (type) {
-      case 'isCollapsed':
+      case "isCollapsed":
         return self.reactComponent.setState({ isCollapsed: isCollapsed });
-      case 'activeTab':
+      case "activeTab":
         return self.reactComponent.setState({ activeTab: activeTab });
-      case 'layers':
+      case "layers":
         return self.reactComponent.setState({
           layers: models.layers.get(
-            { group: 'all' },
+            { group: "all" },
             models.layers[models.layers.activeLayers]
           )
         });
-      case 'isCompareMode':
+      case "isCompareMode":
         return self.reactComponent.setState({
           isCompareMode: models.compare ? models.compare.active : false
         });
-      case 'isCompareA': {
+      case "isCompareA": {
         return self.reactComponent.setState({
           isCompareA: models.compare ? models.compare.isCompareA : true
         });
       }
-      case 'zotsObject':
+      case "zotsObject":
         return self.reactComponent.setState({ zotsObject: value });
-      case 'layerObjects':
+      case "layerObjects":
         compareObj = getCompareObjects(models);
         return self.reactComponent.setState({
           firstDateObject: compareObj.a,
@@ -281,44 +274,44 @@ export function sidebarUi(models, config, ui) {
   };
   self.expandNow = function() {
     isCollapsed = false;
-    updateState('isCollapsed');
+    updateState("isCollapsed");
   };
   var toggleSidebar = function() {
     isCollapsed = !isCollapsed;
-    updateState('isCollapsed');
+    updateState("isCollapsed");
   };
 
   self.selectTab = function(tab) {
     if (activeTab === tab) return;
     activeTab = tab;
-    self.events.trigger('selectTab', tab);
-    updateState('activeTab');
+    self.events.trigger("selectTab", tab);
+    updateState("activeTab");
   };
   var updateLayer = function(layerId, typeOfUpdate, value) {
     var layer;
     var layerGroupString = models.layers.activeLayers;
     var isCompareMode = models.compare && models.compare.active;
     switch (typeOfUpdate) {
-      case 'remove':
+      case "remove":
         models.layers.remove(layerId, layerGroupString);
         break;
-      case 'add':
+      case "add":
         models.layers.add(layerId, {}, layerGroupString);
         break;
-      case 'visibility':
+      case "visibility":
         models.layers.toggleVisibility(layerId, layerGroupString);
         updateState(removeLayerState(isCompareMode));
         break;
-      case 'info':
+      case "info":
         layer = lodashFind(models.layers[layerGroupString], { id: layerId });
         layersInfo(config, models, layer);
         break;
-      case 'options':
+      case "options":
         layer = lodashFind(models.layers[layerGroupString], { id: layerId });
         console.log(models, layer, layerGroupString);
         layersOptions(config, models, layer, layerGroupString);
         break;
-      case 'hover':
+      case "hover":
         timelineDataHightlight(layerId, value);
         break;
       default:
@@ -326,15 +319,15 @@ export function sidebarUi(models, config, ui) {
     }
   };
   var removeLayerState = function(isCompareActive) {
-    return isCompareActive ? 'layers' : 'layerObjects';
+    return isCompareActive ? "layers" : "layerObjects";
   };
   var toggleLayerVisibility = function(layerId, isVisible) {
     var layerGroupString = models.layers.activeLayers;
     models.layers.setVisibility(layerId, isVisible, layerGroupString);
-    if (layerGroupString === 'active') {
-      updateState('layers');
+    if (layerGroupString === "active") {
+      updateState("layers");
     } else {
-      updateState('layerObjects');
+      updateState("layerObjects");
     }
   };
 
