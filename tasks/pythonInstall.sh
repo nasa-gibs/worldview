d6194a953073f3d6465194901cd45d318489c2ff
Benjamin King <benking4@gmail.com>
2018-08-22 17:14:50 -0400

    add --user flag to overcome permissions issues #1212

--- tasks/pythonInstall.sh
+++ tasks/pythonInstall.sh
@@ -4,11 +4,11 @@ if [ $1 = "linux" ]; then
   # If is a linux machine try to run python2
   if command -v python2 &>/dev/null; then
     echo "Installing virtualenv with python2"
-    python2 -m pip install virtualenv
+    python2 -m pip install --user virtualenv
     python2 -m virtualenv .python
   else
     echo "Installing virtualenv with python"
-    python -m pip install virtualenv
+    python -m pip install --user virtualenv
     python -m virtualenv .python
   fi
   PATH=.python/bin:${PATH} pip install -r requirements.txt
