715549d939891fdd3414918365a93cfb35024bd1
ZachTRice <zachtrice@gmail.com>
2018-09-20 16:46:44 -0400

    Implement initial tour modal window

--- web/js/components/tour/start.js
+++ web/js/components/tour/start.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class ModalExample extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false,
+      // modal: false,
+      modal: true,
       backdrop: true
     };
+
+    this.buttonLabel = 'testing';
 
     this.toggle = this.toggle.bind(this);
     this.changeBackdrop = this.changeBackdrop.bind(this);
@@ -28,21 +31,20 @@ class ModalExample extends React.Component {
   }
 
   render() {
+
+    const quickButtonStyle = {
+      'position': 'absolute',
+      'top': '0',
+      'right': '0',
+      'z-index': '999',
+      'height': '24px',
+      'width': '96px',
+    };
+
     return (
       <div>
-        <Form inline onSubmit={(e) => e.preventDefault()}>
-          <FormGroup>
-            <Label for="backdrop">Backdrop value</Label>{' '}
-            <Input type="select" name="backdrop" id="backdrop" onChange={this.changeBackdrop}>
-              <option value="true">true</option>
-              <option value="false">false</option>
-              <option value="static">"static"</option>
-            </Input>
-          </FormGroup>
-          {' '}
-          <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-        </Form>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} backdrop={this.state.backdrop}>
+        {/* <Button color="danger" style={quickButtonStyle} onClick={this.toggle}>Start Tour</Button> */}
+        <Modal isOpen={this.state.modal} toggle={this.toggle} wrapClassName='tour tour-start' className={this.props.className} backdrop={this.state.backdrop}>
           <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
           <ModalBody>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
