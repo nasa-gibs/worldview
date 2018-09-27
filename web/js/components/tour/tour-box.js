af16232bcb161ffaebde77d1cd0208ae6d9bb17a
ZachTRice <zachtrice@gmail.com>
2018-09-27 10:42:19 -0400

    Add JSON background image keys + style state for backgroundImage and backgroundImageHover

--- web/js/components/tour/tour-box.js
+++ web/js/components/tour/tour-box.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
 class TourBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styles: { }
+    };
+
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
+  }
+
+  componentWillMount() {
+    if (this.props.backgroundImage) {
+      let { origin, pathname } = window.location;
+      this.setState({
+        styles: { backgroundImage: `url(${origin}${pathname}stories/${this.props.tourId}/${this.props.backgroundImage})` }
+      });
+    }
+  }
+
+  onMouseOver(e) {
+    e.preventDefault();
+    if (this.props.backgroundImageHover) {
+      let { origin, pathname } = window.location;
+      this.setState({
+        styles: { backgroundImage: `url(${origin}${pathname}stories/${this.props.tourId}/${this.props.backgroundImageHover})` }
+      });
+    }
+  }
+
+  onMouseOut(e) {
+    e.preventDefault();
+    if (this.props.backgroundImage) {
+      let { origin, pathname } = window.location;
+      this.setState({
+        styles: { backgroundImage: `url(${origin}${pathname}stories/${this.props.tourId}/${this.props.backgroundImage})` }
+      });
+    }
+  }
+
   render() {
     return (
-      <a href="#" className={this.props.className} onClick={(e) => this.props.startTour(e, this.props.box)}>
+      <a href="#" style={this.state.styles} onMouseOver={(e) => this.onMouseOver(e)} onMouseOut={(e) => this.onMouseOut(e)} className={this.props.className} onClick={(e) => this.props.startTour(e, this.props.box)}>
         <div className="tour-box-content">
           <div className="tour-box-header">
             <h3 className="tour-box-title">{this.props.title}</h3>
--- web/js/components/tour/tour-boxes.js
+++ web/js/components/tour/tour-boxes.js
@@ -10,7 +10,7 @@ class TourBoxes extends React.Component {
         <div className="tour-box-container">
           <div className="tour-box-row">
             {data.map(function(object, i) {
-              return (<TourBox key={i} box={i} title={object.title} description={object.description} startTour={startTour} className={'tour-box ' + object.type}/>);
+              return (<TourBox key={i} box={i} tourId={object.id} title={object.title} description={object.description} backgroundImage={object.backgroundImage} backgroundImageHover={object.backgroundImageHover} startTour={startTour} className={'tour-box ' + object.type}/>);
             })}
           </div>
         </div>
