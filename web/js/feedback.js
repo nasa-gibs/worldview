b71f34f6f70acff7001daa6224d5bf3b9a0ed70c
ZachTRice <zachtrice@gmail.com>
2018-10-03 20:23:45 -0400

    Modernize feedback module code

--- web/js/feedback.js
+++ web/js/feedback.js
@@ -4,30 +4,22 @@
  * https://fbm.earthdata.nasa.gov/for/worldview/feedback.js
  * which is included via config.scripts in web/config/wv.json
 */
-
 import util from './util/util';
 
-export default (function () {
+export function feedbackUi() {
   var self = {};
   var feedbackInit = false;
 
-  self.decorate = function ($element) {
-    $element.attr('href',
-      'mailto:@MAIL@?subject=Feedback for @LONG_NAME@ tool');
-
-    $element.click(function (event) {
-      if (!util.browser.small && window.feedback) {
-        event.preventDefault();
-        if (!feedbackInit) {
-          feedback.init({
-            showIcon: false
-          });
-        }
-        feedback.showForm();
-        feedbackInit = true;
+  self.showFeedback = function() {
+    if (!util.browser.small && window.feedback) {
+      if (!feedbackInit) {
+        feedback.init({
+          showIcon: false
+        });
       }
-    });
+      feedback.showForm();
+      feedbackInit = true;
+    }
   };
-
   return self;
-})();
+}
