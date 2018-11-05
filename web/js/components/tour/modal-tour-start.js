01aa75c11eea6964ba6911abc42c006897d5f8b0
ZachTRice <zachtrice@gmail.com>
2018-11-04 20:20:47 -0500

    Add checkbox to hide tour modal until a new story has been added

--- web/js/components/tour/modal-tour-start.js
+++ web/js/components/tour/modal-tour-start.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import TourIntro from './content-intro';
 import TourBoxes from './tour-boxes';
 
 class ModalStart extends React.Component {
   render() {
     return (
-      <div>
-        <Modal isOpen={this.props.modalStart} toggle={this.props.toggleModalStart} onClosed={this.props.showTourAlert} wrapClassName='tour tour-start' className={this.props.className} backdrop={true}>
-          <ModalHeader toggle={this.props.toggleModalStart} charCode="">Welcome to Worldview!</ModalHeader>
-          <ModalBody>
-            <TourIntro toggleModalStart={this.props.toggleModalStart}></TourIntro>
-            <TourBoxes stories={this.props.stories} storyOrder={this.props.storyOrder} selectTour={this.props.selectTour}></TourBoxes>
-          </ModalBody>
-        </Modal>
-      </div>
+      <Modal isOpen={this.props.modalStart} toggle={this.props.toggleModalStart} onClosed={this.props.showTourAlert} wrapClassName='tour tour-start' className={this.props.className} backdrop={true}>
+        <ModalHeader toggle={this.props.toggleModalStart} charCode="">Welcome to Worldview!</ModalHeader>
+        <ModalBody>
+          <TourIntro toggleModalStart={this.props.toggleModalStart}></TourIntro>
+          <TourBoxes stories={this.props.stories} storyOrder={this.props.storyOrder} selectTour={this.props.selectTour}></TourBoxes>
+        </ModalBody>
+        <ModalFooter>
+          <InputGroup>
+            <InputGroupText className="w-100">
+              <Input addon type="checkbox" className="float-right m-0" aria-label="Hide this box until a new story has been added." />
+              <span className="ml-2">Check the box to hide this modal until a new story has been added.</span>
+            </InputGroupText>
+          </InputGroup>
+        </ModalFooter>
+      </Modal>
     );
   }
 }
