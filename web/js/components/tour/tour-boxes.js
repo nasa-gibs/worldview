a156d9a9413ab2280af3e5e0dea4919110763963
ZachTRice <zachtrice@gmail.com>
2018-12-20 13:37:24 -0500

    Limit tour start modal to 9 stories

--- web/js/components/tour/tour-boxes.js
+++ web/js/components/tour/tour-boxes.js
@@ -8,7 +8,7 @@ class TourBoxes extends React.Component {
     return (
       <div className="tour-box-container">
         <div className="tour-box-row">
-          {storyOrder.map(function(storyId, i) {
+          {storyOrder.slice(0, 9).map(function(storyId, i) {
             let story = stories[storyId];
             return (<TourBox key={i} index={i} story={story} storyId={story.id} title={story.title} description={story.description} backgroundImage={story.backgroundImage} backgroundImageHover={story.backgroundImageHover} selectTour={selectTour} className={'tour-box ' + story.type}/>);
           })}
