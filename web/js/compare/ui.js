5c7e193c5192a61193caad269d284639082c9a13
Benjamin King <benking4@gmail.com>
2018-12-23 23:33:50 -0500

    update dialog content #1234

--- web/js/compare/ui.js
+++ web/js/compare/ui.js
@@ -4,8 +4,8 @@ const compareAlertBody =
   '<div class="compare-dialog">' +
   '<p>How to get started?</p>' +
   '<ul>' +
-  '<li class="compare-dialog-list-item"><p>Toggle between states by clicking the <b>A</b> or <b>B</b> tabs. Toggling a state on allows you to update the layers and date of that state.<p><img src="images/ab-tabs.png"/></li>' +
-  '<li class="compare-dialog-list-item"><p>There are now time sliders on the timeline. You can click on the deactivated time slider to toggle that state on and change the date.</p><img src="images/ab-picks.png"/></li>' +
+  '<li class="compare-dialog-list-item"><p>Select the respective tab (A or B) in order to update the layers and date of that state.<p><img src="images/ab-tabs.png"/></li>' +
+  '<li class="compare-dialog-list-item"><p>There are now two time sliders on the timeline. You can click on the deactivated time slider to activate that state and change the date.</p><img src="images/ab-picks.png"/></li>' +
   '<li class="compare-dialog-list-item"><p>There are three compare modes. You can choose different modes using the selection at the bottom of the layer list.</p><img src="images/ab-modes.png"/></li>' +
   '</ul></div>';
 
