384b06f90c3a64cef81118078799ecde45e637c5
Benjamin King <benking4@gmail.com>
2019-01-07 16:27:27 -0500

    add check to see if layers in event config are in layer config

--- tasks/validateOptions.py
+++ tasks/validateOptions.py
@@ -158,6 +158,13 @@ for startingLayer in wv["defaults"]["startingLayers"]:
 wv["defaults"]["startingLayers"] = startingLayers
 wv["buildDate"] = datetime.now().isoformat(" ")
 
+for projection, projectionValue in wv["naturalEvents"]["layers"].iteritems():
+    for eventType, eventTypeLayerList in projectionValue.iteritems():
+        for layerObject in eventTypeLayerList:
+            if layerObject[0] not in wv["layers"]:
+                error("The %s layer in the Natural events %s %s config does not have a matching ID in the layer config" %
+                (layerObject[0], projection, eventType))
+
 for measurement in wv["measurements"].values():
     if "sources" not in measurement:
         continue
