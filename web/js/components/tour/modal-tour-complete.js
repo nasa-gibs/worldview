65fe51f53ba70f7eb0fcb8dd208e863b7b7352e8
ZachTRice <zachtrice@gmail.com>
2019-01-09 03:23:01 -0500

    Adjust tour complete modal to open links in new tab and adjust read more li spacing

--- web/js/components/tour/modal-tour-complete.js
+++ web/js/components/tour/modal-tour-complete.js
@@ -12,7 +12,7 @@ class ModalComplete extends React.Component {
           <p>Read more about this story at the links below:</p>
           <ul>
             {readMoreLinks.map((linkId, i) =>
-              <li key={i} index={i}><a href={linkId.link}>{linkId.title}</a></li>
+              <li key={i} index={i}><a href={linkId.link} target="_blank">{linkId.title}</a></li>
             )}
           </ul>
         </React.Fragment>
@@ -23,7 +23,7 @@ class ModalComplete extends React.Component {
         <Modal isOpen={this.props.modalComplete} toggle={this.props.toggleModalComplete} wrapClassName='tour tour-complete' className={this.props.className} backdrop={true}>
           <ModalHeader toggle={this.props.toggleModalComplete} charCode="">Story Complete</ModalHeader>
           <ModalBody>
-            <p>You have now completed a story in Worldview. To view more stories, click the "More Stories" button below or, <a href="#">explore more events</a> within the app. Click the "Exit Tutorial" button or close this window to start using Worldview on your own.</p>
+            <p>You have now completed a story in Worldview. To view more stories, click the "More Stories" button below to explore more events within the app. Click the "Exit Tutorial" button or close this window to start using Worldview on your own.</p>
             {list}
           </ModalBody>
           <ModalFooter>
