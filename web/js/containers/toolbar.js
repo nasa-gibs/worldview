ed87a20754de8a2ecaa9f84f515902e26b4e0743
Benjamin King <benking4@gmail.com>
2019-01-14 19:18:17 -0500

    migrate toolbar to react/redux #1576

--- web/js/containers/toolbar.js
+++ web/js/containers/toolbar.js
@@ -2,28 +2,53 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { openCustomContent } from '../modules/modal/actions';
+import { ButtonToolbar, Button } from 'reactstrap';
 
 class LinksContainer extends Component {
   render() {
     const { openModal } = this.props;
     return (
-      <ul id="wv-toolbar">
-        <li id="wv-link-button" className="wv-toolbar-button" />
-        <li id="wv-proj-button" className="wv-toolbar-button" />
-        <li
+      <ButtonToolbar id="wv-toolbar" className="wv-toolbar">
+        <Button
+          id="wv-link-button"
+          className="wv-toolbar-button"
+          title="Share this map"
+          onClick={() => openModal('TOOLBAR_SHARE_LINK')}
+        >
+          <i className="fas fa-share-square fa-2x" />
+        </Button>
+        <Button
+          id="wv-proj-button"
+          className="wv-toolbar-button"
+          title="Switch projection"
+          onClick={() => openModal('TOOLBAR_PROJECTION')}
+        >
+          <i className="fas fa-globe-asia fa-2x" />{' '}
+        </Button>
+        <Button
           id="wv-image-button"
           className="wv-toolbar-button"
-          onClick={openModal}
-        />
-        <li id="wv-info-button" className="wv-toolbar-button wv-status-hide" />
-      </ul>
+          title="Take a snapshot"
+          onClick={() => openModal('TOOLBAR_SNAPSHOT')}
+        >
+          <i className="fa fa-camera fa-2x" />{' '}
+        </Button>
+        <Button
+          id="wv-info-button"
+          title="Information"
+          className="wv-toolbar-button wv-status-hide"
+          onClick={() => openModal('TOOLBAR_INFO')}
+        >
+          <i className="fa fa-info-circle fa-2x" />{' '}
+        </Button>
+      </ButtonToolbar>
     );
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  openModal: () => {
-    dispatch(openCustomContent('TOOLBAR-SNAPSHOT_MODAL'));
+  openModal: text => {
+    dispatch(openCustomContent(text));
   }
 });
 
@@ -31,3 +56,7 @@ export default connect(
   null,
   mapDispatchToProps
 )(LinksContainer);
+
+LinksContainer.propTypes = {
+  openModal: PropTypes.func
+};
