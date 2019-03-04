a77640ccdd3eb219ad5ecf423c1459eab0e125a0
minniewong <min.m.wong@nasa.gov>
2019-03-04 11:40:16 -0500

    Changed "the" to "a"

--- web/js/components/tour/content-intro.js
+++ web/js/components/tour/content-intro.js
@@ -6,7 +6,7 @@ class TourIntro extends React.Component {
     return (
       <div className="tour-intro">
         <p className="intro">
-          Visually explore the past and the present of this dynamic planet from the satellite's perspective.
+          Visually explore the past and the present of this dynamic planet from a satellite's perspective.
           Select from an array of stories below to learn more about Worldview, the satellite imagery we provide and events occurring around the world. <a href="#" title="Start using Worldview" onClick={this.props.toggleModalStart}>Start using Worldview <i className="fa fa-arrow-right" aria-hidden="true"></i></a>
         </p>
       </div>
