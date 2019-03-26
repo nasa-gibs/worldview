1b2cef2824f12620526cb7c5cebfcb93e6f0d2ab
ZachTRice <zachtrice@gmail.com>
2019-03-26 14:38:06 -0400

    Add point filter selector and refine current style rendering

--- web/js/map/layerbuilder.js
+++ web/js/map/layerbuilder.js
@@ -412,11 +412,45 @@ export function mapLayerBuilder(models, config, cache, mapUi, store) {
       var vectorStyle = def.vectorStyle.id;
       var glStyle = vectorStyles[vectorStyle];
 
-      // Set default styles
-      var styleFunction = stylefunction(layer, glStyle, 'default_style');
-
       $(document).ready(function() {
-        $(document).on('change', document.getElementById('confidenceCheckbox'), function(e) {
+        function getVals() {
+          // Get slider values
+          var parent = this.parentNode;
+          var slides = parent.getElementsByTagName('input');
+          var slide1 = parseFloat(slides[0].value);
+          var slide2 = parseFloat(slides[1].value);
+          // Neither slider will clip the other, so make sure we determine which is larger
+          if (slide1 > slide2) { var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+
+          var displayElement = parent.getElementsByClassName('rangeValues')[0];
+          displayElement.innerHTML = slide1 + ' - ' + slide2;
+        }
+
+        window.onload = function() {
+          // Initialize Sliders
+          var sliderSections = document.getElementsByClassName('range-slider');
+          for (var x = 0; x < sliderSections.length; x++) {
+            var sliders = sliderSections[x].getElementsByTagName('input');
+            for (var y = 0; y < sliders.length; y++) {
+              if (sliders[y].type === 'range') {
+                sliders[y].oninput = getVals;
+                // Manually trigger event first time to display values
+                sliders[y].oninput();
+              }
+            }
+          }
+
+          let confidenceMinFilter = document.getElementById('confidenceMinFilter');
+          let confidenceMaxFilter = document.getElementById('confidenceMaxFilter');
+
+          document.getElementById('confidenceMinFilterLabel').innerHTML = confidenceMinFilter.value;
+          document.getElementById('confidenceMaxFilterLabel').innerHTML = confidenceMaxFilter.value;
+        };
+
+        $(document).on('change', function(e) {
+          // Set default styles
+          var styleFunction = stylefunction(layer, glStyle, 'default_style');
+
           if (document.getElementById('frpCheckbox').checked === true) {
             styleFunction = stylefunction(layer, glStyle, 'MODIS_Fire_Points_FRP');
           } else if (document.getElementById('confidenceCheckbox').checked === true) {
@@ -424,9 +458,15 @@ export function mapLayerBuilder(models, config, cache, mapUi, store) {
           } else {
             styleFunction = stylefunction(layer, glStyle, 'default_style');
           }
+
+          let confidenceMinFilter = document.getElementById('confidenceMinFilter');
+          let confidenceMaxFilter = document.getElementById('confidenceMaxFilter');
+
+          document.getElementById('confidenceMinFilterLabel').innerHTML = confidenceMinFilter.value;
+          document.getElementById('confidenceMaxFilterLabel').innerHTML = confidenceMaxFilter.value;
           // Filter by a feature
           layer.setStyle(function(feature, resolution) {
-            if (feature.get('CONFIDENCE') >= '1' && feature.get('CONFIDENCE') <= '100') {
+            if (feature.get('CONFIDENCE') >= confidenceMinFilter.value && feature.get('CONFIDENCE') <= confidenceMaxFilter.value) {
               return styleFunction(feature, resolution);
             }
           });
