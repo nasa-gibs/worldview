14cdc20a9c6b2b2fc385f24c77249856d583f146
Benjamin King <benking4@gmail.com>
2019-06-06 18:30:34 -0400

    disable GIF when AB is active

--- web/js/components/animation-widget/gif-panel-grid.js
+++ web/js/components/animation-widget/gif-panel-grid.js
@@ -12,14 +12,14 @@ export class GifPanelGrid extends React.Component {
     var size = this.props.requestSize;
     if (!this.props.valid) {
       return (
-        <div id='gif-size' className='gif-size gif-size-invalid grid-child'>
-          <i className='fa fa-times fa-fw' />
+        <div id="gif-size" className="gif-size gif-size-invalid grid-child">
+          <i className="fa fa-times fa-fw" />
           <span>{this.props.maxGifSize + ' MB' + '~' + size + ' MB'}</span>
         </div>
       );
     } else {
       return (
-        <div id='gif-size' className='gif-size grid-child'>
+        <div id="gif-size" className="gif-size grid-child">
           <span>{this.props.maxGifSize + ' MB / ~' + size + ' MB'} </span>
         </div>
       );
@@ -28,24 +28,52 @@ export class GifPanelGrid extends React.Component {
   render() {
     let imageSize = this.renderImageSize();
     return (
-      <div className='gif-download-grid'>
-        <div className='grid-child label'><span>Start Date: </span></div>
-        <div className='grid-child'><span>{this.props.startDate} </span></div>
-        <div className='grid-child label'><span>End Date: </span></div>
-        <div className='grid-child'><span>{this.props.endDate}</span></div>
-        <div className='grid-child label'><span>Speed: </span></div>
-        <div className='grid-child'><span>{this.props.speed + ' Frames Per Second'}</span></div>
-        <div className='grid-child label'><span>Increment:</span></div>
-        <div className='grid-child'><span>{this.props.increment}</span></div>
-        <div className='grid-child label'><span>Max / Raw Size:</span></div>
+      <div className="gif-download-grid">
+        <div className="grid-child label">
+          <span>Start Date: </span>
+        </div>
+        <div className="grid-child">
+          <span>{this.props.startDate} </span>
+        </div>
+        <div className="grid-child label">
+          <span>End Date: </span>
+        </div>
+        <div className="grid-child">
+          <span>{this.props.endDate}</span>
+        </div>
+        <div className="grid-child label">
+          <span>Speed: </span>
+        </div>
+        <div className="grid-child">
+          <span>{this.props.speed + ' Frames Per Second'}</span>
+        </div>
+        <div className="grid-child label">
+          <span>Increment:</span>
+        </div>
+        <div className="grid-child">
+          <span>{this.props.increment}</span>
+        </div>
+        <div className="grid-child label">
+          <span>Max / Raw Size:</span>
+        </div>
         {imageSize}
-        <div className='grid-child label'><span>Max Dimension: </span></div>
-        <div className={this.props.valid ? 'grid-child gif-max-size' : 'grid-child gif-max-size gif-size-invalid'}>
+        <div className="grid-child label">
+          <span>Max Dimension: </span>
+        </div>
+        <div
+          className={
+            this.props.valid
+              ? 'grid-child gif-max-size'
+              : 'grid-child gif-max-size gif-size-invalid'
+          }
+        >
           <span>{this.props.maxImageDimensionSize + 'px'}</span>
         </div>
-        <div className='grid-child label'><span>Image Dimensions:</span></div>
-        <div className='grid-child' id='wv-image-width'>
-          <span>{this.props.width + 'px x ' + this.props.height + 'px' }</span>
+        <div className="grid-child label">
+          <span>Image Dimensions:</span>
+        </div>
+        <div className="grid-child" id="wv-image-width">
+          <span>{this.props.width + 'px x ' + this.props.height + 'px'}</span>
         </div>
       </div>
     );
@@ -60,7 +88,7 @@ GifPanelGrid.propTypes = {
   endDate: PropTypes.string,
   speed: PropTypes.number,
   valid: PropTypes.bool,
-  requestSize: PropTypes.number,
+  requestSize: PropTypes.string,
   fileSizeEstimate: PropTypes.number,
   increment: PropTypes.string
 };
