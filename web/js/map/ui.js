8c3ac6817d7a095197d0315544c3700071c6d49c
ZachTRice <zachtrice@gmail.com>
2019-06-25 13:21:49 -0400

    Fix vector style not applying when new date selected - Add reloadLayers() after SELECT_DATE in map.ui store

--- web/js/map/ui.js
+++ web/js/map/ui.js
@@ -133,6 +133,7 @@ export function mapui(models, config, store, ui) {
       case CALCULATE_RESPONSIVE_STATE:
         return onResize();
       case SELECT_DATE:
+        reloadLayers();
         updateDate();
     }
   };
