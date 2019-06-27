2357912143136821082ad4c40553d25b30d2e0d8
Benjamin King <benking4@gmail.com>
2019-06-27 16:46:22 -0400

    fix layer serialize #1755

--- web/js/location.js
+++ web/js/location.js
@@ -286,7 +286,7 @@ const getParameters = function(config, parameters) {
           const compareIsActive = get(state, 'compare.active');
           const isCompareA = get(state, 'compare.isCompareA');
           const activeLayersB = get(state, 'layers.activeB');
-          return !isCompareA && !compareIsActive
+          return !isCompareA && compareIsActive
             ? serializeLayers(activeLayersB, state, 'activeB')
             : serializeLayers(currentLayers, state, 'active');
         }
