3d4948f7095db8bed3ee09d70cd5eced4e45c0b8
Benjamin King <benking4@gmail.com>
2019-06-28 13:45:50 -0400

    add compare tests

---
+++ web/js/modules/compare/reducers.test.js
@@ -0,0 +1,29 @@
+import { initialCompareState, compareReducer } from './reducers';
+import * as CONSTANTS from './constants';
+
+test('CHANGE_STATE update active state and activeString', () => {
+  const response = compareReducer(initialCompareState, {
+    type: CONSTANTS.CHANGE_STATE
+  });
+  expect(initialCompareState.activeString).toEqual('active');
+  expect(initialCompareState.isCompareA).toBeTruthy();
+  expect(response.activeString).toEqual('activeB');
+  expect(response.isCompareA).toBeFalsy();
+});
+
+test('TOGGLE_ON_OFF toggles compare feature on and off', () => {
+  const response = compareReducer(initialCompareState, {
+    type: CONSTANTS.TOGGLE_ON_OFF
+  });
+  expect(initialCompareState.active).toBeFalsy();
+  expect(response.active).toBeTruthy();
+});
+
+test('CHANGE_MODE updates mode', () => {
+  const response = compareReducer(initialCompareState, {
+    type: CONSTANTS.CHANGE_MODE,
+    mode: 'new-mode'
+  });
+  expect(initialCompareState.mode).toEqual('swipe');
+  expect(response.mode).toEqual('new-mode');
+});
