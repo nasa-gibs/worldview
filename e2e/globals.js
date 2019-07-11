c01a71d45bd8cb6d3ec2748788c521a05f7272cc
Benjamin King <benking4@gmail.com>
2019-07-11 13:21:43 -0400

    fix event e2e tests

--- e2e/globals.js
+++ e2e/globals.js
@@ -7,10 +7,10 @@ module.exports = {
     yearlyResolutionTooltip: '#zoom-years',
     timelineSetToYears: '#current-zoom.zoom-years',
     modalCloseButton: '.modal-header .close',
-    notificationDismissButton: '.ui-dialog.notify-alert .fa-times',
+    notificationDismissButton: '.wv-alert .close-alert .fa-times',
     overlayLayerItems: '#overlays li',
     resolutionTooltip: '#zoom-btn-container',
-    notifyMessage: '.notify-alert .notify-message'
+    notifyMessage: '.wv-alert .alert-content'
   },
   delay: 3000, // Acceptable delay in ms between performing actions and seeing results
   timeout: 20000
