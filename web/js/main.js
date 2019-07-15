c19652d307fc63dedc08131f1b92f84173235a6b
Edward Plato <edplatomail@gmail.com>
2019-07-15 13:22:13 -0400

    add all es polyfils to handle future package/code change support for ie11

--- web/js/main.js
+++ web/js/main.js
@@ -1,16 +1,8 @@
 /* global DEBUG */
 // IE11 corejs polyfills container
-import 'core-js/fn/array/find';
-import 'core-js/fn/object/assign';
-import 'core-js/fn/object/values';
-import 'core-js/fn/object/keys';
-import 'core-js/fn/object/entries';
-import 'core-js/fn/string/includes';
-import 'core-js/fn/array/includes';
-import 'core-js/fn/number/is-nan';
-import 'core-js/fn/number/parse-float';
-import 'core-js/es6/symbol';
-import 'core-js/es6/promise';
+import 'core-js/es5';
+import 'core-js/es6';
+import 'core-js/es7';
 import 'regenerator-runtime/runtime';
 // IE11 corejs polyfills container
 import 'whatwg-fetch';
