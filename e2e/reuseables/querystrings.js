4f5fc4b9895c21251c4800c133a161f65b804543
Edward Plato <edplatomail@gmail.com>
2019-07-24 14:44:23 -0400

    add timeline e2e tests

--- e2e/reuseables/querystrings.js
+++ e2e/reuseables/querystrings.js
@@ -14,6 +14,9 @@ module.exports = {
   swipeAOD:
     '?ca=true&cm=swipe&cv=51&p=geographic&l=MODIS_Terra_CorrectedReflectance_TrueColor,MODIS_Terra_Aerosol&l1=VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor,Reference_Labels(hidden),Reference_Features(hidden),Coastlines&t=2018-08-17-T00%3A00%3A00Z&z=3&t1=2018-08-16-T00%3A00%3A00Z&v=-127.6171875,-46.5556640625,31.7109375,53.0244140625',
 
+  // date
+  knownDate: '?t=2019-07-22',
+
   // events
   eventsTabActive: '?e=true',
   mockEvents: '?p=geographic&l=VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor,Reference_Labels(hidden),Reference_Features(hidden),Coastlines&t=2018-05-02-T00%3A00%3A00Z&z=3&v=-409.00147812273656,-205.62883007565202,270.5880270080828,219.11461063111003&e=true&mockEvents=20170530',
