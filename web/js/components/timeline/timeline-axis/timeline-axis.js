8be1e2a7eccd7501d7170b6a45819e0e7774ff02
Edward Plato <edplatomail@gmail.com>
2019-07-26 10:53:24 -0400

    center dragger on axis init load

--- web/js/components/timeline/timeline-axis/timeline-axis.js
+++ web/js/components/timeline/timeline-axis/timeline-axis.js
@@ -461,7 +461,7 @@ class TimelineAxis extends Component {
       axisWidth
     } = this.props;
     let time = draggerSelected === 'selected' ? draggerTimeState : draggerTimeStateB;
-    this.updateScale(time, timeScale, axisWidth, 0.80);
+    this.updateScale(time, timeScale, axisWidth, 0.50);
   }
 
   componentDidUpdate(prevProps) {
