7ca8177e729cfe1457d0d67230fc581276188af1
Jason Kent <jasontkent@gmail.com>
2019-09-06 10:29:37 -0400

    collapse sidebar when measuring is active

--- web/js/modules/measure/actions.js
+++ web/js/modules/measure/actions.js
@@ -1,4 +1,4 @@
-import { CHANGE_UNITS, USE_GREAT_CIRCLE } from './constants';
+import { CHANGE_UNITS, USE_GREAT_CIRCLE, TOGGLE_MEASURE_ACTIVE } from './constants';
 
 export function changeUnits(units) {
   return {
@@ -10,5 +10,12 @@ export function useGreatCircle(value) {
   return {
     type: USE_GREAT_CIRCLE,
     value
+  };
+}
+
+export function toggleMeasureActive(isActive) {
+  return {
+    type: TOGGLE_MEASURE_ACTIVE,
+    value: isActive
   };
 }
