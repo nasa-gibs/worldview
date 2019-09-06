617e7e6552bab7e7112d28576da589a243e72453
Jason Kent <jasontkent@gmail.com>
2019-09-06 11:50:43 -0400

    show alert for measure tool instructions, remove help tooltip

--- web/js/components/measure-tool/measure-button.js
+++ web/js/components/measure-tool/measure-button.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Button } from 'reactstrap';
 import MeasureMenu from './measure-menu';
 import { openCustomContent } from '../../modules/modal/actions';
+import AlertUtil from '../util/alert';
 
 const MEASURE_MENU_PROPS = {
   headerText: null,
@@ -15,29 +16,70 @@ const MEASURE_MENU_PROPS = {
   wrapClassName: 'toolbar_modal_outer'
 };
 
+const mobileHelpMsg = 'Tap to add a point. Double-tap to complete.';
+const helpMsg = 'Click: Add a point. Right-click: Cancel. Double-click to complete. ';
+
 class MeasureButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAlert: true,
+      isTouchDevice: false
+    };
+    this.onButtonClick = this.onButtonClick.bind(this);
+    this.dismissAlert = this.dismissAlert.bind(this);
+  }
+
   onButtonClick (evt) {
     const { openModal } = this.props;
     MEASURE_MENU_PROPS.clickType = evt.type;
     evt.stopPropagation();
+    evt.preventDefault();
     openModal('MEASURE_MENU', MEASURE_MENU_PROPS);
+    this.setState({
+      isTouchDevice: evt.type === 'touchend',
+      showAlert: true
+    });
+  }
+
+  dismissAlert() {
+    this.setState({ showAlert: false });
   }
 
   render() {
+    const showAlert = this.props.isActive && this.state.showAlert;
+    const message = this.state.isTouchDevice ? mobileHelpMsg : helpMsg;
+
     return (
-      <Button
-        id="wv-measure-button"
-        className="wv-measure-button wv-toolbar-button"
-        title="Measure distances &amp; areas"
-        onTouchEnd={this.onButtonClick.bind(this)}
-        onMouseDown={this.onButtonClick.bind(this)}
-      >
-        <i className="fas fa-ruler fa-2x"></i>{' '}
-      </Button>
+      <>
+        {showAlert && <AlertUtil
+          isOpen={true}
+          iconClassName='fa fa-ruler fa-fw'
+          title='Measure Tool'
+          message={message}
+          onDismiss={this.dismissAlert}
+        />}
+
+        <Button
+          id="wv-measure-button"
+          className="wv-measure-button wv-toolbar-button"
+          title="Measure distances &amp; areas"
+          onTouchEnd={this.onButtonClick}
+          onMouseDown={this.onButtonClick}
+        >
+          <i className="fas fa-ruler fa-2x"></i>{' '}
+        </Button>
+      </>
     );
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    isActive: state.measure.isActive,
+    browser: state.browser
+  };
+};
 const mapDispatchToProps = dispatch => ({
   openModal: (key, customParams) => {
     dispatch(openCustomContent(key, customParams));
@@ -45,10 +87,12 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(MeasureButton);
 
 MeasureButton.propTypes = {
+  browser: PropTypes.object,
+  isActive: PropTypes.bool,
   openModal: PropTypes.func
 };
