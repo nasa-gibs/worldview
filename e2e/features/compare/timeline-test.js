116ed085d32543f0cb938e5171577b6f3b45917d
Benjamin King <benking4@gmail.com>
2019-09-12 12:17:34 -0400

    fix timeline compare e2e tests #1964

--- e2e/features/compare/timeline-test.js
+++ e2e/features/compare/timeline-test.js
@@ -8,71 +8,71 @@ const dateSelectorMonthInput = '#date-selector-main .input-wrapper-month input';
 const TIME_LIMIT = 20000;
 
 module.exports = {
-  before: function(client) {
+  before: function (client) {
     reuseables.loadAndSkipTour(client, TIME_LIMIT);
   },
   // load A|B and verify that it is active
-  'A|B is loaded': function(client) {
+  'A|B is loaded': function (client) {
     client.url(client.globals.url + localQuerystrings.swipeAndAIsActive);
     client.waitForElementVisible(localSelectors.swipeDragger, TIME_LIMIT);
   },
-  'Verify that A|B draggers are visible': function(client) {
+  'Verify that A|B draggers are visible': function (client) {
     client.expect.element(draggerA).to.be.visible;
     client.expect.element(draggerB).to.be.visible;
   },
-  'Dragging active dragger updates date': function(client) {
+  'Dragging active dragger updates date': function (client) {
     client.assert.attributeContains(dateSelectorDayInput, 'value', '17');
     client.assert.attributeContains(dateSelectorMonthInput, 'value', 'AUG');
 
     client
       .useCss()
-      .moveToElement(draggerA, -20, 20)
+      .moveToElement(draggerA, 30, 30)
       .mouseButtonDown(0)
-      .moveToElement(draggerB, -100, 0)
+      .moveToElement(draggerB, 100, 30)
       .mouseButtonUp(0)
       .pause(2000);
-    client.getValue(dateSelectorDayInput, function(dayResult) {
-      client.getValue(dateSelectorMonthInput, function(monthResult) {
+    client.getValue(dateSelectorDayInput, function (dayResult) {
+      client.getValue(dateSelectorMonthInput, function (monthResult) {
         var result = monthResult.value.concat(dayResult.value);
         this.assert.notEqual('AUG17', result);
       });
     });
   },
-  'Clicking inactive dragger updates active state': function(client) {
+  'Clicking inactive dragger updates active state': function (client) {
     client.assert.cssClassPresent(localSelectors.aTab, 'active');
     client
       .useCss()
-      .moveToElement(draggerB, -20, 20)
+      .moveToElement(draggerB, 20, 20)
       .mouseButtonDown(0)
       .mouseButtonUp(0);
     // Reference labels were not active in A but are in B
     client.waitForElementVisible(
       '#activeB-Reference_Features',
       TIME_LIMIT,
-      function() {
+      function () {
         client.assert.attributeContains(dateSelectorDayInput, 'value', '16');
       }
     );
   },
-  'Dragging B dragger updates date in label': function(client) {
+  'Dragging B dragger updates date in label': function (client) {
     client.useCss().assert.containsText(localSelectors.bTab, '2018-08-16');
     client
       .useCss()
-      .moveToElement(draggerB, -20, 20)
+      .moveToElement(draggerB, 20, 20)
       .mouseButtonDown(0)
       .moveToElement(draggerA, -100, 0)
       .mouseButtonUp(0)
       .pause(2000);
-    client.getText(localSelectors.bTab, function(result) {
+    client.getText(localSelectors.bTab, function (result) {
       this.assert.notEqual('B: 2018-08-16', result.value);
     });
   },
-  'Deactivate A|B is no longer active': function(client) {
+  'Deactivate A|B is no longer active': function (client) {
     client.click(localSelectors.compareButton);
     client.waitForElementNotPresent(
       localSelectors.bTab,
       TIME_LIMIT,
-      function() {
+      function () {
         client
           .useCss()
           .assert.containsText(
@@ -84,7 +84,7 @@ module.exports = {
       }
     );
   },
-  after: function(client) {
+  after: function (client) {
     client.end();
   }
 };
