8da2d051a4c833043764e44e5a5e306f58223f39
Benjamin King <benking4@gmail.com>
2019-09-13 16:37:39 -0400

    wip test click func

---
+++ e2e/reuseables/double-click.js
@@ -0,0 +1,67 @@
+module.exports = {
+  customMouseEvent: function (client, selector, offsetX, offsetY, eventType) {
+    console.log(selector, offsetX, offsetY, eventType)
+    eventType = eventType || "dblclick";
+    client.execute(function (obj) {
+      var { eventType, selector, offsetX, offsetY } = obj;
+      var el = document.querySelector(selector || 'body');
+      function simulate(element, eventName) {
+        var options = extend(defaultOptions, arguments[2] || {});
+        var oEvent, eventType = null;
+
+        for (var name in eventMatchers) {
+          if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+        }
+
+        if (!eventType)
+          throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+        if (document.createEvent) {
+          oEvent = document.createEvent(eventType);
+          if (eventType == 'HTMLEvents') {
+            oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+          }
+          else {
+            oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+              options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+              options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+          }
+          element.dispatchEvent(oEvent);
+        }
+        else {
+          options.clientX = options.pointerX;
+          options.clientY = options.pointerY;
+          var evt = document.createEventObject();
+          oEvent = extend(evt, options);
+          element.fireEvent('on' + eventName, oEvent);
+        }
+        return element;
+      }
+
+      function extend(destination, source) {
+        for (var property in source)
+          destination[property] = source[property];
+        return destination;
+      }
+
+      var eventMatchers = {
+        'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+        'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+      }
+      var defaultOptions = {
+        pointerX: 0,
+        pointerY: 0,
+        button: 0,
+        ctrlKey: false,
+        altKey: false,
+        shiftKey: false,
+        metaKey: false,
+        bubbles: true,
+        cancelable: true
+      }
+      return simulate(el, eventType, { pointerX: offsetX, pointerY: offsetY });
+    }, [{ eventType: eventType, selector: selector, offsetX: offsetX, offsetY: offsetY }], function (result) {
+      console.log(result)
+    })
+  }
+};
