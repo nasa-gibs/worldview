6ca4725a6bfd6ad195843b13c5c76360f751d41e
Benjamin King <benking4@gmail.com>
2019-10-11 14:46:24 -0500

    Prevent vector-data table from rerendering if props don't change #2144

--- web/js/components/vector-metadata/table.js
+++ web/js/components/vector-metadata/table.js
@@ -7,18 +7,22 @@ import { find as lodashFind } from 'lodash';
 export default class VectorMetaTable extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       tooltipOpen: false,
       metaFeatures: this.props.metaFeatures,
       metaLegend: this.props.metaLegend
     };
   }
-
+  shouldComponentUpdate(nextProps) {
+    if (this.props.metaLegend && nextProps.metaLegend && this.props.metaLegend.id === nextProps.metaLegend.id) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   render() {
     var header, data;
     const { metaFeatures, metaLegend } = this.state;
-
     return (
       <Table size="sm">
         <tbody>
