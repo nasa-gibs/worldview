4faa344b161ce299092e82f4106f9e38ef9ace44
Benjamin King <benking4@gmail.com>
2019-11-05 18:39:42 -0600

    use valuemap if exists #2144

--- web/js/components/vector-metadata/table.js
+++ web/js/components/vector-metadata/table.js
@@ -40,7 +40,8 @@ export default class VectorMetaTable extends React.Component {
                 <tbody>
                   {metaLegend.map((properties, index) => {
                     const featureId = properties.Identifier;
-                    const value = metaFeatures[featureId];
+                    const hasValueMap = !!properties.ValueMap;
+                    const value = properties.ValueMap ? properties.ValueMap[metaFeatures[featureId]] : metaFeatures[featureId];
                     const id = util.cleanId(String(title + '-' + (metaIndex + index)));
                     if (!value) return undefined;
                     return (
