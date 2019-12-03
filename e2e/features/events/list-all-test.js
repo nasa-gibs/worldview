76c4da38cf546824628aeeecea2cb0cdab268743
Jason Kent <jasontkent@gmail.com>
2019-12-03 16:52:12 -0500

    fix e2e tests

--- e2e/features/events/list-all-test.js
+++ e2e/features/events/list-all-test.js
@@ -1,6 +1,9 @@
 const reuseables = require('../../reuseables/skip-tour.js');
 const localQuerystrings = require('../../reuseables/querystrings.js');
 const TIME_LIMIT = 10000;
+const layersTab = '#layers-sidebar-tab';
+const eventsTab = '#events-sidebar-tab';
+
 module.exports = {
   before: function(client) {
     reuseables.loadAndSkipTour(client, TIME_LIMIT);
@@ -12,18 +15,17 @@ module.exports = {
       TIME_LIMIT,
       function() {
         client.click('#sidebar-event-EONET_3931');
+        client.click(layersTab);
         client.waitForElementPresent(
           '#active-VIIRS_SNPP_Thermal_Anomalies_375m_Night',
           TIME_LIMIT,
           function() {
+            client.click(eventsTab);
             client.expect.element('#sidebar-event-EONET_2703').to.be.visible;
-            client.expect.element('ul#wv-eventscontent > li:nth-child(9)').to.be
-              .visible;
+            client.expect.element('ul#wv-eventscontent > li:nth-child(9)').to.be.visible;
             client.click('#events-footer-checkbox').pause(1000);
-            client.expect.element('#sidebar-event-EONET_2703').to.not.be
-              .visible;
-            client.expect.element('ul#wv-eventscontent > li:nth-child(2)').to
-              .not.be.visible;
+            client.expect.element('#sidebar-event-EONET_2703').to.not.be.visible;
+            client.expect.element('ul#wv-eventscontent > li:nth-child(2)').to.not.be.visible;
           }
         );
       }
