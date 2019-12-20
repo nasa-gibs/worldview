72da7cccc54efccf2cf6cab425631b1df5acbbba
Benjamin King <benking4@gmail.com>
2019-12-20 12:16:55 -0500

    fix lint error

--- web/js/containers/map-interactions.js
+++ web/js/containers/map-interactions.js
@@ -57,7 +57,7 @@ export class MapInteractions extends React.Component {
     const hasFeatures = map.hasFeatureAtPixel(pixels);
     if (hasFeatures && !isShowingClick && !measureIsActive) {
       let isActiveLayer = false;
-      map.forEachFeatureAtPixel(pixels, function (feature, layer) {
+      map.forEachFeatureAtPixel(pixels, function(feature, layer) {
         if (!layer) {
           return;
         }
