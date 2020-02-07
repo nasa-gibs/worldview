bd2e12d74e19957b3b2105c1ccc140611e9347f8
Jason Kent <jasontkent@gmail.com>
2020-02-07 12:08:38 -0500

    self contained filterUnavailable component for reuse

--- web/js/components/layer/product-picker/layer-filters.js
+++ web/js/components/layer/product-picker/layer-filters.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment';
-import Switch from '../../util/switch';
+import FilterUnavailable from './filterUnavailable';
 
 /*
  * A scrollable list of layers
@@ -14,25 +13,16 @@ const LayerFilters = (props) => {
     filterByAvailable,
     toggleFilterByAvailable
   } = props;
-  const diplayDate = moment.utc(selectedDate).format('YYYY MMM DD');
-  const tooltipContent =
-    <div className="filter-tooltip">
-      If enabled, only show results which would be visible
-      at the currently selected date: <br />
-      <div className="display-date"> {diplayDate} </div>
-    </div>;
 
   return (
     <div className="layer-filters-container">
       <div className="filter-controls">
         <h3>Filters</h3>
-        <Switch
-          id="unavailable-toggle"
-          label="Hide unavailable"
-          active={filterByAvailable}
-          toggle={toggleFilterByAvailable}
-          tooltip={tooltipContent}>
-        </Switch>
+        <FilterUnavailable
+          selectedDate={selectedDate}
+          filterByAvailable={filterByAvailable}
+          toggleFilterByAvailable={toggleFilterByAvailable}>
+        </FilterUnavailable>
       </div>
     </div>
   );
