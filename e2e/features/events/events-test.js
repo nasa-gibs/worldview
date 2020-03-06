1347fee2f662b1bcc76c6e0420f539a3e35c07bf
Jason Kent <jasontkent@gmail.com>
2020-03-06 15:44:12 -0500

    fix a few bugs and get e2e tests passing

--- e2e/features/events/events-test.js
+++ e2e/features/events/events-test.js
@@ -18,9 +18,7 @@ module.exports = {
   before(client) {
     reuseables.loadAndSkipTour(client, TIME_LIMIT);
   },
-  'Make sure that 4 fire layers are not present in layer list: use mock': function(
-    client,
-  ) {
+  'Make sure that 4 fire layers are not present in layer list: use mock': (client) => {
     client.url(client.globals.url + localQuerystrings.mockEvents);
     client.waitForElementVisible(
       '#sidebar-event-EONET_3931',
@@ -37,10 +35,10 @@ module.exports = {
       },
     );
   },
-  'Click fire event': function(client) {
+  'Click fire event': (client) => {
     client.click('#sidebar-event-EONET_3931');
   },
-  'Check that 4 fire layers are now present': function(client) {
+  'Check that 4 fire layers are now present': (client) => {
     client.click(layersTab);
     client.waitForElementPresent(
       '#active-VIIRS_SNPP_Thermal_Anomalies_375m_Night',
@@ -55,25 +53,21 @@ module.exports = {
       },
     );
   },
-  'Use Mock to make sure appropriate number of event markers are appended to map': function(
-    client,
-  ) {
+  'Use Mock to make sure appropriate number of event markers are appended to map': (client) => {
     client.url(client.globals.url + localQuerystrings.mockEvents);
     client.waitForElementVisible(listOfEvents, TIME_LIMIT, () => {
-      client.assert.elementCountEquals(eventIcons, 9);
+      client.expect.elements(eventIcons).count.to.equal(9);
     });
   },
-  'On events tab click events list is loaded': function(client) {
+  'On events tab click events list is loaded': (client) => {
     client.url(client.globals.url + localQuerystrings.mockEvents);
     client.waitForElementVisible(listOfEvents, TIME_LIMIT);
   },
-  'Use Mock Ensure number of event track points is correct and event markers and tabs are not visible when layer tab is clicked': function(
-    client,
-  ) {
+  'Use Mock Ensure number of event track points is correct and event markers and tabs are not visible when layer tab is clicked': (client) => {
     const globalSelectors = client.globals.selectors;
     client.click(firstEvent);
     client.waitForElementVisible(trackMarker, TIME_LIMIT, () => {
-      client.assert.elementCountEquals(trackMarker, 5);
+      client.expect.elements(trackMarker).count.to.equal(5);
       client.click(globalSelectors.dataTab).pause(2000);
       client.expect.element(trackMarker).to.not.be.present;
       client.expect.element(eventIcons).to.not.be.present;
@@ -82,7 +76,7 @@ module.exports = {
       client.expect.element(eventIcons).to.be.present;
     });
   },
-  'Click Events tab and select an Event from the List': function(client) {
+  'Click Events tab and select an Event from the List': (client) => {
     client.url(client.globals.url + localQuerystrings.mockEvents);
     client.waitForElementVisible(listOfEvents, TIME_LIMIT, () => {
       client.click(firstEvent);
@@ -91,16 +85,14 @@ module.exports = {
       });
     });
   },
-  'Verify that Url is updated': function(client) {
+  'Verify that Url is updated': (client) => {
     client.assert
       .urlParameterEquals('l', true)
       .assert.urlParameterEquals('t', true)
       .assert.urlParameterEquals('v', true)
       .assert.urlParameterEquals('e', true);
   },
-  'Verify Events may not be visible at all times is visible ': function(
-    client,
-  ) {
+  'Verify Events may not be visible at all times is visible ': (client) => {
     const globalSelectors = client.globals.selectors;
     client.waitForElementVisible(
       globalSelectors.notifyMessage,
@@ -113,7 +105,7 @@ module.exports = {
       },
     );
   },
-  'Clicking event notifcation opens explanation in dialog': function(client) {
+  'Clicking event notifcation opens explanation in dialog': (client) => {
     const globalSelectors = client.globals.selectors;
 
     client.click(globalSelectors.notifyMessage).pause(2000);
@@ -127,11 +119,11 @@ module.exports = {
     client.expect.element(globalSelectors.notificationDismissButton).to.not.be
       .present;
   },
-  'Clicking selected event deselects event': function(client) {
+  'Clicking selected event deselects event': (client) => {
     client.click(selectedFirstEvent).pause(500);
     client.expect.element(selectedFirstEvent).to.not.be.present;
   },
-  'Check that clicking eternal link opens in new window': function(client) {
+  'Check that clicking eternal link opens in new window': (client) => {
     client.click(firstEvent).pause(500);
     client.windowHandles((tabs) => {
       client.assert.equal(tabs.value.length, 1);
--- e2e/features/timeline/date-selector-test.js
+++ e2e/features/timeline/date-selector-test.js
@@ -39,11 +39,13 @@ module.exports = {
 
   // verify default left arrow enabled since loaded on current day
   'Left timeline arrow will not be disabled by default': (client) => {
+    reuseables.loadAndSkipTour(client, TIME_LIMIT);
     client.assert.cssClassNotPresent('#left-arrow-group', 'button-disabled');
   },
 
   // verify default right arrow disabled since loaded on current day
   'Right timeline arrow will be disabled by default': (client) => {
+    reuseables.loadAndSkipTour(client, TIME_LIMIT);
     client.assert.cssClassPresent('#right-arrow-group', 'button-disabled');
   },
 
