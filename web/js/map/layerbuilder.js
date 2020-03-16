286c3c8e5e4ece48201429252264dbf4b2dc5afa
Ed Plato <edplatomail@gmail.com>
2020-03-16 13:20:21 -0400

    remove utc time zeroing from getting request dates

--- web/js/map/layerbuilder.js
+++ web/js/map/layerbuilder.js
@@ -183,11 +183,7 @@ export function mapLayerBuilder(models, config, cache, ui, store) {
     if (def.period === 'subdaily') {
       date = nearestInterval(def, date);
     } else if (previousDateFromRange) {
-      date = util.clearTimeUTC(previousDateFromRange);
-    } else {
-      date = options.date
-        ? util.clearTimeUTC(new Date(date.getTime()))
-        : util.clearTimeUTC(date);
+      date = previousDateFromRange;
     }
 
     return { closestDate: date, previousDate: previousLayerDate, nextDate: nextLayerDate };
