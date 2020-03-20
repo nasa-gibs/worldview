4b5faa5fed52c9f5f2a28c3fdaeaf2e3b7f160b7
ZachTRice <zachtrice@gmail.com>
2020-03-20 10:50:25 -0400

    Use python to invoke pip and add --user flag

--- tasks/pythonInstall.sh
+++ tasks/pythonInstall.sh
@@ -6,20 +6,23 @@ if [ $1 = "linux" ]; then
     echo "Installing virtualenv with python3"
     python3 -m pip install --user virtualenv
     python3 -m virtualenv .python
-    PATH=.python/bin:${PATH} pip install -r requirements.txt
+    PATH=.python/bin:${PATH} python3 -m pip install --user -r requirements.txt
   elif [[ $PYTHON_VERSION = *"Python 3"* ]]; then
     echo "Installing virtualenv with python3"
     python -m pip install --user virtualenv
     python -m virtualenv .python
-    PATH=.python/bin:${PATH} pip install -r requirements.txt
+    PATH=.python/bin:${PATH} python -m pip install --user -r requirements.txt
   else
     echo "Please install or update python v3"
   fi
 else
   # If is a window machine use virtualenv
-    if command -v python3 &>/dev/null || [[ $PYTHON_VERSION = *"Python 3"* ]]; then
+    if command -v python3 &>/dev/null; then
       virtualenv .python
-      PATH=.python/Scripts:${PATH} pip install -r requirements.txt
+      PATH=.python/Scripts:${PATH} python3 -m pip install --user -r requirements.txt
+    elif [[ $PYTHON_VERSION = *"Python 3"* ]]; then
+      virtualenv .python
+      PATH=.python/Scripts:${PATH} python -m pip install --user -r requirements.txt
     else
       echo "Please install or update python v3"
     fi
