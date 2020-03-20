7e5a2bb9699ed72b5b265363d2503d7380a281f5
ZachTRice <zachtrice@gmail.com>
2020-03-20 12:09:03 -0400

    Fix echo message

--- tasks/pythonInstall.sh
+++ tasks/pythonInstall.sh
@@ -13,7 +13,7 @@ if command -v python3 &>/dev/null; then
   python3 -m virtualenv .python
   PATH=${PYPATH} python3 -m pip install -r requirements.txt
 elif [[ $PYTHON_VERSION = *"Python 3"* ]]; then
-  echo "Installing virtualenv with python3"
+  echo "Installing virtualenv with python"
   python -m pip install --user virtualenv
   python -m virtualenv .python
   PATH=${PYPATH} python -m pip install -r requirements.txt
