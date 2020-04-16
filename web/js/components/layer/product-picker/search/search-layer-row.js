6d6b5f7f9341b9d0809c9c3a8b016e12463f0352
Jason Kent <jasontkent@gmail.com>
2020-04-16 10:24:21 -0400

    resolve title splitting issue
    * consolidate layer title splitting code into a re-usable component
    * make sure we aren't adding orbit track asc/desc day/night to every layer title

--- web/js/components/layer/product-picker/search/search-layer-row.js
+++ web/js/components/layer/product-picker/search/search-layer-row.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { getOrbitTrackTitle } from '../../../../modules/layers/util';
 import {
   addLayer as addLayerAction,
   removeLayer as removeLayerAction,
 } from '../../../../modules/layers/actions';
 import { getActiveLayers } from '../../../../modules/layers/selectors';
+import RenderSplitLayerTitle from '../renderSplitTitle';
 
 /**
  * A single layer search result row
@@ -55,53 +55,28 @@ class SearchLayerRow extends React.Component {
     }
   }
 
-  /**
-   * Split the layer name and details (which are found in parentheses)
-   * onto separate lines
-   *
-   * @param {*} title - the full layer title
-   */
-  renderSplitTitle = (title) => {
-    const splitIdx = title.indexOf('(');
-    const attrs = title.slice(splitIdx);
-    const titleName = title.slice(0, splitIdx - 1);
-    return splitIdx < 0
-      ? (<h3>{title}</h3>)
-      : (
-        <>
-          <h3>{titleName}</h3>
-          <h4>{attrs}</h4>
-        </>
-      );
-  }
-
   render() {
     const { isEnabled, layer, selectedLayer } = this.props;
-    const isMetadataShowing = selectedLayer && layer.id === selectedLayer.id;
-    const {
-      title, track, description, subtitle,
-    } = layer;
-    const layerTitle = !track ? title : `${title} (${getOrbitTrackTitle(layer)})`;
+    const { id, description } = layer;
+    const isMetadataShowing = selectedLayer && id === selectedLayer.id;
     const rowClass = isMetadataShowing
       ? 'search-row layers-all-layer selected'
       : 'search-row layers-all-layer';
     const checkboxClass = isEnabled ? 'wv-checkbox checked' : 'wv-checkbox';
 
     return (
-      <div id={`${layer.id}-search-row`} className={rowClass}>
+      <div id={`${id}-search-row`} className={rowClass}>
         <div className={checkboxClass}>
           <input
             type="checkbox"
-            id={`${layer.id}-checkbox`}
-            title={title}
-            name={`${layer.id}-checkbox`}
+            id={`${id}-checkbox`}
+            name={`${id}-checkbox`}
             checked={isEnabled}
             onChange={this.toggleEnabled}
           />
         </div>
         <div className="layers-all-header" onClick={this.toggleShowMetadata}>
-          {!track ? this.renderSplitTitle(layerTitle) : <h3>{layerTitle}</h3>}
-          {subtitle && <h5>{subtitle}</h5>}
+          <RenderSplitLayerTitle layer={layer} />
           {description && !isMetadataShowing && (
             <FontAwesomeIcon icon={faInfoCircle} />
           )}
@@ -112,7 +87,6 @@ class SearchLayerRow extends React.Component {
 }
 SearchLayerRow.propTypes = {
   isEnabled: PropTypes.bool,
-  isMobile: PropTypes.bool,
   layer: PropTypes.object,
   addLayer: PropTypes.func,
   removeLayer: PropTypes.func,
@@ -121,11 +95,9 @@ SearchLayerRow.propTypes = {
 };
 
 const mapStateToProps = (state, ownProps) => {
-  const { browser, date, productPicker } = state;
+  const { date, productPicker } = state;
   const activeLayerMap = getActiveLayers(state);
-  const isMobile = browser.lessThan.medium;
   return {
-    isMobile,
     isEnabled: !!activeLayerMap[ownProps.layer.id],
     selectedDate: date.selected,
     selectedLayer: productPicker.selectedLayer,
