febaa294608e20d96d396a55786d05793d527a52
ibamibrhm <ibrhm.ibam@gmail.com>
2020-10-05 15:54:00 +0700

    Fix error loading metadata with empty body

--- web/js/components/layer/product-picker/search/search-layers-list.js
+++ web/js/components/layer/product-picker/search/search-layers-list.js
@@ -101,7 +101,7 @@ class SearchLayerList extends React.Component {
           // Check that we have a metadata html snippet, rather than a fully
           // formed HTML file. Also avoid executing any script or style tags.
           const isMetadataSnippet = !body.match(/<(head|body|html|style|script)[^>]*>/i);
-          layer.metadata = isMetadataSnippet ? body : errorMessage;
+          layer.metadata = isMetadataSnippet || !body.length ? body : errorMessage;
           selectLayer(layer);
         });
     } else {
@@ -140,7 +140,7 @@ class SearchLayerList extends React.Component {
     }
   }
 
-  renderNoResults () {
+  renderNoResults() {
     const { recentLayerMode } = this.props;
     return recentLayerMode
       ? (<RecentLayersInfo />)
