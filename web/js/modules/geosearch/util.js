a06f1191182d50cbaf083e7c95054e5dc8313992
Ed Plato <edplatomail@gmail.com>
2020-10-07 21:39:12 -0400

    add isExpanded bool maplocation check based on browser size

--- web/js/modules/geosearch/util.js
+++ web/js/modules/geosearch/util.js
@@ -8,14 +8,18 @@ import update from 'immutability-helper';
 export default function mapLocationToGeosearchState(
   parameters,
   stateFromLocation,
+  state,
 ) {
-  if (parameters.marker) {
-    stateFromLocation = update(stateFromLocation, {
-      geosearch: {
-        coordinates: { $set: parameters.marker.split(',') },
-      },
-    });
-  }
+  const { marker } = parameters;
+  const coordinates = marker
+    ? marker.split(',')
+    : [];
+  stateFromLocation = update(stateFromLocation, {
+    geosearch: {
+      coordinates: { $set: coordinates },
+      isExpanded: { $set: !state.browser.lessThan.medium },
+    },
+  });
 
   return stateFromLocation;
 }
