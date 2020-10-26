954c3ed4ae3c6232227b0712a10cd6586f606be9
Ed Plato <edplatomail@gmail.com>
2020-10-26 16:04:29 -0400

    add ol cooridnates marker map interactions coordinates dialog and util

---
+++ web/js/components/geosearch/coordinates-dialog.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-restricted-syntax */
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+class CoordinatesDialog extends PureComponent {
+  render() {
+    const {
+      toggleWithClose, coordinatesMetadata,
+    } = this.props;
+
+    const {
+      features, // { latitude, longitude }
+      title, // "Wairau Valley"
+    } = coordinatesMetadata;
+
+    const latitudeText = `Latitude: ${features.latitude} °`;
+    const longitudeText = `Longitude: ${features.longitude} °`;
+    return (
+      <div className="tooltip-custom-black tooltip-static">
+        <div className="tooltip-coordinates-title">{title}</div>
+        <div className="tooltip-coordinates-group">
+          <div>
+            {latitudeText}
+          </div>
+          <div>
+            {longitudeText}
+          </div>
+        </div>
+        <span className="close-tooltip coordinates-dialog-close" onClick={toggleWithClose}>
+          <FontAwesomeIcon icon={faTimes} fixedWidth />
+        </span>
+      </div>
+    );
+  }
+}
+
+export default CoordinatesDialog;
+CoordinatesDialog.propTypes = {
+  toggleWithClose: PropTypes.func,
+  coordinatesMetadata: PropTypes.object,
+};
