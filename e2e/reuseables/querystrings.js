395706daff1ce96152296581afa8c00f2f8994bf
Jason Kent <jasontkent@gmail.com>
2020-11-10 10:18:18 -0500

    add e2e test for inactive event alert

--- e2e/reuseables/querystrings.js
+++ e2e/reuseables/querystrings.js
@@ -25,6 +25,7 @@ module.exports = {
   // events
   eventsTabActive: '?e=true',
   mockEvents: '?p=geographic&l=VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor,Reference_Labels(hidden),Reference_Features(hidden),Coastlines&t=2018-05-02-T00%3A00%3A00Z&z=3&v=-409.00147812273656,-205.62883007565202,270.5880270080828,219.11461063111003&e=true&mockEvents=20170530',
+  closedEvent: '?e=EONET_5133',
 
   // layers
   multipleDataLayers: '?p=geographic&l=MODIS_Terra_Aerosol,MODIS_Terra_Brightness_Temp_Band31_Day&t=2017-03-22&z=3&v=136.07019188386334,14.722152527011556,155.59817576644127,24.312819167567586',
