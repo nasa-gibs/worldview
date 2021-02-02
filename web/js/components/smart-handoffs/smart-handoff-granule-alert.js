f6d44786ece2059a7c21dd9e6031606a176c10bf
Jason Kent <jasontkent@gmail.com>
2021-02-02 11:38:41 -0500

    abbrev

--- web/js/components/smart-handoffs/smart-handoff-granule-alert.js
+++ web/js/components/smart-handoffs/smart-handoff-granule-alert.js
@@ -12,15 +12,15 @@ const GranuleAlertModalBody = () => (
       {' '}
       <a href="https://earthdata.nasa.gov/earth-observation-data/near-real-time/near-real-time-versus-standard-products" target="_blank" rel="noopener noreferrer">Near Real-Time (NRT)</a>
       {' '}
-      and then &quot;backfilled&quot; by the corresponding Standard product when it is available. Worldview displays them on a
+      and then &quot;backfilled&quot; by the corresponding Standard (STD) product when it is available. Worldview displays them on a
       <a href="https://wiki.earthdata.nasa.gov/display/GIBS/GIBS+API+for+Developers#GIBSAPIforDevelopers-%22BestAvailable%22Layers" target="_blank" rel="noopener noreferrer"> &quot;Best Available&quot; </a>
       basis, showing Standard-quality imagery if it is available, NRT otherwise.
     </p>
     <p>
-      Often layers will have both NRT and Standard collections available for download in Earthdata Search. If you are looking
+      Often layers will have both NRT and STD collections available for download in Earthdata Search. If you are looking
       for data within the last ~7 days you will likely want to use the NRT collection.  If you have the NRT collection
       selected and the granule count shows &quot;NONE&quot;, this may be an indication that your selected date is too far in the past and you
-      may have better luck with the Standard collection.
+      may have better luck with the STD collection.
     </p>
     <p>You may also see NONE or “0 out of n” granules when:</p>
     <ol>
