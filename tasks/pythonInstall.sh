69dae01ade95907b170d47599e86b089d66d60d7
Jason Kent <jasontkent@gmail.com>
2021-03-04 12:56:34 -0500

    system dependent pipenv

--- tasks/pythonInstall.sh
+++ tasks/pythonInstall.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 # Install python dependencies
 PYTHON_VERSION="$(python -V 2>&1)"
-if [[ $1 = "linux" ]]; then
-  PYPATH=.python/bin:${PATH}
-else
-  PYPATH=.python/Scripts:${PATH}
-fi
 
 if command -v python3 &>/dev/null; then
-  echo "Installing virtualenv with python3"
-  python3 -m pip install --user virtualenv
-  python3 -m virtualenv .python
-  PATH=${PYPATH} python3 -m pip install -r requirements.txt
+  echo "Installing pipenv and dependencies with python3"
+  python3 -m pip install pipenv
+  python3 -m pipenv install
 elif [[ $PYTHON_VERSION = *"Python 3"* ]]; then
-  echo "Installing virtualenv with python"
-  python -m pip install --user virtualenv
-  python -m virtualenv .python
-  PATH=${PYPATH} python -m pip install -r requirements.txt
+  echo "Installing pipenv and dependencies  with python"
+  pip install pipenv
+  pipenv install
 else
   echo "Please install or update python v3"
 fi
-
-
-
-# TODO remove
