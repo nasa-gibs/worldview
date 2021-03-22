4904d80fdaf3d0480171d2ad8cb1444dd462fef0
Ed Plato <edplatomail@gmail.com>
2021-03-22 13:28:23 -0400

    Add distraction free mode query string param, add shortcuts section in about page (#3399)
    * add df query string, add keyboard shortcuts, revise about, remove unused pages
    
    * use separate exit distraction free mode toolbar button, revise test
    
    * add distract free wv-logo condition class, use entities with borders for shortcuts, remove comments
    
    * darken opacity in df mode
    
    * add df mode to mobile list, minor info list refactors, update related e2e

--- e2e/features/ui/info-mobile-test.js
+++ e2e/features/ui/info-mobile-test.js
@@ -8,45 +8,40 @@ const {
 const TIME_LIMIT = 5000;
 
 module.exports = {
-  beforeEach: (client) => {
-    reuseables.loadAndSkipTour(client, TIME_LIMIT);
-    client.resizeWindow(375, 667); // iPhone 6/7/8 dimensions
+  before: (c) => {
+    reuseables.loadAndSkipTour(c, TIME_LIMIT);
+    c.resizeWindow(375, 667); // iPhone 6/7/8 dimensions
   },
 
   // verify mobile info toolbar is visible and contains valid mobile menu items
-  'Mobile info toolbar is visible and contains valid mobile menu items': (client) => {
-    client.expect.element(infoToolbarButton).to.be.present;
-    client.pause(500);
-    client.click(infoToolbarButton);
+  'Mobile info toolbar is visible and contains valid mobile menu items': (c) => {
+    c.expect.element(infoToolbarButton).to.be.present;
+    c.pause(500);
+    c.click(infoToolbarButton);
 
-    client.waitForElementVisible('#toolbar_info', TIME_LIMIT);
-    client.expect.element('#send_feedback_info_item').to.be.present;
-    client.expect.element('#source_code_info_item').to.be.present;
-    client.expect.element('#whats_new_info_item').to.be.present;
-    client.expect.element('#about_info_item').to.be.present;
+    c.waitForElementVisible('#toolbar_info', TIME_LIMIT);
+    c.expect.element('#send_feedback_info_item').to.be.present;
+    c.expect.element('#source_code_info_item').to.be.present;
+    c.expect.element('#whats_new_info_item').to.be.present;
+    c.expect.element('#about_info_item').to.be.present;
+    c.expect.element('#distraction_free_info_item').to.be.present;
   },
 
   // verify mobile source code menu item opens separate tab or window
-  'Mobile source code menu item opens separate tab or window': (client) => {
-    client.pause(1000);
-    client.waitForElementVisible(infoToolbarButton, TIME_LIMIT);
-    client.click(infoToolbarButton);
-    client.waitForElementVisible('#toolbar_info', TIME_LIMIT);
-
-    client.windowHandles((tabs) => {
-      client.assert.equal(tabs.value.length, 1);
+  'Mobile source code menu item opens separate tab or window': (c) => {
+    c.windowHandles((tabs) => {
+      c.assert.equal(tabs.value.length, 1);
     });
 
-    client.waitForElementVisible('#source_code_info_item', TIME_LIMIT);
-    client.click('#source_code_info_item');
-    client.pause(1000);
+    c.click('#source_code_info_item');
+    c.pause(500);
 
-    client.windowHandles((tabs) => {
-      client.assert.equal(tabs.value.length, 2);
+    c.windowHandles((tabs) => {
+      c.assert.equal(tabs.value.length, 2);
     });
   },
 
-  afterEach: (client) => {
-    client.end();
+  after: (c) => {
+    c.end();
   },
 };
