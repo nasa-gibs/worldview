9f05c067b2d6bd0ee78ce93a3a87b636f72a00cb
Jason Kent <jasontkent@gmail.com>
2021-04-07 14:45:55 -0400

    historical events base functionality
    - make request based on year
    - filter categories locally

--- web/js/modules/natural-events/selectors.js
+++ web/js/modules/natural-events/selectors.js
@@ -1,21 +1,26 @@
-import { createSelector } from 'reselect';
-import {
-  get as lodashGet,
-} from 'lodash';
+// import { createSelector } from 'reselect';
+// import {
+//   get as lodashGet,
+// } from 'lodash';
 
 const getEvents = ({ requestedEvents }) => (requestedEvents
   ? requestedEvents.response
   : []
 );
 
-// eslint-disable-next-line import/prefer-default-export
-export const getEventCategories = createSelector(
-  [getEvents],
-  (events) => (events || []).reduce((categories, event) => {
-    const categoryTitle = lodashGet(event, 'categories[0].title');
-    if (categories.indexOf(categoryTitle) === -1) {
-      categories.push(categoryTitle);
-    }
-    return categories;
-  }, []),
+const getCategories = ({ requestedEventCategories }) => (requestedEventCategories
+  ? requestedEventCategories.response
+  : []
 );
+
+// eslint-disable-next-line import/prefer-default-export
+// export const getEventCategories = createSelector(
+//   [getEvents],
+//   (events) => (events || []).reduce((categories, event) => {
+//     const categoryTitle = lodashGet(event, 'categories[0].title');
+//     if (categories.indexOf(categoryTitle) === -1) {
+//       categories.push(categoryTitle);
+//     }
+//     return categories;
+//   }, []),
+// );
