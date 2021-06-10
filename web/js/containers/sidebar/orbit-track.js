c5dd21be945ffad6b0093a5a47c2f24d32292762
Jason Kent <jasontkent@gmail.com>
2021-06-10 10:29:17 -0400

    fix errors
    - don't trigger callbacks that set state in component A while setting state in component B (checkbox.js)
    - dont dispatch action during render method/in main body of function component (orbit-tracks.js)

--- web/js/containers/sidebar/orbit-track.js
+++ web/js/containers/sidebar/orbit-track.js
@@ -1,58 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { isEmpty as lodashIsEmpty, get as lodashGet } from 'lodash';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PaletteLegend from '../../components/sidebar/paletteLegend';
 import {
-  getPalette,
+  getPalette as getPaletteSelector,
   getPaletteLegends,
 } from '../../modules/palettes/selectors';
-import { requestPalette } from '../../modules/palettes/actions';
+import { requestPalette as requestPaletteAction } from '../../modules/palettes/actions';
 import { getOrbitTrackTitle } from '../../modules/layers/util';
 
+function OrbitTrack(props) {
+  const {
+    trackLayer,
+    paletteLegends,
+    getPalette,
+    renderedPalette,
+    requestPalette,
+    isLoading,
+    isMobile,
+    parentLayer,
+    hasPalette,
+  } = props;
 
-class OrbitTrack extends React.Component {
-  getPaletteLegend = () => {
-    const {
-      trackLayer,
-      paletteLegends,
-      getPalette,
-      renderedPalette,
-      requestPalette,
-      isLoading,
-      isMobile,
-      parentLayer,
-    } = this.props;
-    if (!lodashIsEmpty(renderedPalette)) {
-      return (
-        <PaletteLegend
-          layer={trackLayer}
-          parentLayer={parentLayer}
-          getPalette={getPalette}
-          paletteLegends={paletteLegends}
-          isMobile={isMobile}
-        />
-      );
-    } if (!isLoading) {
+  const containerClasses = `wv-orbit-track ${!trackLayer.visible ? 'not-visible' : ''}`;
+  const [palette, setPalette] = useState();
+
+  useEffect(() => {
+    if (hasPalette && !isLoading && !renderedPalette) {
       requestPalette(trackLayer.id);
+      return;
     }
-  }
+    setPalette((
+      <PaletteLegend
+        layer={trackLayer}
+        parentLayer={parentLayer}
+        getPalette={getPalette}
+        paletteLegends={paletteLegends}
+        isMobile={isMobile}
+      />
+    ));
+  }, [renderedPalette]);
 
-  render() {
-    const { trackLayer, hasPalette } = this.props;
-    const containerClasses = `wv-orbit-track ${!trackLayer.visible ? 'not-visible' : ''}`;
-
-    return (
-      <div className={containerClasses}>
-        {hasPalette ? this.getPaletteLegend() : ''}
-        <FontAwesomeIcon icon="satellite" />
-        <span className="wv-orbit-track-label">
-          {getOrbitTrackTitle(trackLayer)}
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className={containerClasses}>
+      {palette}
+      <FontAwesomeIcon icon="satellite" />
+      <span className="wv-orbit-track-label">
+        {getOrbitTrackTitle(trackLayer)}
+      </span>
+    </div>
+  );
 }
 
 OrbitTrack.propTypes = {
@@ -89,13 +88,13 @@ function mapStateToProps(state, ownProps) {
     renderedPalette: renderedPalettes[paletteName],
     isMobile: state.browser.lessThan.medium,
     hasPalette,
-    getPalette: (layerId, index) => getPalette(trackLayer.id, index, compareState, state),
+    getPalette: (layerId, index) => getPaletteSelector(trackLayer.id, index, compareState, state),
   };
 }
 
 const mapDispatchToProps = (dispatch) => ({
   requestPalette: (id) => {
-    dispatch(requestPalette(id));
+    dispatch(requestPaletteAction(id));
   },
 });
 export default connect(
