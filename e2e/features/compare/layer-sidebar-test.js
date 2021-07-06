8a9ddd8ef7d7fcd0de436fa3d7307c0ab7823ee1
Jason Kent <jasontkent@gmail.com>
2021-07-06 10:10:27 -0600

    fix e2e tests

--- e2e/features/compare/layer-sidebar-test.js
+++ e2e/features/compare/layer-sidebar-test.js
@@ -2,70 +2,65 @@ const reuseables = require('../../reuseables/skip-tour.js');
 const selectors = require('../../reuseables/selectors.js');
 const localQueryStrings = require('../../reuseables/querystrings.js');
 
+const aodChoices = '#accordion-legacy-all-aerosol-optical-depth .measure-row-contents';
 const aodCombinedValueId = 'MODIS_Combined_Value_Added_AOD';
 const aodMAIACId = 'MODIS_Combined_MAIAC_L2G_AerosolOpticalDepth';
-const aodCheckBox = '#checkbox-case-MODIS_Combined_Value_Added_AOD .wv-checkbox';
-const aodMAIACCheckbox = '#checkbox-case-MODIS_Combined_MAIAC_L2G_AerosolOpticalDepth .wv-checkbox';
+const aodCheckBox = '#MODIS_Combined_Value_Added_AOD-checkbox';
+const aodMAIACCheckbox = '#checkbox-case-MODIS_Combined_MAIAC_L2G_AerosolOpticalDepth .wv-checkbox input';
 const TIME_LIMIT = 10000;
 
 module.exports = {
-  before: (client) => {
-    reuseables.loadAndSkipTour(client, client.globals.timeout);
+  before: (c) => {
+    reuseables.loadAndSkipTour(c, c.globals.timeout);
   },
-  'Add AOD Layer to Layer Group A': (client) => {
-    client.url(client.globals.url + localQueryStrings.swipeAndAIsActive);
-    client.waitForElementVisible(selectors.swipeDragger, client.globals.timeout, () => {
-      client.click(selectors.addLayers);
-      client.waitForElementVisible(selectors.aerosolOpticalDepth, client.globals.timeout, () => {
-        client.click(selectors.aerosolOpticalDepth);
-        client.waitForElementVisible(aodCheckBox, client.globals.timeout, () => {
-          client.click(aodCheckBox);
-          client.waitForElementVisible(`#active-${aodCombinedValueId}`, client.globals.timeout);
-          client.click(selectors.layersModalCloseButton);
-        });
-      });
-    });
-    client.waitForElementNotPresent('#layer_picker_component', TIME_LIMIT);
-    client.pause(250);
+  'Add AOD Layer to Layer Group A': (c) => {
+    c.url(c.globals.url + localQueryStrings.swipeAndAIsActive);
+    c.waitForElementVisible(selectors.swipeDragger, c.globals.timeout);
+    c.click(selectors.addLayers);
+    c.waitForElementVisible(selectors.aerosolOpticalDepth, c.globals.timeout);
+    c.click(selectors.aerosolOpticalDepth);
+
+    c.waitForElementVisible(aodChoices, TIME_LIMIT);
+    c.click(aodCheckBox);
+    c.waitForElementVisible(`#active-${aodCombinedValueId}`, c.globals.timeout);
+    c.click(selectors.layersModalCloseButton);
+    c.waitForElementNotPresent('#layer_picker_component', TIME_LIMIT);
+    c.pause(250);
   },
-  'Toggle compare mode to Active state B': (client) => {
-    client.click(`${selectors.bTab} .productsIcon`);
-    client.waitForElementVisible('#activeB-Coastlines_15m', client.globals.timeout);
+  'Toggle compare mode to Active state B': (c) => {
+    c.click(`${selectors.bTab} .productsIcon`);
+    c.waitForElementVisible('#activeB-Coastlines_15m', c.globals.timeout);
   },
-  'Verify that AOD layer is not visible': (client) => {
-    client.expect.element(`#active-${aodCombinedValueId}`).to.not.be.present;
+  'Verify that AOD layer is not visible': (c) => {
+    c.expect.element(`#active-${aodCombinedValueId}`).to.not.be.present;
   },
-  'Add AOD index layer to Active state B and verify it has been added': (client) => {
-    client.click(selectors.addLayers);
-    client.waitForElementVisible(
-      aodMAIACCheckbox,
-      client.globals.timeout, () => {
-        client.click(aodMAIACCheckbox);
-        client.waitForElementVisible(
-          '#activeB-MODIS_Combined_MAIAC_L2G_AerosolOpticalDepth',
-          client.globals.timeout,
-        );
-        client.expect.element(`#activeB-${aodCombinedValueId}`).to.not.be.present;
-        client.expect.element(`#active-${aodCombinedValueId}`).to.not.be.present;
-      },
+  'Add AOD index layer to Active state B and verify it has been added': (c) => {
+    c.click(selectors.addLayers);
+    c.waitForElementVisible(aodChoices, c.globals.timeout);
+    c.click(aodMAIACCheckbox);
+    c.waitForElementVisible(
+      '#activeB-MODIS_Combined_MAIAC_L2G_AerosolOpticalDepth',
+      c.globals.timeout,
     );
+    c.expect.element(`#activeB-${aodCombinedValueId}`).to.not.be.present;
+    c.expect.element(`#active-${aodCombinedValueId}`).to.not.be.present;
   },
   'Verify that AOD combined is visible and AOD index is not present in Layer list A': function(
-    client,
+    c,
   ) {
-    client.click(selectors.layersModalCloseButton);
-    client.pause(100);
-    client.click(selectors.aTab);
-    client.waitForElementVisible(
+    c.click(selectors.layersModalCloseButton);
+    c.pause(100);
+    c.click(selectors.aTab);
+    c.waitForElementVisible(
       `#active-${aodCombinedValueId}`,
-      client.globals.timeout,
+      c.globals.timeout,
       () => {
-        client.expect.element(`#activeB-${aodCombinedValueId}`).to.not.be.present;
-        client.expect.element(`#activeA-${aodMAIACId}`).to.not.be.present;
+        c.expect.element(`#activeB-${aodCombinedValueId}`).to.not.be.present;
+        c.expect.element(`#activeA-${aodMAIACId}`).to.not.be.present;
       },
     );
   },
-  after(client) {
-    client.end();
+  after(c) {
+    c.end();
   },
 };
