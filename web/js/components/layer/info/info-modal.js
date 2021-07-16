4659cb39b3ca7f4110000a535b5a9daf098b6fcb
Jason Kent <jasontkent@gmail.com>
2021-07-16 15:51:19 -0600

    show measurement metadata in layer info modal

--- web/js/components/layer/info/info-modal.js
+++ web/js/components/layer/info/info-modal.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import LayerInfo from './info';
-import Scrollbars from '../../util/scrollbar';
+import util from '../../../util/util';
 
 export default function LayerInfoModal(props) {
-  const { screenHeight, layer } = props;
+  const { layer, measurementDescriptionPath } = props;
+  const [metadata, setMetadata] = useState();
+
+  const initMetadata = async () => {
+    if (!metadata && measurementDescriptionPath) {
+      const url = `config/metadata/layers/${measurementDescriptionPath}.html`;
+      const data = await util.get(url);
+      setMetadata(data || 'No metadata found.');
+    }
+  };
+
+  useEffect(() => {
+    initMetadata();
+  }, [layer]);
 
   return (
-    <Scrollbars style={{ maxHeight: `${screenHeight - 200}px` }}>
+    <div id="layer-description" className="layer-description">
       <LayerInfo layer={layer} />
-      {/*
-        Put the source specific metadata here as well
-      */}
-    </Scrollbars>
+      {metadata && (
+        <div
+          className="layer-metadata"
+          dangerouslySetInnerHTML={{ __html: metadata }}
+        />
+      )}
+    </div>
   );
 }
 
 LayerInfoModal.propTypes = {
-  screenHeight: PropTypes.number,
   layer: PropTypes.object,
+  measurementDescriptionPath: PropTypes.string,
 };
