a4b7f5085408d6ba5bba1da99a0b081d22a1fe63
Jason Kent <jasontkent@gmail.com>
2021-07-30 09:37:41 -0600

    basic preloading on click and hold

--- web/js/components/timeline/timeline-controls/date-change-arrows.js
+++ web/js/components/timeline/timeline-controls/date-change-arrows.js
@@ -1,9 +1,14 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import HoverTooltip from '../../util/hover-tooltip';
+import {
+  setArrowDown as setArrowDownAction,
+  setArrowUp as setArrowUpAction,
+} from '../../../modules/date/actions';
 
-const ANIMATION_DELAY = 200; // interval firing to trigger parent level arrow change
-const CLICK_TIMEOUT_DELAY = 500; // wait before click becomes a delay
+const ANIMATION_DELAY = 500; // interval firing to trigger parent level arrow change
+const CLICK_HOLD_DELAY = 500; // wait before click is considered a hold
 
 let mouseHoldCheckTimer = null;
 let isMouseHolding = false;
@@ -14,69 +19,51 @@ const intervals = {
 };
 
 class DateChangeArrows extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.arrowDownMap = {
+      left: props.leftArrowDown,
+      right: props.rightArrowDown,
+    };
+
+    this.arrowUpMap = {
+      left: props.leftArrowUp,
+      right: props.rightArrowUp,
+    };
+  }
+
   componentWillUnmount() {
     clearInterval(intervals.left);
     clearInterval(intervals.right);
   }
 
-  /**
-  * @desc repeatedly call while mouse down - decrement date
-  * @returns {void}
-  */
-  leftArrowDown = () => {
-    const { leftArrowDown } = this.props;
-    leftArrowDown();
-    mouseHoldCheckTimer = setTimeout(() => {
-      mouseHoldCheckTimer = null;
-      isMouseHolding = true;
-      // set interval for holding arrow down
-      intervals.left = setInterval(leftArrowDown, ANIMATION_DELAY);
-    }, CLICK_TIMEOUT_DELAY);
+  clickAndHold = (direction) => {
+    const { setArrowDown } = this.props;
+    setArrowDown(direction);
+    mouseHoldCheckTimer = null;
+    isMouseHolding = true;
+    // set interval for holding arrow down
+    intervals[direction] = setInterval(this.arrowDownMap[direction], ANIMATION_DELAY);
   }
 
-  /**
-  * @desc repeatedly call while mouse down - decrement date
-  * @returns {void}
-  */
-  rightArrowDown = () => {
-    const { rightArrowDown } = this.props;
-    rightArrowDown();
+  arrowDown = (direction) => {
+    this.arrowDownMap[direction]();
     mouseHoldCheckTimer = setTimeout(() => {
-      mouseHoldCheckTimer = null;
-      isMouseHolding = true;
-      // set interval for holding arrow down
-      intervals.right = setInterval(rightArrowDown, ANIMATION_DELAY);
-    }, CLICK_TIMEOUT_DELAY);
+      this.clickAndHold(direction);
+    }, CLICK_HOLD_DELAY);
   }
 
-  /**
-  * @desc stop animation from left arrow - clear timeout invocation
-  * @returns {void}
-  */
-  leftArrowUp = () => {
-    const { leftArrowUp } = this.props;
+  arrowUp = (direction) => {
+    const { setArrowUp } = this.props;
     if (mouseHoldCheckTimer) {
       clearTimeout(mouseHoldCheckTimer);
     } else if (isMouseHolding) {
       isMouseHolding = false;
     }
-    clearInterval(intervals.left);
-    leftArrowUp();
-  }
-
-  /**
-  * @desc stop animation from right arrow - clear timeout invocation
-  * @returns {void}
-  */
-  rightArrowUp = () => {
-    const { rightArrowUp } = this.props;
-    if (mouseHoldCheckTimer) {
-      clearTimeout(mouseHoldCheckTimer);
-    } else if (isMouseHolding) {
-      isMouseHolding = false;
-    }
-    clearInterval(intervals.right);
-    rightArrowUp();
+    clearInterval(intervals[direction]);
+    this.arrowUpMap[direction]();
+    setArrowUp();
   }
 
   render() {
@@ -87,15 +74,21 @@ class DateChangeArrows extends PureComponent {
       nowButtonDisabled,
       rightArrowDisabled,
     } = this.props;
+
+    const leftArrowDown = () => this.arrowDown('left');
+    const rightArrowDown = () => this.arrowDown('right');
+    const leftArrowUp = () => this.arrowUp('left');
+    const rightArrowUp = () => this.arrowUp('right');
+
     return (
       <div>
         {/* LEFT ARROW */}
         <div
           className={`button-action-group${leftArrowDisabled ? ' button-disabled' : ''}`}
           id="left-arrow-group"
-          onMouseDown={this.leftArrowDown}
-          onMouseUp={this.leftArrowUp}
-          onMouseLeave={this.leftArrowUp}
+          onMouseDown={leftArrowDown}
+          onMouseUp={leftArrowUp}
+          onMouseLeave={leftArrowUp}
           aria-disabled={leftArrowDisabled}
         >
           <HoverTooltip
@@ -116,9 +109,9 @@ class DateChangeArrows extends PureComponent {
         <div
           className={`button-action-group${rightArrowDisabled ? ' button-disabled' : ''}`}
           id="right-arrow-group"
-          onMouseDown={this.rightArrowDown}
-          onMouseUp={this.rightArrowUp}
-          onMouseLeave={this.rightArrowUp}
+          onMouseDown={rightArrowDown}
+          onMouseUp={rightArrowUp}
+          onMouseLeave={rightArrowUp}
           aria-disabled={rightArrowDisabled}
         >
           <HoverTooltip
@@ -160,6 +153,17 @@ class DateChangeArrows extends PureComponent {
   }
 }
 
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = (dispatch) => ({
+  setArrowDown: (isDownDirection) => {
+    dispatch(setArrowDownAction(isDownDirection));
+  },
+  setArrowUp: () => {
+    dispatch(setArrowUpAction());
+  },
+});
+
 DateChangeArrows.propTypes = {
   handleSelectNowButton: PropTypes.func,
   leftArrowDisabled: PropTypes.bool,
@@ -170,6 +174,8 @@ DateChangeArrows.propTypes = {
   rightArrowDisabled: PropTypes.bool,
   rightArrowDown: PropTypes.func,
   rightArrowUp: PropTypes.func,
+  setArrowDown: PropTypes.func,
+  setArrowUp: PropTypes.func,
 };
 
-export default DateChangeArrows;
+export default connect(mapStateToProps, mapDispatchToProps)(DateChangeArrows);
