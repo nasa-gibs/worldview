cc0f81ee50c3acdd7caf1e163410eb419899c2d0
Ed Plato <edplatomail@gmail.com>
2021-08-09 17:33:41 -0400

    add warning for both wings, revise text, refactor out of render, add e2e

---
+++ e2e/features/image-download/crosses-dateline-test.js
@@ -0,0 +1,55 @@
+const { normalizeViewport } = require('../../reuseables/normalize-viewport');
+const {
+  openImageDownloadPanel,
+} = require('../../reuseables/image-download');
+
+const TIME_LIMIT = 10000;
+
+const datelineAlertIcon = '.snapshot-date-alert-icon';
+const datelineAlertIconTooltipText = '.snapshot-date-warning-tooltip-inner';
+
+const withinMapURLParams = '?v=-67.80916012733559,-56.052180562072095,-30.50743102883792,-30.873513420586164&t=2021-08-08-T0';
+const crossesPrevDayURLParams = '?v=161.16767164758798,-54.46571918482002,198.46940074608565,-29.287052043334096&t=2021-08-08-T0';
+const crossesNextDayURLParams = '?v=-198.76946733086245,-59.504883811673906,-161.46773823236478,-34.326216670187975&t=2021-08-08-T0';
+
+module.exports = {
+  after(client) {
+    client.end();
+  },
+
+  'No dateline alert icon with message if not crossing dateline(s)': (c) => {
+    normalizeViewport(c, 1024, 768);
+    c.url(c.globals.url + withinMapURLParams);
+    openImageDownloadPanel(c);
+    c.pause(500);
+
+    c.expect.element(datelineAlertIcon).to.not.be.present;
+  },
+
+  'Dateline alert icon with previous day message if crosses previous day dateline': (c) => {
+    normalizeViewport(c, 1024, 768);
+    c.url(c.globals.url + crossesPrevDayURLParams);
+    openImageDownloadPanel(c);
+    c.pause(500);
+
+    c.expect.element(datelineAlertIcon).to.be.present;
+    c.moveToElement(datelineAlertIcon, 0, 0);
+    c.waitForElementVisible(datelineAlertIconTooltipText, TIME_LIMIT, (e) => {
+      c.assert.containsText(datelineAlertIconTooltipText, 'The selected snapshot area crosses the dateline and uses imagery from the previous day 2021 AUG 07.');
+    });
+  },
+
+  'Dateline alert icon with next day message if crosses next day dateline': (c) => {
+    normalizeViewport(c, 1024, 768);
+    c.url(c.globals.url + crossesNextDayURLParams);
+    openImageDownloadPanel(c);
+    c.pause(500);
+
+    c.expect.element(datelineAlertIcon).to.be.present;
+    c.moveToElement(datelineAlertIcon, 0, 0);
+    c.waitForElementVisible(datelineAlertIconTooltipText, TIME_LIMIT, (e) => {
+      c.assert.containsText(datelineAlertIconTooltipText, 'The selected snapshot area crosses the dateline and uses imagery from the next day 2021 AUG 09.');
+    });
+  },
+
+};
