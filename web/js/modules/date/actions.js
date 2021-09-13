c0586589d4afc09b75e8ae7ed045bb63b671a0d6
Ed Plato <edplatomail@gmail.com>
2021-09-13 12:07:01 -0400

    remove init second date logic and update test

--- web/js/modules/date/actions.js
+++ web/js/modules/date/actions.js
@@ -5,7 +5,6 @@ import {
   SELECT_DATE,
   UPDATE_APP_NOW,
   TOGGLE_CUSTOM_MODAL,
-  INIT_SECOND_DATE,
   ARROW_DOWN,
   ARROW_UP,
   SET_PRELOAD,
@@ -47,11 +46,6 @@ export function updateAppNow(date) {
   return {
     type: UPDATE_APP_NOW,
     value: date,
-  };
-}
-export function initSecondDate() {
-  return {
-    type: INIT_SECOND_DATE,
   };
 }
 export function selectDate(date) {
