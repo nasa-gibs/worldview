a7a15b44c416a7a88fd2e024fac2b4090477e42d
minniewong <min.m.wong@nasa.gov>
2021-09-14 17:22:42 -0400

    update text for readme and about; remove measurement description

--- web/js/components/about/about-page.js
+++ web/js/components/about/about-page.js
@@ -24,7 +24,7 @@ export default function AboutPage() {
           {' '}
           <a href="https://earthdata.nasa.gov/eosdis" target="_blank" rel="noopener noreferrer">EOSDIS</a>
           {' '}
-          provides the capability to interactively browse over 900 global, full-resolution satellite imagery
+          provides the capability to interactively browse over 1000 global, full-resolution satellite imagery
           layers and then download the underlying data. Many of the imagery layers are updated daily and are available within three hours
           of observation - essentially showing the entire Earth as it looks &quot;right now&quot;. This supports time-critical application
           areas such as wildfire management, air quality measurements, and flood monitoring. Arctic and Antarctic views
@@ -144,7 +144,7 @@ export default function AboutPage() {
         <hr />
         <h2>Acknowledgements</h2>
         <p>
-          With over 900 imagery layers, Worldview acknowledges the data providers of the near real-time (NRT) and standard quality imagery layers.
+          With over 1000 imagery layers, Worldview acknowledges the data providers of the near real-time (NRT) and standard quality imagery layers.
         </p>
         <p>
           While Worldview provides the highest priority products/parameters as decided by the data providers and users, not all NASA standard products (or all parameters within each product) are available via Worldview, see
@@ -262,8 +262,13 @@ export default function AboutPage() {
         </p>
 
         <ul>
+          <li>GeoColor imagery layers from GOES-East and GOES-West are provided by
+          {' '}
+            <a href="https://www.star.nesdis.noaa.gov/goes/" target="_blank" rel="noopener noreferrer">NOAA/NESDIS/STAR</a>.
+            {' '}
+          </li>
           <li>
-            Geostationary imagery layers are provided by the
+            Geostationary imagery layers from GOES-East, GOES-West and Himawari-8 (Red Visible, Clean Infrared, and Air Mass) are provided by the
             {' '}
             <a href="https://weather.msfc.nasa.gov/sport/" target="_blank" rel="noopener noreferrer">Short-term Prediction Research and Transition Center (SPoRT)</a>
             {' '}
