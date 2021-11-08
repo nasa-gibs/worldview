1db9bd62718553e4506ed2e2f950534ae6d14c81
Jason Kent <jasontkent@gmail.com>
2021-11-08 14:13:57 -0700

    use fixed pipenv version

--- tasks/pythonInstall.sh
+++ tasks/pythonInstall.sh
@@ -4,7 +4,7 @@ PYTHON_VERSION="$(python -V 2>&1)"
 
 if command -v python3 &>/dev/null; then
   echo "Installing pipenv and dependencies with python3"
-  python3 -m pip install pipenv
+  python3 -m pip install pipenv==2020.11.15
   python3 -m pipenv install
 elif [[ $PYTHON_VERSION = *"Python 3"* ]]; then
   echo "Installing pipenv and dependencies  with python"
