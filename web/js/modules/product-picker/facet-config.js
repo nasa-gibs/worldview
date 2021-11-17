e5c00f46113c6fb8c6775e48682d7c23588d09ca
Jason Kent <jasontkent@gmail.com>
2021-11-17 13:12:11 -0700

    un-pluralize

--- web/js/modules/product-picker/facet-config.js
+++ web/js/modules/product-picker/facet-config.js
@@ -58,7 +58,7 @@ export default [
     field: 'dataCenter',
     label: 'DAAC/SIPS',
     filterType: 'any',
-    tooltip: 'Distributed Active Archive Centers / Science Investigator-led Processing System',
+    tooltip: 'Distributed Active Archive Center / Science Investigator-led Processing System',
     show: 30,
     hideZeroCount: true,
   },
