9e33661225198804263749a3ae3deb314da33f7b
Jason Hurley <jwhurley1@gmail.com>
2022-01-03 10:28:52 -0500

    WV-2098 Removed 'orientationChanged' from 'isMobile && isOpen' if statement.  Was causing modal to close in mobile devices with short screens when keyboard pops up.

--- web/js/containers/modal.js
+++ web/js/containers/modal.js
@@ -55,12 +55,11 @@ class ModalContainer extends Component {
       desktopOnly,
     } = newProps;
 
-    const orientationChanged = orientation !== prevProps.orientation;
     const screenHeightChanged = screenHeight !== prevProps.screenHeight;
     const screenWidthChanged = screenWidth !== prevProps.screenWidth;
     const toggleFunction = toggleWithClose(onToggle, onClose, isOpen);
     if (isMobile && isOpen) {
-      if (desktopOnly || orientationChanged) {
+      if (desktopOnly) {
         toggleFunction();
       }
       if (customProps.mobileFullScreen && (screenHeightChanged || screenWidthChanged)) {
