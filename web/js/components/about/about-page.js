0eba706aafbf9953e4a08fca61841fff22686609
Jason Hurley <jwhurley1@gmail.com>
2022-01-21 16:06:16 -0500

    WV-2084: -Simplified 'about-page.js' by creating a child component, 'about-section.js'

--- web/js/components/about/about-page.js
+++ web/js/components/about/about-page.js
@@ -1,37 +1,19 @@
 import React from 'react';
 import Scrollbar from '../util/scrollbar';
+import AboutSection from './about-section';
 
 export default function AboutPage() {
-  const [welcomeDescription, setwelcomeDescription] = useState();
-
-  const fetchHTML = (sectionName, setFn) => {
-    let controller = new AbortController();
-    (async () => {
-      if (!sectionName) {
-        return;
-      }
-      try {
-      // const options = { signal: controller.signal };
-        const data = await fetch(`brand/about/${sectionName}.html`);
-        const dataHTML = await data.text();
-        controller = null;
-        setFn(dataHTML || 'No description was found for this section.');
-      } catch (e) {
-        if (!controller.signal.aborted) {
-        // eslint-disable-next-line no-console
-          console.error(e);
-        }
-      }
-    })();
-    return controller ? controller.abort() : null;
-  };
-
-  const welcomeHTML = fetchHTML('welcome');
-
   return (
     <Scrollbar style={{ height: 'calc(var(--vh, 1vh) * 100 - 100px)' }}>
-      <div className="about-page">
-        {welcomeHTML}
+      <div
+        className="about-page"
+      >
+        <AboutSection section="welcome" />
+        <AboutSection section="keyboard" />
+        <AboutSection section="imagery" />
+        <AboutSection section="acknowledgements" />
+        <AboutSection section="disclaimer" />
+        <AboutSection section="license" />
       </div>
     </Scrollbar>
   );
