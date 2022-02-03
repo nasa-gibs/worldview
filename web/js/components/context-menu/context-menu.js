c28dc676236b663f122abc977e537810bd2cca60
Jason Hurley <jwhurley1@gmail.com>
2022-02-03 17:54:56 -0500

    WV-2126 -Consolidated context menu code into one component and deleted useContextMenu.js - 'Copy Coordinates to Clipboard' works.  'Copied' tooltip display is WIP

--- web/js/components/context-menu/context-menu.js
+++ web/js/components/context-menu/context-menu.js
@@ -1,22 +1,80 @@
-import React from 'react';
-import useContextMenu from './useContextMenu';
+import { connect } from 'react-redux';
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+import { Tooltip } from 'reactstrap';
+import util from '../../util/util';
 
-const ContextMenu = () => {
-  const { anchorPoint, show } = useContextMenu();
+const { events } = util;
+
+function ContextMenu(props) {
+  const [show, setShow] = useState(false);
+  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+  const [coord, setCoord] = useState();
+  const [isCopied, setIsCopied] = useState(false);
+  // const { map } = props;
+
+  const handleClick = () => (show ? setShow(false) : null);
+
+  function handleContextEvent(event, map, crs) {
+    event.originalEvent.preventDefault();
+    const pixels = event.pixel;
+    setAnchorPoint({ x: pixels[0], y: pixels[1] });
+    setCoord(map.getCoordinateFromPixel(pixels));
+    setShow(true);
+  }
+
+  function copyCoordsToClipboard(coords) {
+    navigator.clipboard.writeText(coords);
+  }
+
+  events.on('map:singleclick', handleClick);
+  events.on('map:contextmenu', handleContextEvent);
 
   if (show) {
     return (
-      <ul
-        className="context-menu"
-        style={{ top: anchorPoint.y, left: anchorPoint.x }}
-      >
-        <li>Copy Coordinates to Clipboard</li>
-        <li>Start a Measurement</li>
-        <li>Add a Place Marker</li>
-      </ul>
+      <div>
+        <Tooltip
+          placement="top"
+          target="context-menu-list"
+          isOpen={isCopied}
+          toggle={() => setIsCopied(!isCopied)}
+          delay={{ show: 0, hide: 300 }}
+        >
+          <p>COPIED</p>
+        </Tooltip>
+        <ul
+          className="context-menu"
+          style={{ top: anchorPoint.y, left: anchorPoint.x }}
+          id="context-menu-list"
+        >
+          <li
+            onClick={copyCoordsToClipboard(coord)}
+          >
+            Copy Coordinates to Clipboard
+          </li>
+          <li>Start a Measurement</li>
+          <li>Add a Place Marker</li>
+        </ul>
+      </div>
     );
   }
   return <></>;
-};
+}
 
-export default ContextMenu;
+function mapStateToProps(state) {
+  const {
+    map,
+  } = state;
+
+  return {
+    map,
+  };
+}
+
+// ContextMenu.propTypes = {
+//   map: PropTypes.object,
+// };
+
+export default connect(
+  mapStateToProps,
+)(ContextMenu);
