7ede0f2c53292786e85dc9c21dc47a9909c19eba
Jason Kent <jasontkent@gmail.com>
2022-03-09 14:28:47 -0700

    WV-2218
    - pass store so we use latest state
    - fix memoize fn

--- web/js/map/compare/opacity.js
+++ web/js/map/compare/opacity.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OpacitySlider from '../../components/compare/opacity-slider';
-import { memoizedDateMonthAbbrev } from '../../modules/compare/selectors';
+import { getCompareDates } from '../../modules/compare/selectors';
 import util from '../../util/util';
 
 const { events } = util;
@@ -10,15 +10,16 @@ let slider;
 let value = 50;
 
 export default class Opacity {
-  constructor(olMap, state, eventListenerStringObj, valueOverride) {
+  constructor(olMap, store, eventListenerStringObj, valueOverride) {
     this.map = olMap;
     this.sliderCase = document.createElement('div');
     value = Number(valueOverride) || value;
-    this.create(state);
+    this.create(store);
   }
 
-  create(state) {
-    const { dateA, dateB } = memoizedDateMonthAbbrev(state)();
+  create(store) {
+    const state = store.getState();
+    const { dateA, dateB } = getCompareDates(state);
     this.dateA = dateA;
     this.dateB = dateB;
     slider = this.createSlider(this.map.getLayers().getArray());
@@ -28,11 +29,12 @@ export default class Opacity {
   /**
    * Refresh secondLayer layer group (after date change for example)
    */
-  update(state) {
-    const { dateA, dateB } = memoizedDateMonthAbbrev(state)();
+  update(store) {
+    const state = store.getState();
+    const { dateA, dateB } = getCompareDates(state);
     if (dateA !== this.dateA || dateB !== this.dateB) {
       this.destroy();
-      this.create(state);
+      this.create(store);
     } else {
       [this.firstLayer, this.secondLayer] = this.map.getLayers().getArray();
       this.oninput(value);
