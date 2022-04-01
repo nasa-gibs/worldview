e196735181c4f276190ed5c106f7114d667a5b82
Jason Hurley <jwhurley1@gmail.com>
2022-03-31 21:15:22 -0400

    -Added e2e testing for each context menu item

---
+++ e2e/features/ui/context-menu-test.js
@@ -0,0 +1,91 @@
+const reuseables = require('../../reuseables/skip-tour.js');
+const selectors = require('../../reuseables/selectors.js');
+
+const {
+  contextMenu,
+  contextMenuAddMarker,
+  contextMenuDistance,
+  contextMenuArea,
+  contentMenuChangeUnits,
+  geoMeasurementTooltip,
+  measureBtn,
+  unitOfMeasureToggle,
+} = selectors;
+
+const TIME_LIMIT = 1000;
+
+function openContextMenu(c, startX, startY) {
+  c.moveToElement('#wv-map-geographic', startX, startY); // 31.8636°, -102.1328°  Midland, Texas
+  c.mouseButtonClick('right');
+  c.waitForElementVisible(contextMenu);
+}
+
+function createDistanceMeasurement(c, [startX, startY], [endX, endY]) {
+  c.waitForElementVisible('#measurement-alert', TIME_LIMIT);
+  c.moveToElement('#wv-map-geographic', startX, startY)
+    .mouseButtonClick(0);
+  c.pause(200);
+  c.moveToElement('#wv-map-geographic', endX, endY)
+    .mouseButtonClick(0)
+    .mouseButtonClick(0)
+    .pause(100);
+}
+
+module.exports = {
+  before: (c) => {
+    reuseables.loadAndSkipTour(c, TIME_LIMIT);
+  },
+
+  'Verify context menu opens on right-click': (c) => {
+    openContextMenu(c, 500, 200);
+    c.expect.element(contextMenu).to.be.present;
+  },
+
+  'Adding a marker after clicking "Add Place Marker" in context menu': (c) => {
+    c.moveToElement(contextMenuAddMarker, 10, 10);
+    c.mouseButtonClick('left');
+    c.waitForElementVisible('#marker-pin');
+  },
+
+  'Creating a distance measurement after clicking "Measure Distance" in context menu': (c) => {
+    openContextMenu(c, 525, 200);
+    c.click(contextMenuDistance);
+    c.pause(200);
+    createDistanceMeasurement(c, [400, 200], [450, 300]);
+    c.waitForElementVisible(geoMeasurementTooltip, TIME_LIMIT);
+    c.expect.elements(geoMeasurementTooltip).count.to.equal(1);
+  },
+
+  'Creating a area measurement after clicking "Measure Area" in context menu': (c) => {
+    openContextMenu(c, 550, 200);
+    c.click(contextMenuArea);
+    c.waitForElementVisible('#measurement-alert', TIME_LIMIT, (el) => {
+      c.moveTo(null, -250, 10);
+      c.mouseButtonClick(0);
+      c.pause(200);
+      c.moveTo(null, 0, 100);
+      c.mouseButtonClick(0);
+      c.pause(200);
+      c.moveTo(null, 100, 0);
+      c.mouseButtonClick(0);
+      c.pause(200);
+      c.moveTo(null, 0, -100);
+      c.mouseButtonClick(0);
+      c.mouseButtonClick(0);
+      c.waitForElementVisible(geoMeasurementTooltip, TIME_LIMIT);
+      c.expect.elements(geoMeasurementTooltip).count.to.equal(2);
+    });
+  },
+
+  'Change measurement units to miles after click "Change Units to mi" in context menu': (c) => {
+    openContextMenu(c, 575, 200);
+    c.click(contentMenuChangeUnits);
+    c.click(measureBtn);
+    c.waitForElementVisible(unitOfMeasureToggle);
+    c.assert.containsText(unitOfMeasureToggle, 'mi');
+  },
+
+  after: (c) => {
+    c.end();
+  },
+};
