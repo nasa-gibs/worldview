2ac07b23a041dead0ff65a45b37585e553da96a9
Benjaki2 <Benjaki2@users.noreply.github.com>
2022-06-30 12:41:08 -0400

    WV-2317: Add Ability To manually input image download extents (#3898)
    * Add image download coord download #2317
    * Update react-image-crop version
    * Fix input coord issues
    * Fix lat-long label  issue
    * Bring top right selection above bottom left
    * Fix onclick exit event
    * Add image-download input e2e tests
    * #wv-2317 swap lat for long position, image inputs
    * #wv-2317 remove state to hold boundaries instead of latlong
    * remove getter
    * #wv-2317 fix NaN bug
    * #wv-2317 replace bg color with border
    * fix scss lint error
    * Fix snapshot click-out event #wv-2317
    * make cropper a functional component
    * #wv-2317 Update invalid-test font
    * #wv-2317 fix snapshots over-dateline error
    * #wv-2317 remove useEffect to prevent react error
    * fix lint errors
    * #wv-3217 Fix crop update issue
    * #wv-2316 Add image download select globe option
    * #wv-2316 add e2e tests
    * remove unused var
    * #wv-2317 Changes based on feedback
    * #wv-2317 Add try/catch in input verify
    * #wv-2317 Use OL Lat long in inputs
    * #wv-2317 Add dateline during image download
    * add proptype
    * #wv-2317 Fix over-dateline input conversion
    * #wv-2317 make crop show ol coords
    * fix crop box shifting bug
    * fix tests, revert crop box on uncheck
    * fix vector interaction/mouseover issue
    * extra wait
    
    Co-authored-by: Jason Kent <jasontkent@gmail.com>

--- web/js/components/util/checkbox.js
+++ web/js/components/util/checkbox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import HoverTooltip from './hover-tooltip';
 /*
  * A checkbox component
  * @class Checkbox
@@ -8,31 +8,39 @@ import PropTypes from 'prop-types';
  */
 export default function Checkbox (props) {
   const {
-    onCheck, checked, isRound, color, classNames, id, name, title, label, children,
+    onCheck, checked, isRound, color, tooltipPlacement, classNames, id, name, title, label, children, disabled,
   } = props;
 
   const roundClassName = isRound ? 'wv-checkbox-round ' : '';
   const defaultClassName = 'wv-checkbox ';
   const checkedClassName = checked ? 'checked ' : '';
-  const caseClassName = defaultClassName + roundClassName + checkedClassName + color;
-
+  const disabledClassName = disabled ? ' disabled' : '';
+  const caseClassName = defaultClassName + roundClassName + checkedClassName + color + disabledClassName;
+  const showDisabledToolTip = disabled && id && title;
   return (
-    <div className={caseClassName}>
-      <input
-        type="checkbox"
-        id={id}
-        title={title}
-        name={name}
-        checked={checked}
-        className={classNames}
-        onChange={onCheck}
-        tabIndex="0"
-      />
-      {children}
-      <label htmlFor={id}>
-        <span>{label}</span>
-      </label>
-    </div>
+    <>
+      {showDisabledToolTip && (
+        <>
+          <HoverTooltip target={`#${id}-case`} labelText={title} placement={tooltipPlacement} />
+        </>
+      )}
+      <div id={`${id}-case`} className={caseClassName}>
+        <input
+          type="checkbox"
+          id={id}
+          title={showDisabledToolTip ? '' : title}
+          name={name}
+          checked={checked}
+          className={classNames}
+          onChange={showDisabledToolTip ? () => null : onCheck}
+          tabIndex="0"
+        />
+        {children}
+        <label htmlFor={id}>
+          <span>{label}</span>
+        </label>
+      </div>
+    </>
   );
 }
 Checkbox.defaultProps = {
@@ -40,12 +48,16 @@ Checkbox.defaultProps = {
   color: '',
   isRound: false,
   onCheck: null,
+  disabled: false,
+  tooltipPlacement: 'bottom',
 };
 
 Checkbox.propTypes = {
   checked: PropTypes.bool,
   children: PropTypes.node,
   classNames: PropTypes.string,
+  disabled: PropTypes.bool,
+  tooltipPlacement: PropTypes.string,
   color: PropTypes.string,
   id: PropTypes.string.isRequired,
   isRound: PropTypes.bool,
