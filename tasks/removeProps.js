616e953c5fe12e2a11849c38d48605ed17ced73a
Jason Kent <jasontkent@gmail.com>
2022-07-11 10:58:26 -0600

    remove unused products props

--- tasks/removeProps.js
+++ tasks/removeProps.js
@@ -3,7 +3,7 @@ const dir = require('node-dir');
 
 const SOURCE_DIR = './config/default/common/config/wv.json/layers/';
 const removeKeys = [
-  // 'id',
+  'product',
   'title',
   'subtitle',
   'inactive',
