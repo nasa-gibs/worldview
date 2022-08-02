f36fca0c09f10d9d044d268d3c1e2c51b10e7a86
ryanweiler92 <ryanweiler92@gmail.com>
2022-08-02 08:06:25 -0400

    Revert "connect ol-coordinates to update settings/coordinateFormat in redux store"
    This reverts commit 196b05daf1e7ff07d1b432899df7eef5ccc80018.

--- web/js/components/map/ol-coordinates.js
+++ web/js/components/map/ol-coordinates.js
@@ -37,6 +37,7 @@ class OlCoordinates extends React.Component {
     this.mouseOut = lodashThrottle(this.mouseOut.bind(this), 200, options);
     this.changeFormat = this.changeFormat.bind(this);
     this.setInitFormat = this.setInitFormat.bind(this);
+
   }
 
   componentDidMount() {
@@ -105,9 +106,8 @@ class OlCoordinates extends React.Component {
   }
 
   changeFormat(format) {
-    const { changeCoordinateFormatAction } = this.props;
-    changeCoordinateFormatAction(format);
     util.setCoordinateFormat(format);
+    // changeCoordinateFormatPls(format);
     const width = getContainerWidth(format);
     this.setState({
       format,
@@ -119,7 +119,7 @@ class OlCoordinates extends React.Component {
     const {
       hasMouse, format, latitude, longitude, crs, width,
     } = this.state;
-    const { show } = this.props;
+    const { show, changeCoordinateFormatPls } = this.props;
     return (
       <div id="ol-coords-case" className="wv-coords-container" style={{ width }}>
         {hasMouse && show && (
@@ -130,6 +130,7 @@ class OlCoordinates extends React.Component {
               longitude={longitude}
               crs={crs}
               onFormatChange={this.changeFormat}
+              changeCoordinateFormatPls={changeCoordinateFormatPls}
             />
             {latitude && latitude && (
               <UncontrolledTooltip placement="bottom" target="ol-coords-case">
@@ -143,7 +144,10 @@ class OlCoordinates extends React.Component {
   }
 }
 
+
+
 function mapStateToProps(state) {
+  console.log('dadaboodadad')
   const { settings } = state;
   const { coordinateFormat } = settings;
   return {
@@ -152,7 +156,7 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  changeCoordinateFormatAction: (value) => {
+  changeCoordinateFormatPls: (value) => {
   dispatch(changeCoordinateFormat(value))
   }
 })
@@ -161,6 +165,8 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps,
 )(OlCoordinates)
+
+
 
 OlCoordinates.propTypes = {
   show: PropTypes.bool,
