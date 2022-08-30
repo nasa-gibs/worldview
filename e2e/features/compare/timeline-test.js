21a12ffa52d06203317c7b41e83fb13930fffc45
Jason Kent <jasontkent@gmail.com>
2022-08-30 11:00:24 -0600

    Update timeline-test

--- e2e/features/compare/timeline-test.js
+++ e2e/features/compare/timeline-test.js
@@ -1,7 +1,6 @@
 const reuseables = require('../../reuseables/skip-tour.js');
 const localSelectors = require('../../reuseables/selectors.js');
 const localQueryStrings = require('../../reuseables/querystrings.js');
-const { c } = require('tar');
 
 const draggerA = '.timeline-dragger.draggerA ';
 const draggerB = '.timeline-dragger.draggerB ';
@@ -10,85 +9,81 @@ const dateSelectorMonthInput = '#date-selector-main .input-wrapper-month input';
 const TIME_LIMIT = 20000;
 
 module.exports = {
-  before(client) {
-    reuseables.loadAndSkipTour(client, TIME_LIMIT);
+  before(c) {
+    reuseables.loadAndSkipTour(c, TIME_LIMIT);
   },
   // load A|B and verify that it is active
-  'A|B is loaded': function(client) {
-    client.url(client.globals.url + localQueryStrings.swipeAndAIsActive);
-    client.waitForElementVisible(localSelectors.swipeDragger, TIME_LIMIT);
+  'A|B is loaded': function(c) {
+    c.url(c.globals.url + localQueryStrings.swipeAndAIsActive);
+    c.waitForElementVisible(localSelectors.swipeDragger, TIME_LIMIT);
   },
-  'Verify that A|B draggers are visible': function(client) {
-    client.expect.element(draggerA).to.be.visible;
-    client.expect.element(draggerB).to.be.visible;
+  'Verify that A|B draggers are visible': function(c) {
+    c.expect.element(draggerA).to.be.visible;
+    c.expect.element(draggerB).to.be.visible;
   },
-  'Dragging active dragger updates date': function(client) {
-    client.assert.attributeContains(dateSelectorDayInput, 'value', '17');
-    client.assert.attributeContains(dateSelectorMonthInput, 'value', 'AUG');
-    client
-      .useCss()
-      .moveToElement(draggerA, 0, 0)
-      .pause(500)
-      .click(draggerA)
-      .mouseButtonDown(0)
-      .pause(500)
-      .moveToElement(draggerB, 100, 30)
-      .mouseButtonUp(0)
-      .pause(500);
-    client.getValue(dateSelectorDayInput, (dayResult) => {
-      client.getValue(dateSelectorMonthInput, function(monthResult) {
+  'Dragging active dragger updates date': function(c) {
+    c.assert.attributeContains(dateSelectorDayInput, 'value', '17');
+    c.assert.attributeContains(dateSelectorMonthInput, 'value', 'AUG');
+    c.perform(function() {
+      const actions = this.actions({ async: true });
+      const dragA = c.findElement(draggerA);
+      return actions.dragAndDrop(dragA, { x: 100, y: 0 });
+    });
+    c.getValue(dateSelectorDayInput, (dayResult) => {
+      c.getValue(dateSelectorMonthInput, function(monthResult) {
         const result = monthResult.value.concat(dayResult.value);
         this.assert.notEqual('AUG17', result);
       });
     });
   },
-  'Clicking inactive dragger updates active state': function(client) {
-    client.assert.cssClassPresent(localSelectors.aTab, 'active');
-    client
-      .useCss()
-      .moveToElement(draggerB, 20, 20)
-      .mouseButtonDown(0)
-      .mouseButtonUp(0);
+  'Clicking inactive dragger updates active state': function(c) {
+    c.assert.hasClass(localSelectors.aTab, 'active');
+    c.perform(function() {
+      const actions = this.actions({ async: true });
+      const dragB = c.findElement(draggerB);
+      return actions
+        .move({ origin: dragB })
+        .click(dragB);
+    });
     // Reference labels were not active in A but are in B
-    client.waitForElementVisible(
+    c.waitForElementVisible(
       '#activeB-Reference_Features_15m',
       TIME_LIMIT,
       () => {
-        client.assert.attributeContains(dateSelectorDayInput, 'value', '16');
+        c.assert.attributeContains(dateSelectorDayInput, 'value', '16');
       },
     );
   },
-  'Dragging B dragger updates date in label': function(client) {
-    client.useCss().assert.containsText(localSelectors.bTab, '2018 AUG 16');
-    client
-      .useCss()
-      .moveToElement(draggerB, 20, 20)
-      .mouseButtonDown(0)
-      .moveToElement(draggerA, -100, 0)
-      .mouseButtonUp(0)
-      .pause(2000);
-    client.getText(localSelectors.bTab, function(result) {
+  'Dragging B dragger updates date in label': function(c) {
+    c.useCss().assert.textContains(localSelectors.bTab, '2018 AUG 16');
+    c.perform(function() {
+      const actions = this.actions({ async: true });
+      const dragB = c.findElement(draggerB);
+
+      return actions.dragAndDrop(dragB, { x: 100, y: 0 });
+    });
+    c.getText(localSelectors.bTab, function(result) {
       this.assert.notEqual('B: 2018 AUG 16', result.value);
     });
   },
-  'Deactivate A|B is no longer active': function(client) {
-    client.click(localSelectors.compareButton);
-    client.waitForElementNotPresent(
+  'Deactivate A|B is no longer active': function(c) {
+    c.click(localSelectors.compareButton);
+    c.waitForElementNotPresent(
       localSelectors.bTab,
       TIME_LIMIT,
       () => {
-        client
+        c
           .useCss()
-          .assert.containsText(
+          .assert.textContains(
             localSelectors.compareButton,
             'Start Comparison',
           );
-        client.expect.element(draggerA).to.not.be.present;
-        client.expect.element(draggerB).to.be.visible;
+        c.expect.element(draggerA).to.not.be.present;
+        c.expect.element(draggerB).to.be.visible;
       },
     );
   },
-  after(client) {
-    client.end();
+  after(c) {
+    c.end();
   },
 };
