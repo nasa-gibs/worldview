3e9f46e5ccbe80f6d4170e3d16bde510efe54a93
Jason Kent <jasontkent@gmail.com>
2022-09-16 09:28:24 -0600

    don't preload granule layers

--- web/js/modules/map/util.js
+++ web/js/modules/map/util.js
@@ -289,6 +289,9 @@ export async function promiseImageryForTime(state, date, activeString) {
   } = map.ui;
   const layers = getActiveVisibleLayersAtDate(state, date, activeString);
   await Promise.all(layers.map(async (layer) => {
+    if (layer.type === 'granule') {
+      return Promise.resolve();
+    }
     const options = { date, group: activeString };
     const key = layerKey(layer, options, state);
     const layerGroup = cache.getItem(key) || await createLayer(layer, options);
