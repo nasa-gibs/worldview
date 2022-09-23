6b3c6beaf466de300e87e352542b2716182315fd
Jason Kent <jasontkent@gmail.com>
2022-09-23 11:04:30 -0600

    WV-2427
    * Drop python 2 check, assume python 3
    * Update pipenv version to latest

--- tasks/pythonInstall.sh
+++ tasks/pythonInstall.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 # Install python dependencies
-PYTHON_VERSION="$(python -V 2>&1)"
-
-if command -v python3 &>/dev/null; then
-  echo "Installing pipenv and dependencies with python3"
-  python3 -m pip install pipenv==2020.11.15
-  python3 -m pipenv install
-elif [[ $PYTHON_VERSION = *"Python 3"* ]]; then
-  echo "Installing pipenv and dependencies  with python"
-  pip install pipenv
-  pipenv install
-else
-  echo "Please install or update python v3"
-fi
+echo "Installing pipenv and dependencies with python3"
+python3 -m pip install pipenv==2022.9.21
+python3 -m pipenv install
