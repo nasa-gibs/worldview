6cf4672c7e9bcc23f5e7c8030345098a2f507b3d
ryanweiler92 <ryanweiler92@gmail.com>
2022-10-04 16:06:25 -0400

    update e2e test

--- e2e/features/animation/mobile-animation-test.js
+++ e2e/features/animation/mobile-animation-test.js
@@ -1,9 +1,4 @@
 const skipTour = require('../../reuseables/skip-tour.js');
-const localSelectors = require('../../reuseables/selectors.js');
-
-const {
-  mobileAnimationWidget,
-} = localSelectors;
 
 const TIME_LIMIT = 1000;
 
@@ -11,25 +6,25 @@ module.exports = {
   before(c) {
     skipTour.loadAndSkipTour(c, TIME_LIMIT);
     c.url(`${c.globals.url}'?v=-82.73697802714918,27.137724977419197,-71.17181984959728,52.16591344371096&lg=false&t=2022-01-07-T15%3A27%3A49Z`);
-    c.setWindowSize(748, 1024); // iPad Mini dimensions
+    c.setWindowSize(450, 850);
     c.pause(1000);
   },
 
   'Mobile animate button opens widget': (c) => {
     c.useCss().click('.mobile-animate-button');
     c.pause(300);
-    c.waitForElementPresent(mobileAnimationWidget, TIME_LIMIT);
+    c.waitForElementPresent('.wv-animation-widget', TIME_LIMIT);
     c.expect.element('.custom-interval-delta-input').to.have.value.that.equals('1');
     c.expect.element('.dropdown-toggle').text.to.equal('DAY');
   },
 
   'Minimizing mobile animation widget opens collapsed animation widget': (c) => {
     c.useCss().click('.wv-minimize');
-    c.waitForElementVisible('#collapsed-animate-widget-tablet-portrait', TIME_LIMIT);
+    c.waitForElementVisible('#collapsed-animate-widget-phone-portrait', TIME_LIMIT);
   },
 
   'Playing the animation changes the date of the mobile date picker': (c) => {
-    c.useCss().click('#collapsed-animate-widget-tablet-portrait');
+    c.useCss().click('#collapsed-animate-widget-phone-portrait');
     c.pause(3000);
     c.expect.element('.mobile-date-picker-select-btn-text span').text.to.equal('2022 JAN 17');
   },
@@ -37,7 +32,7 @@ module.exports = {
   'Pressing the animation button brings up the mobile animation widget with the same information': (c) => {
     c.useCss().click('.mobile-animate-button');
     c.pause(300);
-    c.waitForElementVisible(mobileAnimationWidget, TIME_LIMIT);
+    c.waitForElementVisible('.wv-animation-widget', TIME_LIMIT);
     c.expect.element('#mobile-animation-start-date .mobile-date-picker-select-btn span').text.to.equal('2022 JAN 07');
     c.expect.element('#mobile-animation-end-date .mobile-date-picker-select-btn span').text.to.equal('2022 JAN 17');
   },
