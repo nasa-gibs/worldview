4a8cecfa0099fc97cdfa55538ef06dd367f8f468
ZachTRice <zachtrice@gmail.com>
2022-12-12 11:02:34 -0500

    Add eslint for nodejs tasks

--- tasks/generateLayergroup.js
+++ tasks/generateLayergroup.js
@@ -1,12 +1,12 @@
-const _ = require('lodash');
-const fs = require('fs');
-const dir = require('node-dir');
+const _ = require('lodash')
+const fs = require('fs')
+const dir = require('node-dir')
 
-const SOURCE_DIR = './config/default/common/config/wv.json/layers/';
-const WV_JSON_PATH = './build/options/config/wv.json';
+const SOURCE_DIR = './config/default/common/config/wv.json/layers/'
+const WV_JSON_PATH = './build/options/config/wv.json'
 
-const measurementsMap = {};
-const measurementsArray = [];
+const measurementsMap = {}
+const measurementsArray = []
 
 // This is a set of layer IDs which we do not wish to generate (or re-generate)
 const EXCEPTIONS = [
@@ -20,25 +20,25 @@ const EXCEPTIONS = [
   'AMSRE_Sea_Ice_Brightness_Temp_89V',
   'AMSRE_Sea_Ice_Brightness_Temp_89H',
   'AMSRU2_Sea_Ice_Brightness_Temp_6km_89V',
-  'AMSRU2_Sea_Ice_Brightness_Temp_6km_89H',
-];
+  'AMSRU2_Sea_Ice_Brightness_Temp_6km_89H'
+]
 
 const errCallback = (err) => {
   if (err) {
-    console.log(err);
-    throw err;
+    console.log(err)
+    throw err
   }
-};
+}
 
 function setLayerProp (layer, prop, value) {
-  const featuredMeasurement = prop === 'measurements' && (value && value.includes('Featured'));
+  const featuredMeasurement = prop === 'measurements' && (value && value.includes('Featured'))
   if (!layer || featuredMeasurement) {
-    return;
+    return
   }
   if (!layer[prop]) {
-    layer[prop] = [value];
+    layer[prop] = [value]
   } else if (!layer[prop].includes(value)) {
-    layer[prop].push(value);
+    layer[prop].push(value)
   }
 }
 
@@ -46,64 +46,64 @@ function generateMeasurements (layers, measurementsConfig) {
   _.forEach(measurementsConfig, (measureObj, measureKey) => {
     _.forEach(measureObj.sources, ({ settings = [] }, sourceKey) => {
       settings.forEach((id) => {
-        setLayerProp(layers[id], 'measurements', measureKey);
+        setLayerProp(layers[id], 'measurements', measureKey)
         if (!measurementsArray.includes(measureKey)) {
-          measurementsArray.push(measureKey);
+          measurementsArray.push(measureKey)
         }
-      });
-    });
-  });
+      })
+    })
+  })
   _.forEach(layers, ({ measurements }, id) => {
     // Reduce to a single measurement:
     if (id.toLowerCase().includes('orbit')) {
-      measurementsMap[id] = 'Orbital Track';
+      measurementsMap[id] = 'Orbital Track'
     } else {
       // eslint-disable-next-line prefer-destructuring
-      measurementsMap[id] = measurements[0];
+      measurementsMap[id] = measurements[0]
     }
-  });
+  })
 }
 
 function modifyProps (layer) {
-  const { id } = layer;
-  layer.layergroup = measurementsMap[id];
+  const { id } = layer
+  layer.layergroup = measurementsMap[id]
 
   return {
     [id]: {
-      ...layer,
-    },
-  };
+      ...layer
+    }
+  }
 }
 
-function migrate(filePath) {
-  const pathStrings = filePath.split('/');
-  const fileName = pathStrings[pathStrings.length - 1];
+function migrate (filePath) {
+  const pathStrings = filePath.split('/')
+  const fileName = pathStrings[pathStrings.length - 1]
   if (!fileName.includes('.json')) {
-    return;
+    return
   }
 
-  const layerJson = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-  const fileLayerId = fileName.slice(0, fileName.length - 5);
-  const objPropLayerId = Object.keys(layerJson.layers)[0];
+  const layerJson = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+  const fileLayerId = fileName.slice(0, fileName.length - 5)
+  const objPropLayerId = Object.keys(layerJson.layers)[0]
   if (fileLayerId !== objPropLayerId) {
-    console.log('Mismatched ids!', filePath);
+    console.log('Mismatched ids!', filePath)
   }
-  const layer = layerJson.layers[objPropLayerId];
+  const layer = layerJson.layers[objPropLayerId]
 
   if (EXCEPTIONS.includes(layer.id)) {
-    return;
+    return
   }
 
   fs.writeFile(
     `${filePath}`,
     JSON.stringify({ layers: modifyProps(layer) }, null, 2),
-    errCallback,
-  );
+    errCallback
+  )
 }
 
 dir.files(SOURCE_DIR, (err, files) => {
-  if (err) throw err;
-  const wvJson = JSON.parse(fs.readFileSync(WV_JSON_PATH, 'utf-8'));
-  generateMeasurements(wvJson.layers, wvJson.measurements);
-  files.forEach(migrate);
-});
+  if (err) throw err
+  const wvJson = JSON.parse(fs.readFileSync(WV_JSON_PATH, 'utf-8'))
+  generateMeasurements(wvJson.layers, wvJson.measurements)
+  files.forEach(migrate)
+})
--- tasks/node/getCapabilities.js
+++ tasks/node/getCapabilities.js
@@ -1,16 +1,14 @@
-#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const yargs = require('yargs')
+const console = require('console')
+const request = require('axios').default
+const convert = require('xml-js')
+const { promisify } = require('util')
+const stream = require('stream')
 
-const fs = require('fs');
-const path = require('path');
-const yargs = require('yargs');
-const console = require('console');
-const request = require('axios').default;
-const convert = require('xml-js');
-const { promisify } = require('util');
-const stream = require('stream');
-
-const finished = promisify(stream.finished);
-const prog = path.basename(__filename);
+const finished = promisify(stream.finished)
+const prog = path.basename(__filename)
 // const baseDir = path.join(__dirname, '..');
 
 const options = yargs
@@ -19,167 +17,166 @@ const options = yargs
     demandOption: true,
     alias: 'c',
     type: 'string',
-    description: 'config file',
+    description: 'config file'
   })
   .option('getcapabilities', {
     demandOption: true,
     alias: 'g',
     type: 'string',
-    description: 'getcapabilities file',
+    description: 'getcapabilities file'
   })
-  .epilog('Pulls GetCapabilities XML and linked metadata from configured locations');
+  .epilog('Pulls GetCapabilities XML and linked metadata from configured locations')
 
 // import config file from ./config/default/release/config.json
 
-const { argv } = options;
+const { argv } = options
 if (!argv.config && !argv.getcapabilities) {
-  console.error('Invalid number of arguments');
-  process.exit(1);
+  throw new Error('Invalid number of arguments')
 }
-const configFile = argv.config;
-const outputDir = argv.getcapabilities;
-const colormaps = {};
-const vectorstyles = {};
-const vectordata = {};
-const colormapsDir = path.join(outputDir, 'colormaps');
-const vectorstylesDir = path.join(outputDir, 'vectorstyles');
-const vectordataDir = path.join(outputDir, 'vectordata');
+const configFile = argv.config
+const outputDir = argv.getcapabilities
+const colormaps = {}
+const vectorstyles = {}
+const vectordata = {}
+const colormapsDir = path.join(outputDir, 'colormaps')
+const vectorstylesDir = path.join(outputDir, 'vectorstyles')
+const vectordataDir = path.join(outputDir, 'vectordata')
 
-const configData = fs.readFileSync(configFile);
-const config = JSON.parse(configData);
+const configData = fs.readFileSync(configFile)
+const config = JSON.parse(configData)
 
 async function main () {
   if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
+    fs.mkdirSync(outputDir)
   }
 
-  await GetCapabilities();
+  await getCapabilities()
 
+  if (colormaps) {
+    await gatherProcess(colormaps, 'colormaps', colormapsDir, '.xml')
+  }
+  if (vectorstyles) {
+    await gatherProcess(vectorstyles, 'vectorstyles', vectorstylesDir, '.json')
+  }
+  if (vectordata) {
+    await gatherProcess(vectordata, 'vectordatas', vectordataDir, '.json')
+  }
   // process.exit();
 }
 
-async function GetCapabilities() {
+async function getCapabilities () {
   // Download each GC xml using the "from" attribute and put it in the "to" location
   if (Object.prototype.hasOwnProperty.call(config, 'wv-options-fetch')) {
-    const fetchValues = config['wv-options-fetch'];
-    Object.entries(fetchValues).forEach(async ([key, value]) => {
-      const inputFile = value.from;
-      const outputFile = `${outputDir}/${value.to}`;
+    const fetchValues = config['wv-options-fetch']
+    for (const value of fetchValues) {
+      const inputFile = value.from
+      const outputFile = `${outputDir}/${value.to}`
 
-      await fetchConfigs(inputFile, outputFile);
-      await processGetCapabilities(outputFile);
-
-      if (colormaps) {
-        await gatherProcess(colormaps, 'colormaps', colormapsDir, '.xml');
-      }
-      if (vectorstyles) {
-        await gatherProcess(vectorstyles, 'vectorstyles', vectorstylesDir, '.json');
-      }
-      if (vectordata) {
-        await gatherProcess(vectordata, 'vectordatas', vectordataDir, '.json');
-      }
-    });
+      await fetchConfigs(inputFile, outputFile)
+      await processGetCapabilities(outputFile)
+    }
   }
 }
 
-async function fetchConfigs(inputFile, outputFile) {
-  const writer = await fs.createWriteStream(outputFile);
+async function fetchConfigs (inputFile, outputFile) {
+  const writer = await fs.createWriteStream(outputFile)
   return request({
     method: 'get',
     url: inputFile,
     responseType: 'stream',
+    timeout: 10000
   }).then(async (response) => {
-    await response.data.pipe(writer);
-    return finished(writer);
-  });
+    await response.data.pipe(writer)
+    return finished(writer)
+  })
 }
 
-async function handleException(error, link) {
-  console.warn(`\n ${prog} WARN: Unable to fetch ${link} ${error}`);
+async function handleException (error, link) {
+  console.warn(`\n ${prog} WARN: Unable to fetch ${link} ${error}`)
 }
 
-async function processVectorData(layer) {
+async function processVectorData (layer) {
   if (layer['ows:Metadata']) {
     Object.values(layer['ows:Metadata']).forEach((item) => {
-      const schemaVersion = item._attributes['xlink:role'];
+      const schemaVersion = item._attributes['xlink:role']
       if (schemaVersion === 'http://earthdata.nasa.gov/gibs/metadata-type/layer/1.0') {
-        const vectorDataLink = item._attributes['xlink:href'];
-        const vectorDataFile = path.basename(vectorDataLink);
-        const vectorDataId = path.parse(vectorDataFile).name;
-        vectordata[vectorDataId] = vectorDataLink;
+        const vectorDataLink = item._attributes['xlink:href']
+        const vectorDataFile = path.basename(vectorDataLink)
+        const vectorDataId = path.parse(vectorDataFile).name
+        vectordata[vectorDataId] = vectorDataLink
       }
-    });
+    })
   }
 }
 
-async function processLayer(layer) {
-  const ident = layer['ows:Identifier']._text;
+async function processLayer (layer) {
+  const ident = layer['ows:Identifier']._text
   if (layer['ows:Metadata']) {
     if (config.skipPalettes) {
-      console.warn(`${prog}: WARN: Skipping palette for ${ident} \n`);
+      console.warn(`${prog}: WARN: Skipping palette for ${ident} \n`)
     } else {
       Object.values(layer['ows:Metadata']).forEach((item) => {
-        const schemaVersion = item._attributes['xlink:role'];
+        const schemaVersion = item._attributes['xlink:role']
         if (schemaVersion === 'http://earthdata.nasa.gov/gibs/metadata-type/colormap/1.3') {
-          const colormapLink = item._attributes['xlink:href'];
-          const colormapFile = path.basename(colormapLink);
-          const colormapId = path.parse(colormapFile).name;
-          colormaps[colormapId] = colormapLink;
+          const colormapLink = item._attributes['xlink:href']
+          const colormapFile = path.basename(colormapLink)
+          const colormapId = path.parse(colormapFile).name
+          colormaps[colormapId] = colormapLink
         } else if (schemaVersion === 'http://earthdata.nasa.gov/gibs/metadata-type/mapbox-gl-style/1.0') {
-          const vectorStyleLink = item._attributes['xlink:href'];
-          const vectorStyleFile = path.basename(vectorStyleLink);
-          const vectorStyleId = path.parse(vectorStyleFile).name;
-          vectorstyles[vectorStyleId] = vectorStyleLink;
+          const vectorStyleLink = item._attributes['xlink:href']
+          const vectorStyleFile = path.basename(vectorStyleLink)
+          const vectorStyleId = path.parse(vectorStyleFile).name
+          vectorstyles[vectorStyleId] = vectorStyleLink
         }
-      });
+      })
     }
   }
 }
 
-async function processGetCapabilities(outputFile) {
-  const gcXML = fs.readFileSync(outputFile, { encoding: 'utf-8' });
-  let gc = convert.xml2json(gcXML, { compact: true, spaces: 2 });
-  gc = JSON.parse(gc);
+async function processGetCapabilities (outputFile) {
+  const gcXML = fs.readFileSync(outputFile, { encoding: 'utf-8' })
+  let gc = convert.xml2json(gcXML, { compact: true, spaces: 2 })
+  gc = JSON.parse(gc)
 
   try {
     if (!gc.Capabilities || !gc.Capabilities.Contents) {
-      console.error(`error: ${outputFile}: no layers`);
-      process.exit(1);
+      console.error(`error: ${outputFile}: no layers`)
+      process.exit(1)
     }
 
-    const layers = gc.Capabilities.Contents.Layer;
+    const layers = gc.Capabilities.Contents.Layer
 
     Object.values(layers).forEach((layer) => {
-      processLayer(layer);
-      processVectorData(layer);
-    });
+      processLayer(layer)
+      processVectorData(layer)
+    })
   } catch (error) {
-    console.error(`ERROR: ${outputFile}: ${error}`);
-    process.exit(1);
+    console.error(`ERROR: ${outputFile}: ${error}`)
+    process.exit(1)
   }
 }
 
-async function processMetadata(link, dir, ext) {
+async function processMetadata (link, dir, ext) {
   try {
-    const outputFile = path.join(dir, path.basename(link));
+    const outputFile = path.join(dir, path.basename(link))
     if (link.endsWith(ext)) {
-      await fetchConfigs(link, outputFile);
+      await fetchConfigs(link, outputFile)
     }
   } catch (error) {
-    handleException(error, link);
+    handleException(error, link)
   }
 }
 
-async function gatherProcess(type, typeStr, dir, ext) {
-  console.warn(`${prog}: Fetching ${Object.keys(type).length} ${typeStr}`);
+async function gatherProcess (type, typeStr, dir, ext) {
+  console.warn(`${prog}: Fetching ${Object.keys(type).length} ${typeStr}`)
   if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+    fs.mkdirSync(dir)
   }
   Object.values(type).forEach(async (link) => {
-    await processMetadata(link, dir, ext);
-  });
+    await processMetadata(link, dir, ext)
+  })
 }
 
 main().catch((err) => {
-  console.error(err.stack);
-});
+  console.error(err.stack)
+})
