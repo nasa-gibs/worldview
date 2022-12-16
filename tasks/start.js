c6a920b396a34687b09752b22913476f67d3dadb
ZachTRice <zachtrice@gmail.com>
2022-12-16 13:34:22 -0500

    fix relative paths

--- tasks/start.js
+++ tasks/start.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(compression());
-app.use(express.static(path.join(__dirname, '../web')));
+// app.use(express.static(path.join(__dirname, '../web')));
+app.use('/test', express.static(path.join(__dirname, '../build/worldview'))); // test dist in a "test" sub directory
 app.set('port', port);
 app.listen(port, () => {
   console.log(`Worldview is available at http://localhost:${port}`);
