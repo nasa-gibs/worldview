0a415c46696882409694c13cf272c01420bc6f72
ZachTRice <zachtrice@gmail.com>
2022-12-21 14:33:57 -0500

    Fix most issues with extractConfigFromWMTS and processTemporalLayer

--- tasks/build/processTemporalLayer.js
+++ tasks/build/processTemporalLayer.js
@@ -1,22 +1,29 @@
 const moment = require('moment')
-const re = require('regex-parser')
 
 function toList (val) {
   return val instanceof Array ? val : [val]
 }
 
-function processTemporal (wvLayer, value) {
+class ValueError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ValueError'
+  }
+}
+
+async function processTemporalLayer (wvLayer, value) {
   const dateFormat = 'YYYY-MM-DD'
   const timeFormat = 'HH:mm:ss'
   const dateTimeFormat = 'YYYY-MM-DD HH:mm:ss'
   try {
     const ranges = toList(value)
-    if (ranges[0].includes('T')) {
+    if (ranges && ranges[0] && ranges[0]._text && ranges[0]._text.includes('T')) {
+      console.warn(ranges[0]._text)
       wvLayer.period = 'subdaily'
     } else {
-      if (ranges[0].endsWith('Y')) {
+      if (ranges && ranges[0] && ranges[0]._text && ranges[0]._text.endsWith('Y')) {
         wvLayer.period = 'yearly'
-      } else if (ranges[0].endsWith('M')) {
+      } else if (ranges && ranges[0] && ranges[0]._text && ranges[0]._text.endsWith('M')) {
         wvLayer.period = 'monthly'
       } else {
         wvLayer.period = 'daily'
@@ -28,7 +35,7 @@ function processTemporal (wvLayer, value) {
     const dateRangeEnd = []
     const rangeInterval = []
     for (const range of ranges) {
-      const [start, end, interval] = range.split('/')
+      const [start, end, interval] = range._text.split('/')
       if (
         wvLayer.period === 'daily' ||
         wvLayer.period === 'monthly' ||
@@ -53,9 +60,11 @@ function processTemporal (wvLayer, value) {
           )
         }
         if (interval !== 'P1D') {
-          endDate = moment(endDate).add(moment.duration(interval)).toDate()
+          endDate = moment(endDate).add(moment.duration(interval))
         }
-        rangeInterval.push(re.parse(interval).match)
+        const regex = new RegExp(/\d+/g)
+        const match = regex.exec(interval)
+        rangeInterval.push(match)
       } else {
         // Subdaily Layers
         const startTime = start.replace('T', ' ').replace('Z', '')
@@ -72,7 +81,7 @@ function processTemporal (wvLayer, value) {
           dateRangeEnd.push(endTimeParse.format(`${dateFormat}T${timeFormat}Z`))
         }
 
-        rangeInterval.push(re.search(/\d+/, interval).group())
+        rangeInterval.push(interval.match(/\d+/)[0])
       }
 
       wvLayer.startDate = moment(startDate).format(`${dateFormat}T${timeFormat}Z`)
@@ -97,5 +106,5 @@ function processTemporal (wvLayer, value) {
 }
 
 module.exports = {
-  processTemporal
+  processTemporalLayer
 }
