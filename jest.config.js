bc597879fae4d093e628da4bdf1ab3812b869338
ZachTRice <zachtrice@gmail.com>
2023-01-13 17:38:26 -0500

    Update jest to latest version

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  setupFiles: [
+    'jest-canvas-mock',
+    'jsdom-worker'
+  ],
+  moduleDirectories: [
+    'node_modules'
+  ],
+  moduleNameMapper: {
+    '^googleTagManager$': '<rootDir>/web/js/components/util/google-tag-manager.js'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(ol|ol-mapbox-style|quick-lru|geotiff|@mapbox|node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)/)'
+  ],
+  modulePathIgnorePatterns: [
+    '<rootDir>/build/'
+  ],
+  testEnvironment: 'jsdom'
+}
