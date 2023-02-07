74918596cceb09447876d4101e4f76b3c3b8aee6
Tom Cariello <tomcariello@gmail.com>
2023-02-07 14:27:09 -0500

    Added notranslate class to events-loading-text div to prevent google translate extension from breaking the page.

--- web/js/containers/sidebar/events.js
+++ web/js/containers/sidebar/events.js
@@ -135,8 +135,12 @@ function Events(props) {
       >
         <div id="wv-events">
           {isLoading || hasRequestError ? (
-            <div className="events-loading-text">
-              {hasRequestError && (<FontAwesomeIcon icon="exclamation-triangle" fixedWidth />)}
+            // notranslate included below to prevent Google Translate extension from crashing the page
+            <div className="events-loading-text notranslate">
+              {hasRequestError && (
+                <FontAwesomeIcon icon="exclamation-triangle" fixedWidth />
+              )}
+
               {errorOrLoadingText}
             </div>
           ) : renderEventList()}
