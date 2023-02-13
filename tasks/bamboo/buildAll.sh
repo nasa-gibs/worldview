c6d0efacafba9c6b26e89d7ad2527249ca62efdb
ZachTRice <zachtrice@gmail.com>
2023-02-13 13:19:19 -0500

    Update buildAll.sh to use npm dist from root

--- tasks/bamboo/buildAll.sh
+++ tasks/bamboo/buildAll.sh
@@ -2,19 +2,12 @@
 
 # Bash commands to run in Bamboo to prepare an rpm for deployment
 
-BASE=$(dirname "$0")/..
+BASE=$(dirname "$0")/../..
 
 # Bamboo automatically sets NODE_ENV to production. Override this so that
 # devDependencies are installed.
 npm ci --production=false --ignore-scripts
 node node_modules/.bin/patch-package  --allow-root
-# pipenv run bash $BASE/tasks/distExplict.sh
 
-# Replacement of npm run dist
-# Needed for bamboo to avoid permission errors with npm
-node $BASE/tasks/util/clean.js
-FETCH_GC=1 $BASE/tasks/buildOptions.sh
-$BASE/tasks/buildOptions.sh
-node $BASE/tasks/util/config.js
-NODE_ENV=production npx webpack
-node $BASE/tasks/util/dist.js
+cd $BASE
+npm run dist
