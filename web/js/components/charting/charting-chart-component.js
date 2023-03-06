09968fa3e63070f4998bb2defdc428362253e5b3
Tom Cariello <tomcariello@gmail.com>
2023-03-06 09:41:11 -0500

    Printing generic chart.

--- web/js/components/charting/charting-chart-component.js
+++ web/js/components/charting/charting-chart-component.js
@@ -9,65 +9,72 @@ function ChartingChartComponent (props) {
     liveData,
   } = props;
 
-  const { data } = liveData;
-  //   {
-  //     name: 'January',
-  //     Google: 467,
-  //     Yahoo: 369,
-  //     Bing: 415,
-  //   },
-  //   {
-  //     name: 'February',
-  //     Google: 861,
-  //     Yahoo: 78,
-  //     Bing: 351,
-  //   },
-  //   {
-  //     name: 'March',
-  //     Google: 601,
-  //     Yahoo: 716,
-  //     Bing: 675,
-  //   },
-  //   {
-  //     name: 'April',
-  //     Google: 833,
-  //     Yahoo: 875,
-  //     Bing: 802,
-  //   },
-  //   {
-  //     name: 'May',
-  //     Google: 436,
-  //     Yahoo: 718,
-  //     Bing: 378,
-  //   },
-  //   {
-  //     name: 'June',
-  //     Google: 818,
-  //     Yahoo: 240,
-  //     Bing: 492,
-  //   },
-  //   {
-  //     name: 'July',
-  //     Google: 23,
-  //     Yahoo: 574,
-  //     Bing: 325,
-  //   },
-  // ];
+  // const { data } = liveData;
+  const data = [
+    {
+      name: 'January',
+      Google: 467,
+      Yahoo: 369,
+      Bing: 415,
+    },
+    {
+      name: 'February',
+      Google: 861,
+      Yahoo: 78,
+      Bing: 351,
+    },
+    {
+      name: 'March',
+      Google: 601,
+      Yahoo: 716,
+      Bing: 675,
+    },
+    {
+      name: 'April',
+      Google: 833,
+      Yahoo: 875,
+      Bing: 802,
+    },
+    {
+      name: 'May',
+      Google: 436,
+      Yahoo: 718,
+      Bing: 378,
+    },
+    {
+      name: 'June',
+      Google: 818,
+      Yahoo: 240,
+      Bing: 492,
+    },
+    {
+      name: 'July',
+      Google: 23,
+      Yahoo: 574,
+      Bing: 325,
+    },
+  ];
+
   const lineColors = ['#8884D8', '#82CA9D', 'orange', 'pink'];
-  const dataKeys = [];
-  for (let i = 0; i < data.length; i++) {
-    dataKeys.push(data[i].name);
-  }
 
   function getLineChart(data) {
-    const chartLinesArr = dataKeys.map((name, index) => (
+    console.log('data');
+    console.log(data);
+
+    // dataKeys are the XAxis values (dates). These are used to align the values
+    const dataKeys = Object.keys(data[0]).slice(1);
+    console.log('dataKeys');
+    console.log(dataKeys);
+    const chartLinesArr = dataKeys.map((id, index) => (
       <Line
         type="linear"
-        key={name}
+        key={id} // XAxis (date) value
         dataKey={dataKeys[index]}
-        stroke={lineColors[index]}
+        // stroke={lineColors[index]}
       />
     ));
+    console.log('chartLinesArr');
+    console.log(chartLinesArr);
     return chartLinesArr;
   }
 
