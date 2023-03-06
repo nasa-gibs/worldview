2d524b854032bc48617918089da65ac65c559d2a
ryanweiler92 <ryanweiler92@gmail.com>
2023-03-06 13:23:40 -0500

    lat-long-input-test

--- test-utils/hooks/basicHooks.js
+++ test-utils/hooks/basicHooks.js
@@ -1,23 +1,50 @@
 const { skipTour } = require('../../test-utils/global-variables/querystrings')
 
-// use this hook to make selections from dropdowns
+/**
+ * Make selections for dropdown elements.
+ * @param {Object} page - Playwright object representing the browser page.
+ * @param {string} element - Class or id to identify the element
+ * @param {integer} index - The index position of the selection you want to make
+ */
 const selectOption = async (page, element, index) => {
   const selectElement = await page.locator(element)
   await selectElement.selectOption({ index: index})
 }
 
-// return an elements attribute value for testing
-const getAttribute = async (page, element, attribute) => {
-  const el = await page.locator(element)
-  const elAttribute = await el.getAttribute(attribute)
-  return elAttribute
+/**
+ * Returns an element's specific attribute value to be tested agaisnt.
+ * @param {Object} page - Playwright object representing the browser page.
+ * @param {string} el - Class or id to identify the element
+ * @param {string} attribute - The attribute ex: 'url'
+ */
+const getAttribute = async (page, el, attribute) => {
+  const element = await page.locator(el)
+  const elementAttribute = await element.getAttribute(attribute)
+  return elementAttribute
 }
 
+/**
+ * Returns a URL from an array of string parameters.
+ * @param {Array} startParams - Array of strings each representing URL parameters.
+ */
 const joinUrl = async (startParams) => {
   return `${skipTour}?${startParams.join('&')}`
 }
 
+/**
+ * Clears an input and enters a new value.
+ * @param {Object} page - Playwright object representing the browser page.
+ * @param {string} inputElement - Class or id to identify the input element
+ * @param {string} newValue - The value you want to enter as text to the input
+ */
+const clearAndChangeInput = async (page, inputElement, newValue) => {
+  const input = page.locator(inputElement)
+  await input.clear()
+  await input.fill(newValue)
+}
+
 module.exports = {
+  clearAndChangeInput,
   getAttribute,
   joinUrl,
   selectOption,
--- test-utils/hooks/wvHooks.js
+++ test-utils/hooks/wvHooks.js
@@ -1,20 +1,8 @@
-// const { chromium, firefox, webkit } = require('@playwright/test')
-
-// const launchBrowser = async (browserName, url) => {
-//   const browser = await { chromium, firefox, webkit }[browserName].launch()
-//   const page = await browser.newPage()
-//   await page.goto(url)
-//   return page
-// }
-
-// const launchBrowserSkipTour = async (browserName, url) => {
-//   const browser = await { chromium, firefox, webkit }[browserName].launch()
-//   const page = await browser.newPage()
-//   await page.goto(url)
-//   await page.getByRole('link', { name: 'Start using @NAME@' }).click()
-//   return page
-// }
-
+/**
+ * Switches the projection
+ * @param {Object} page - Playwright object representing the browser page.
+ * @param {string} proj - The specified projection ex: 'geographic', 'arctic' etc..
+ */
 const switchProjections = async (page, proj) => {
   await page.locator('#wv-proj-button').click()
   await page.locator(`#change-${proj}-button`).click()
