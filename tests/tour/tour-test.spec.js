6cafc0b90f49d5863626b9a996e517ea46b49cb9
ryanweiler92 <ryanweiler92@gmail.com>
2023-03-14 14:38:17 -0400

    tour-test

---
+++ tests/tour/tour-test.spec.js
@@ -0,0 +1,38 @@
+// @ts-check
+const { test, expect } = require('@playwright/test')
+const createSelectors = require('../../test-utils/global-variables/selectors')
+const { skipTour } = require('../../test-utils/global-variables/querystrings')
+
+let page
+let selectors
+
+test.describe.configure({ mode: 'serial' })
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage()
+  selectors = createSelectors(page)
+})
+
+test.afterAll(async () => {
+  await page.close()
+})
+
+test('Verify that all tour modals are present when the page is loaded', async () => {
+  const { infoToolbarButton } = selectors
+  await page.goto(skipTour)
+  await infoToolbarButton.click()
+  await page.locator('#start_tour_info_item').click()
+})
+
+test('Run tour', async () => {
+  let totalSteps
+  await page.locator('.tour-box:first-child').click()
+  const stepsElement = await page.locator('.tour-in-progress .step-total')
+  const stepsText = await stepsElement.textContent()
+  totalSteps = parseInt(stepsText)
+  const nextStep = await page.locator('.step-container .step-next')
+  for (let i = 0; i < totalSteps; i += 1){
+    await nextStep.click()
+  }
+  await page.locator('.tour-complete button.close')
+})
