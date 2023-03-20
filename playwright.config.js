4358bf3b9ccf25eba9251372fb68212637f12342
ryanweiler92 <ryanweiler92@gmail.com>
2023-03-20 07:40:58 -0400

    update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,6 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test')
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 module.exports = defineConfig({
   testDir: './e2e/features',
   outputDir: './test-results',
