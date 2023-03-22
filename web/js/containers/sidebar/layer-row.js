04b021113b8d38d7db103bf07a7d6ed367fae9d7
VictorS <victor.sh.91@gmail.com>
2023-03-22 13:55:46 -0700

    fix badge pill nrt. fix blue hover on sidebar tabs

--- web/js/containers/sidebar/layer-row.js
+++ web/js/containers/sidebar/layer-row.js
@@ -333,7 +333,7 @@ function LayerRow (props) {
       ? ['far', 'eye-slash']
       : ['far', 'eye'];
 
-  const collectionClass = collections?.type === 'NRT' ? 'collection-title badge badge-pill badge-secondary' : 'collection-title badge badge-pill badge-light';
+  const collectionClass = collections?.type === 'NRT' ? 'collection-title badge rounded-pill bg-secondary' : 'collection-title badge rounded-pill bg-light';
 
   const renderLayerRow = () => (
     <>
