40b9a4961d42ae653c7c7caf30b997ac7eb0ed0b
VictorS <victor.sh.91@gmail.com>
2023-04-03 08:34:57 -0700

    update unit tests

--- web/js/modules/settings/util.test.js
+++ web/js/modules/settings/util.test.js
@@ -4,11 +4,11 @@ import {
 } from './util';
 
 test('is unitConvert successfully converting units', () => {
-  const actual = unitConvert(32, 'Fahrenheit', 'Celcius');
+  const actual = unitConvert(32, 'Fahrenheit', 'Celsius');
   expect(actual).toBe(0);
 });
 
 test('is convertPaletteValue returning the correct string', () => {
-  const actual = convertPaletteValue('32', 'Fahrenheit', 'Celcius');
+  const actual = convertPaletteValue('32', 'Fahrenheit', 'Celsius');
   expect(actual).toBe('0.00 °C');
 });
