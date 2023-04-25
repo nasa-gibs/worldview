1be0b6e12e71843c8d9eff3fc941bd7f0ed084d0
ryanweiler92 <ryanweiler92@gmail.com>
2023-04-25 08:42:47 -0400

    update tile counter to handle individual layertiles

--- web/js/mapUI/util/util.js
+++ web/js/mapUI/util/util.js
@@ -14,52 +14,60 @@ export const clearLayers = function(ui) {
   ui.cache.clear();
 };
 
-/**
- * Counts the number of tiles that are loaded and expected to be loaded
- * @param {object} ui
- * @returns {object}
- */
+// Helper function to process the tile layer
+const processTileLayer = (layer, map, view) => {
+  const source = layer.getSource();
+  const size = map.getSize();
+  const extent = view.calculateExtent(size);
+  const zoom = view.getZoom();
+  const sourceProjection = source.getProjection() || view.getProjection();
+  const transformedExtent = transformExtent(extent, view.getProjection(), sourceProjection);
+  const tileGrid = source.getTileGridForProjection(sourceProjection);
+  const resolution = view.getResolutionForZoom(zoom);
+  const currentZ = tileGrid.getZForResolution(resolution);
+
+  let expectedTileCount = 0;
+  let loadedTileCount = 0;
+
+  const tileCoordFunction = (tileCoord) => {
+    const tile = source.getTile(tileCoord[0], tileCoord[1], tileCoord[2], 1, sourceProjection);
+    const tileState = tile.getState();
+    if (tileState === 2) {
+      loadedTileCount += 1;
+      expectedTileCount += 1;
+    } else if (tileState === 3) {
+      expectedTileCount += 1;
+    }
+  };
+
+  tileGrid.forEachTileCoord(transformedExtent, currentZ, tileCoordFunction);
+
+  return {
+    expectedTileCount,
+    loadedTileCount,
+  };
+};
+
 export const countTiles = (ui) => {
   const map = ui.selected;
   const view = map.getView();
-  const layerGroup = map.getLayers().item(0);
-  const layers = layerGroup.getLayers().getArray();
+  const layers = map.getLayers().getArray();
 
   let totalExpectedTileCount = 0;
   let totalLoadedTileCount = 0;
 
-  layers.forEach((layer) => {
+  const processLayer = (layer) => {
     if (layer instanceof TileLayer) {
-      const source = layer.getSource();
-      const size = map.getSize();
-      const extent = view.calculateExtent(size);
-      const zoom = view.getZoom();
-      const sourceProjection = source.getProjection() || view.getProjection();
-      const transformedExtent = transformExtent(extent, view.getProjection(), sourceProjection);
-      const tileGrid = source.getTileGridForProjection(sourceProjection);
-      const resolution = view.getResolutionForZoom(zoom);
-      const currentZ = tileGrid.getZForResolution(resolution);
-
-      let expectedTileCount = 0;
-      let loadedTileCount = 0;
-
-      const tileCoordFunction = (tileCoord) => {
-        const tile = source.getTile(tileCoord[0], tileCoord[1], tileCoord[2], 1, sourceProjection);
-        const tileState = tile.getState();
-        if (tileState === 2) {
-          loadedTileCount += 1;
-          expectedTileCount += 1;
-        } else if (tileState === 3) {
-          expectedTileCount += 1;
-        }
-      };
-
-      tileGrid.forEachTileCoord(transformedExtent, currentZ, tileCoordFunction);
-
+      const { expectedTileCount, loadedTileCount } = processTileLayer(layer, map, view);
       totalExpectedTileCount += expectedTileCount;
       totalLoadedTileCount += loadedTileCount;
+    } else if (layer.getLayers) {
+      const subLayers = layer.getLayers().getArray();
+      subLayers.forEach(processLayer);
     }
-  });
+  };
+
+  layers.forEach(processLayer);
 
   return {
     totalExpectedTileCount,
