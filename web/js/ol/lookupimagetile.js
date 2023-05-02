44d5fb5c5a4bb13577d1cfa11d39cdb0d335b5d9
Tom Cariello <tomcariello@gmail.com>
2023-05-02 08:54:47 -0400

    Better organization

--- web/js/ol/lookupimagetile.js
+++ web/js/ol/lookupimagetile.js
@@ -1,7 +1,6 @@
 import OlImageTile from 'ol/ImageTile';
 import OlTileState from 'ol/TileState';
 import UPNG from 'upng-js';
-import pako from 'pako';
 
 class LookupImageTile extends OlImageTile {
   constructor(lookup, tileCoord, state, src, crossOrigin, tileLoadFunction, sourceOptions) {
@@ -11,19 +10,16 @@ class LookupImageTile extends OlImageTile {
   }
 }
 
-let pngProcessed = false;
 LookupImageTile.prototype.getImage = function() {
   return this.canvas_;
 };
 LookupImageTile.prototype.load = function() {
+  console.log('LookupImageTile.prototype.load');
   if (this.state === OlTileState.IDLE) {
     this.state = OlTileState.LOADING;
     this.changed();
     const that = this;
     const onImageLoad = function() {
-      if (!pngProcessed) {
-        createPNG();
-      }
       that.canvas_ = document.createElement('canvas');
       that.canvas_.width = that.image_.width;
       that.canvas_.height = that.image_.height;
@@ -62,14 +58,19 @@ LookupImageTile.prototype.load = function() {
   }
 };
 
-const createPNG = () => {
-  pngProcessed = true;
-  console.log('createPNG');
+const getOpaquePNG = () => {
   fetch('https://gibs-a.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi?TIME=2023-03-20T00:00:00Z&layer=MODIS_Terra_L3_Sea_Ice_Daily&style=default&tilematrixset=1km&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=0&TileCol=0&TileRow=0')
     .then((response) => response.arrayBuffer())
     .then((buffer) => {
       // decode the buffer PNG file
       const decodedPNG = UPNG.decode(buffer);
+      // console.log(decodedPNG.data);
+
+      // Extract the colormap values, and make all colors opaque
+      const colorMapArr = getColormap(decodedPNG.tabs.PLTE);
+
+      // Extract the data values, which are the colormap lookup values
+      const pixelData = decodedPNG.data;
 
       // Create an array buffer matching the dimensions of the provided image
       const bufferSize = decodedPNG.height * decodedPNG.width * 4;
@@ -79,30 +80,20 @@ const createPNG = () => {
       const arrBufferView = new Uint32Array(arrBuffer);
 
       // iterate through the original image, re-drawing each pixel with the alpha channel set to 1
-      for (let i = 0; i < bufferSize; i += 1) {
-        arrBufferView[i] = Math.floor(Math.random() * 255) + 1;
+      for (let i = 0; i < pixelData.length; i += 4) {
+        const lookupVal = pixelData[i] * 4;
+        // console.log(`lookupVal: ${lookupVal}`);
+        // console.log(`colorMapArr[lookupVal]: ${colorMapArr[lookupVal]}`);
+
+        arrBufferView[i] = colorMapArr[lookupVal]; // red channel
+        arrBufferView[i + 1] = colorMapArr[lookupVal + 1]; // green channel
+        arrBufferView[i + 2] = colorMapArr[lookupVal + 2]; // blue channel
+        arrBufferView[i + 3] = 255; // alpha channel
       }
 
       const encodedBufferImage = UPNG.encode([arrBufferView], decodedPNG.width, decodedPNG.height, 256);
-
-      // This creates a new buffer and assigns alternating colors
-      const testBuffer = new ArrayBuffer(10000 * 4); // 50x50 image, 4 bytes per pixel
-      const view = new Uint32Array(testBuffer);
-
-      // assign alternating colors to the buffer view
-      for (let i = 0; i < 10000; i += 1) {
-        if (i % 2 === 0) {
-          view[i] = 0xff0000ff; // red
-        } else {
-          view[i] = 0xFFFF00FF; // yellow
-        }
-      }
-
-      // Create a new PNG image using the modified pixel data and original image dimensions
-      const encodedImage = UPNG.encode([testBuffer], decodedPNG.width, decodedPNG.height, 256);
-
-      // passing the original "buffer" here shows the original image (as expected)
-      putItOnDom(encodedBufferImage);
+      return encodedBufferImage;
+      // putItOnDom(encodedBufferImage);
     });
 };
 
@@ -116,7 +107,19 @@ function putItOnDom(encodedImg) {
   el.appendChild(img);
 }
 
+function getColormap(rawColormap) {
+  const colorMapArr = [];
+  for (let i = 0; i < rawColormap.length; i += 3) {
+    colorMapArr.push(rawColormap[i]);
+    colorMapArr.push(rawColormap[i + 1]);
+    colorMapArr.push(rawColormap[i + 2]);
+    colorMapArr.push(255);
+  }
+  return colorMapArr;
+}
+
 export default function lookupFactory(lookup, sourceOptions) {
+  console.log('lookupFactory');
   return function(tileCoord, state, src, crossOrigin, tileLoadFunction) {
     return new LookupImageTile(
       lookup,
