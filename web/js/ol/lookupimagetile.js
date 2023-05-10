3423a3977b23f3e302bea722d980b4ca4d1af762
Tom Cariello <tomcariello@gmail.com>
2023-05-10 16:29:34 -0400

    Toggling colors works (technically) but it is too slow & initializes incorrectly

--- web/js/ol/lookupimagetile.js
+++ web/js/ol/lookupimagetile.js
@@ -22,12 +22,53 @@ LookupImageTile.prototype.load = function() {
     this.changed();
 
 
+    // const onImageLoad = function() {
+    //   that.canvas_ = document.createElement('canvas');
+    //   that.canvas_.width = that.image_.width;
+    //   that.canvas_.height = that.image_.height;
+    //   const g = that.canvas_.getContext('2d');
+    //   g.drawImage(that.image_, 0, 0);
+    //   that.state = OlTileState.LOADED;
+    //   that.changed();
+    //   that.image_.removeEventListener('load', onImageLoad);
+    // };
+
     const onImageLoad = function() {
       that.canvas_ = document.createElement('canvas');
       that.canvas_.width = that.image_.width;
       that.canvas_.height = that.image_.height;
+      const octets = that.canvas_.width * that.canvas_.height * 4;
       const g = that.canvas_.getContext('2d');
       g.drawImage(that.image_, 0, 0);
+      const imageData = g.getImageData(
+        0,
+        0,
+        that.canvas_.width,
+        that.canvas_.height,
+      );
+      const pixels = imageData.data;
+
+      for (let i = 0; i < octets; i += 4) {
+        const source = `${pixels[i + 0]},${
+          pixels[i + 1]},${
+          pixels[i + 2]},${
+          pixels[i + 3]}`;
+        const target = that.lookup_[source];
+
+        if (target) {
+          pixels[i + 0] = target.r;
+          pixels[i + 1] = target.g;
+          pixels[i + 2] = target.b;
+          pixels[i + 3] = target.a;
+        }
+      }
+      g.putImageData(imageData, 0, 0);
+
+      // uses the tileload function passed from layerbuilder
+      if (that.customTileLoadFunction_) {
+        that.customTileLoadFunction_(that, that.src_);
+      }
+
       that.state = OlTileState.LOADED;
       that.changed();
       that.image_.removeEventListener('load', onImageLoad);
