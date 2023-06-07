4c171fe141216a6f9d143b02fbdbbced201d597f
ryanweiler92 <ryanweiler92@gmail.com>
2023-06-07 16:48:31 -0400

    turn off EIC animation checks for antarctic proj

--- web/js/containers/animation-widget/animation-widget.js
+++ web/js/containers/animation-widget/animation-widget.js
@@ -377,6 +377,8 @@ const mapStateToProps = (state) => {
   const hasFutureLayers = activeLayersForProj.filter((layer) => layer.futureTime).length > 0;
   const layerDateRange = getDateRange({}, activeLayersForProj);
 
+  const isAntarctic = proj.id === 'arctic';
+
   const minDate = new Date(config.startDate);
   let maxDate;
   if (layerDateRange && layerDateRange.end > appNow) {
@@ -388,7 +390,7 @@ const mapStateToProps = (state) => {
   const {
     isDistractionFreeModeActive, isKioskModeActive, animationAvailabilityChecked, eic,
   } = ui;
-  const checkAnimationAvailability = (eic === 'sa' || eic === 'da') && !animationAvailabilityChecked && isKioskModeActive && isPlaying;
+  const checkAnimationAvailability = (eic === 'sa' || eic === 'da') && !animationAvailabilityChecked && isKioskModeActive && isPlaying && !isAntarctic;
   const { isEmbedModeActive } = embed;
   const animationIsActive = isActive
     && lodashGet(map, 'ui.selected.frameState_')
