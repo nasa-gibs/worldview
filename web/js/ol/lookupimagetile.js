ea170c1a4fc65511cb1914cf34b2e74135fd99b4
Tom Cariello <tomcariello@gmail.com>
2023-06-12 14:14:09 -0400

    Refactoring/merging the old palette swap code with the new code

--- web/js/ol/lookupimagetile.js
+++ web/js/ol/lookupimagetile.js
@@ -16,54 +16,42 @@ LookupImageTile.prototype.getImage = function() {
   return this.canvas_;
 };
 LookupImageTile.prototype.load = function() {
-  console.log('LookupImageTile: load');
   if (this.state === OlTileState.IDLE) {
     this.state = OlTileState.LOADING;
     const that = this;
     this.changed();
-
-
-    // const onImageLoad = function() {
-    //   that.canvas_ = document.createElement('canvas');
-    //   that.canvas_.width = that.image_.width;
-    //   that.canvas_.height = that.image_.height;
-    //   const g = that.canvas_.getContext('2d');
-    //   g.drawImage(that.image_, 0, 0);
-    //   that.state = OlTileState.LOADED;
-    //   that.changed();
-    //   that.image_.removeEventListener('load', onImageLoad);
-    // };
+    const pixelsToDisplay = getPixelColorsToDisplay(this.lookup_);
 
     const onImageLoad = function() {
       that.canvas_ = document.createElement('canvas');
       that.canvas_.width = that.image_.width;
       that.canvas_.height = that.image_.height;
-      const octets = that.canvas_.width * that.canvas_.height * 4;
+      // const octets = that.canvas_.width * that.canvas_.height * 4;
       const g = that.canvas_.getContext('2d');
       g.drawImage(that.image_, 0, 0);
-      const imageData = g.getImageData(
-        0,
-        0,
-        that.canvas_.width,
-        that.canvas_.height,
-      );
-      const pixels = imageData.data;
+      // const imageData = g.getImageData(
+      //   0,
+      //   0,
+      //   that.canvas_.width,
+      //   that.canvas_.height,
+      // );
+      // const pixels = imageData.data;
 
-      for (let i = 0; i < octets; i += 4) {
-        const source = `${pixels[i + 0]},${
-          pixels[i + 1]},${
-          pixels[i + 2]},${
-          pixels[i + 3]}`;
-        const target = that.lookup_[source];
+      // for (let i = 0; i < octets; i += 4) {
+      //   const source = `${pixels[i + 0]},${
+      //     pixels[i + 1]},${
+      //     pixels[i + 2]},${
+      //     pixels[i + 3]}`;
+      //   const target = that.lookup_[source];
 
-        if (target) {
-          pixels[i + 0] = target.r;
-          pixels[i + 1] = target.g;
-          pixels[i + 2] = target.b;
-          pixels[i + 3] = target.a;
-        }
-      }
-      g.putImageData(imageData, 0, 0);
+      //   if (target) {
+      //     pixels[i + 0] = target.r;
+      //     pixels[i + 1] = target.g;
+      //     pixels[i + 2] = target.b;
+      //     pixels[i + 3] = target.a;
+      //   }
+      // }
+      // g.putImageData(imageData, 0, 0);
 
       // uses the tileload function passed from layerbuilder
       if (that.customTileLoadFunction_) {
@@ -77,6 +65,7 @@ LookupImageTile.prototype.load = function() {
 
     // -----Swap in opaque code block--------------------
     if (true) {
+      console.log('fetching');
       fetch(this.src_)
         .then((response) => response.arrayBuffer())
         .then((buffer) => {
@@ -84,40 +73,73 @@ LookupImageTile.prototype.load = function() {
           const decodedPNG = UPNG.decode(buffer);
           const { width, height } = decodedPNG;
 
-          // Extract the colormap values, and make all colors opaque
-          const colorMapArr = getColormap(decodedPNG.tabs.PLTE);
-
-          // Extract the data (colormap) lookup values
-          const pixelData = decodedPNG.data;
-
           // Create an array buffer matching the pixel dimensions of the provided image
           const bufferSize = height * width * 4;
           const arrBuffer = new Uint32Array(bufferSize);
 
-          // iterate through the image, re-drawing each pixel with the alpha channel set to 1
+          // Extract the colormap values. This is an array of integers representing rgba values.
+          // Used in sets of 4 (i.e. colorMapArr[0] = r, colorMapArr[1] = g, etc.)
+          const colorMapArr = getColormap(decodedPNG.tabs.PLTE);
+
+          // Extract the pixel data. This is an array of integers corresponding to the colormap
+          // i.e. if pixelData[0] == 5, this pixel is the color of the 5th value in the colormap
+          const pixelData = decodedPNG.data;
+
+          // iterate through the pixelData, drawing each pixel using the appropriate color
           for (let i = 0; i < pixelData.length; i += 1) {
             const arrBuffIndex = i * 4;
-            const lookupVal = pixelData[i] * 4;
+            const lookupIndex = pixelData[i] * 4;
 
-            arrBuffer[arrBuffIndex] = colorMapArr[lookupVal]; // red channel
-            arrBuffer[arrBuffIndex + 1] = colorMapArr[lookupVal + 1]; // green channel
-            arrBuffer[arrBuffIndex + 2] = colorMapArr[lookupVal + 2]; // blue channel
-            arrBuffer[arrBuffIndex + 3] = 255; // alpha channel
+            // Determine desired RGBA for this pixel
+            const r = colorMapArr[lookupIndex];
+            const g = colorMapArr[lookupIndex + 1];
+            const b = colorMapArr[lookupIndex + 2];
+            const a = 255;
+
+            // Concatentate to 'r,g,b,a' string & check if that color is in the pixelsToDisplay array
+            const rgbaStr = `${r},${g},${b},${a}`;
+            const drawThisColor = pixelsToDisplay[rgbaStr];
+
+            // If the intended color exists in pixelsToDisplay obj, draw that color, otherwise draw transparent
+            if (drawThisColor !== undefined) {
+              arrBuffer[arrBuffIndex + 0] = r;
+              arrBuffer[arrBuffIndex + 1] = g;
+              arrBuffer[arrBuffIndex + 2] = b;
+              arrBuffer[arrBuffIndex + 3] = a;
+            } else {
+              arrBuffer[arrBuffIndex] = 0;
+              arrBuffer[arrBuffIndex + 1] = 0;
+              arrBuffer[arrBuffIndex + 2] = 0;
+              arrBuffer[arrBuffIndex + 3] = 0;
+            }
           }
 
-          // Encode the image, creating a PNG file
+          // Encode the image, creating a new PNG file
           const encodedBufferImage = UPNG.encode([arrBuffer], decodedPNG.width, decodedPNG.height, decodedPNG.depth);
           const blob = new Blob([encodedBufferImage], { type: 'image/png' });
           const dataURL = `${URL.createObjectURL(blob)}`;
-
           that.image_.src = dataURL;
           that.image_.addEventListener('load', onImageLoad);
         });
     }
-    that.image_.src = this.src_;
-    that.image_.addEventListener('load', onImageLoad);
   }
 };
+
+/**
+   * @method getPixelColorsToDisplay
+   * @static
+   * @param obj {object} - The Colormap for this product
+   *
+   * @returns {object} Colormap containing only active colors
+   */
+function getPixelColorsToDisplay(obj) {
+  return Object.entries(obj).reduce((acc, [key, value]) => {
+    if (!(value.r === 0 && value.g === 0 && value.b === 0 && value.a === 0)) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+}
 
 function getColormap(rawColormap) {
   const colorMapArr = [];
@@ -131,13 +153,13 @@ function getColormap(rawColormap) {
 }
 
 /**
-   * Create a new WMTS Layer
-   * @method lookupFactory
-   * @static
-   * @param {object} lookup - The layer palette
-   * @param {object} sourceOptions - Layer options
-   * @returns {object} function to create LookupImageTile
-   */
+ * Create a new WMTS Layer
+ * @method lookupFactory
+ * @static
+ * @param {object} lookup - The layer palette
+ * @param {object} sourceOptions - Layer options
+ * @returns {object} function to create LookupImageTile
+ */
 export default function lookupFactory(lookup, sourceOptions) {
   return function(tileCoord, state, src, crossOrigin, tileLoadFunction) {
     return new LookupImageTile(
