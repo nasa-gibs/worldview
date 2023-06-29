ad523fbeb9d80334fd2f3a0b1222399a917eba89
ryanweiler92 <ryanweiler92@gmail.com>
2023-06-29 09:19:57 -0400

    measure imagery within visible extent

--- web/js/mapUI/components/kiosk/tile-measurement/utils/image-api-request.js
+++ web/js/mapUI/components/kiosk/tile-measurement/utils/image-api-request.js
@@ -2,15 +2,25 @@
 import axios from 'axios';
 import { saveAs } from 'file-saver';
 
-export default async function fetchWMSImage(layer, date, testMode) {
+export default async function fetchWMSImage(layer, date, extent, testMode) {
   const baseUrl = 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi';
+  const boundingBox = `${extent[0]},${extent[1]},${extent[2]},${extent[3]}`;
+  const fullExtentBBox = '-20037508.34,-20048966.1,20037508.34,20048966.1';
+  let bbox;
+  // when we are zoomed out too far, the bounding box will include NaN
+  if (boundingBox.includes('NaN')) {
+    bbox = fullExtentBBox;
+  } else {
+    bbox = boundingBox;
+  }
+
   const params = {
     version: '1.3.0',
     service: 'WMS',
     request: 'GetMap',
     format: 'image/png',
     STYLE: 'default',
-    bbox: '-20037508.34,-20048966.1,20037508.34,20048966.1',
+    bbox,
     CRS: 'EPSG:3857',
     HEIGHT: '256',
     WIDTH: '256',
