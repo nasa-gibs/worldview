04471baaaf80eeacdae285168690da61ae27773d
PatchesMaps <patrick.moulden@gmail.com>
2023-11-20 14:59:04 -0500

    modal

--- web/js/components/feature-alert/granuleAlertModal.js
+++ web/js/components/feature-alert/granuleAlertModal.js
@@ -17,40 +17,8 @@ function GranuleAlertModalBody() {
         <Col>
 
           <p>
-            Vector layers identify locations on earth using points, line segments or polygons while raster layers represent locations on earth through a grid of cells or pixels that have associated color values. The vector layers have attribute information that can be examined when a vector feature is clicked. For example, when a point is clicked on in the Dams layer, a table of attributes will appear including the dam name, river, main use, representative capacity, etc.
+          Certain imagery visualization layers have narrower and smaller footprints so there isn&apos;t imagery to view at every location across the globe. Some imagery layers also have lower temporal revisit periods meaning there won&apos;t be daily imagery for a specific location on earth. Click on “View Options” in the layer list to access a date picker to locate imagery for your desired location.
           </p>
-        </Col>
-      </Row>
-      <Row className="p-2">
-        <Col>
-          <h2>
-            Why can’t I click on a feature in a vector layer?
-          </h2>
-        </Col>
-      </Row>
-      <Row className="p-3">
-        <Col>
-
-          <p>
-            To speed up map interactions, vector layers that have a vast number of features are rendered as raster images in outer zoom levels. Zoom in to be able to query the vector layer.
-          </p>
-        </Col>
-      </Row>
-      <Row className="p-2">
-        <Col>
-          <h2>
-            How can I determine which layers have clickable features?
-          </h2>
-        </Col>
-      </Row>
-      <Row className="p-3">
-        <Col>
-          <p>
-            If you see the blue pointer icon next to the layer in the sidebar Layer List, you should be able to click on that layer’s features in order to access attribute information associated with that vector feature. If you see the blue pointer with a red X over it, you are zoomed out too far and the layer is being rendered as a raster, zoom in to be able to examine the attribute information of the vector layer.
-          </p>
-        </Col>
-        <Col>
-          <img src="images/vector-alert.png" />
         </Col>
       </Row>
     </Container>
