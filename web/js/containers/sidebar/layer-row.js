82709532da81e351c2b1127a33ef1555feed9b0b
PatchesMaps <patrick.moulden@gmail.com>
2023-12-01 09:21:01 -0500

    zero out datetime

--- web/js/containers/sidebar/layer-row.js
+++ web/js/containers/sidebar/layer-row.js
@@ -117,9 +117,13 @@ function LayerRow (props) {
   useEffect(() => {
     const asyncFunc = async () => {
       if (layer?.collection_concept_id) {
-        const olderRes = await fetch(`https://cmr.earthdata.nasa.gov/search/granules.json?collection_concept_id=${layer?.collection_concept_id}&bounding_box=${map?.extent?.join(',')}&temporal=P0Y0M0DT12H0M/${selectedDate?.toISOString()}&sort_key=-start_date&pageSize=1`);
+        const dateTime = selectedDate?.toISOString().split('T');
+        dateTime.pop();
+        dateTime.push('00:00:00.000Z');
+        const zeroedDate = dateTime.join('T');
+        const olderRes = await fetch(`https://cmr.earthdata.nasa.gov/search/granules.json?collection_concept_id=${layer?.collection_concept_id}&bounding_box=${map?.extent?.join(',')}&temporal=P0Y0M0DT0H0M/${zeroedDate}&sort_key=-start_date&pageSize=1`);
         const olderGranules = await olderRes.json();
-        const newerRes = await fetch(`https://cmr.earthdata.nasa.gov/search/granules.json?collection_concept_id=${layer?.collection_concept_id}&bounding_box=${map?.extent?.join(',')}&temporal=${selectedDate?.toISOString()}/P0Y0M0DT13H0M&sort_key=-start_date&pageSize=1`);
+        const newerRes = await fetch(`https://cmr.earthdata.nasa.gov/search/granules.json?collection_concept_id=${layer?.collection_concept_id}&bounding_box=${map?.extent?.join(',')}&temporal=${zeroedDate}/P0Y0M1DT0H0M&sort_key=-start_date&pageSize=1`);
         const newerGranules = await newerRes.json();
         const olderEntries = olderGranules?.feed?.entry || [];
         const newerEntries = newerGranules?.feed?.entry || [];
