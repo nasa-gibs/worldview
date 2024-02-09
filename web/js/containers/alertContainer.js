c3027dc3863a78bb44000a729d6d84fa7d4c7f88
PatchesMaps <patrick.moulden@gmail.com>
2024-02-09 16:29:37 -0500

    cleanup

--- web/js/containers/alertContainer.js
+++ web/js/containers/alertContainer.js
@@ -1,10 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-} from 'reactstrap';
 import FeatureAlert from '../components/feature-alert/alert';
 import Alerts from './alerts';
 import Tour from './tour';
@@ -12,16 +7,12 @@ import Tour from './tour';
 export default function AlertContainer(isTourActive, numberOutagesUnseen, isMobile, isEmbedModeActive) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const containerRef = useRef(null);
-
-  let notifications = containerRef?.current?.children.length;
-
-  console.log(containerRef?.current?.children.length);
-  
+  const notifications = containerRef?.current?.children.length;
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
   return (
     <div className="wv-alert-dropdown">
-      <button hidden={notifications <= 1} onClick={toggle}>
+      <button type="button" hidden={notifications <= 1} onClick={toggle}>
         <FontAwesomeIcon
           icon="exclamation-triangle"
           className="wv-alert-icon"
