707aadd0f865f5494f5de0fe8156547aa022ae7f
PatchesMaps <patrick.moulden@gmail.com>
2024-02-09 16:25:39 -0500

    first pass

--- web/js/containers/alertContainer.js
+++ web/js/containers/alertContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   Dropdown,
@@ -11,24 +11,30 @@ import Tour from './tour';
 
 export default function AlertContainer(isTourActive, numberOutagesUnseen, isMobile, isEmbedModeActive) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const containerRef = useRef(null);
 
+  let notifications = containerRef?.current?.children.length;
+
+  console.log(containerRef?.current?.children.length);
+  
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
   return (
-    <Dropdown group isOpen={dropdownOpen} toggle={toggle} className="wv-alert-dropdown">
-      <DropdownToggle caret>
+    <div className="wv-alert-dropdown">
+      <button hidden={notifications <= 1} onClick={toggle}>
         <FontAwesomeIcon
           icon="exclamation-triangle"
           className="wv-alert-icon"
           size="1x"
         />
         Multiple Layer Alerts
-      </DropdownToggle>
-      <DropdownMenu id="wv-alert-container" className="wv-alert-container">
+        {dropdownOpen ? <FontAwesomeIcon icon="fa-solid fa-caret-down" /> : <FontAwesomeIcon icon="fa-solid fa-caret-up" />}
+      </button>
+      <div ref={containerRef} hidden={!(dropdownOpen || notifications === 1)} id="wv-alert-container" className="wv-alert-container">
         <FeatureAlert />
         <Alerts />
         {isTourActive && numberOutagesUnseen === 0 && (!isMobile || isEmbedModeActive) ? <Tour /> : null}
-      </DropdownMenu>
-    </Dropdown>
+      </div>
+    </div>
   );
 }
