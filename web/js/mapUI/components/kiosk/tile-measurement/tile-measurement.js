70eda3532b0b7e8f818008e07be1065c742affd4
ryanweiler92 <ryanweiler92@gmail.com>
2024-04-03 08:54:23 -0400

    lint fixes

--- web/js/mapUI/components/kiosk/tile-measurement/tile-measurement.js
+++ web/js/mapUI/components/kiosk/tile-measurement/tile-measurement.js
@@ -220,10 +220,10 @@ function TileMeasurement({ ui }) {
       // If we are using the best date, we need to make sure there are tiles on the map so we include the abort prodcedure parameter
       // This allows us to fall back to the static map if the best date fails as a last resort
       const bestDate = findBestDate(measurementLayers, bestDates);
-      if (!fullImageryDate || bestDate === fullImageryDate){
+      if (!fullImageryDate || bestDate === fullImageryDate) {
         updateDate(bestDate, layerPeriod);
         verifyTilesAndHandleErrors();
-        return
+        return;
       }
 
       // Format date based on period and dispatch redux action
