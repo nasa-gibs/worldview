37c681a6e4773e126dca0894e9007950a8ba0661
christof-wittreich <christof.wittreich@nasa.gov>
2024-05-03 12:00:50 -0400

    Updated to redux 5.0.1

--- web/js/modules/modal/actions.test.js
+++ web/js/modules/modal/actions.test.js
@@ -1,6 +1,6 @@
 import { TextEncoder, TextDecoder } from 'util';
 import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import fetchMock from 'fetch-mock';
 import {
   openBasicContent,
