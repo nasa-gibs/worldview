79f0326df1f74e8b3776fa355394a2877bbf8bc7
Patrick Moulden <4834892+PatchesMaps@users.noreply.github.com>
2024-05-17 16:57:16 -0400

    WV-3174 Provide ability to set default number of granules to display on a per layer basis (#5202)
    * fallback to the layer defs count value
    
    * get count from layer def
    
    * add documentation
    
    * more

--- web/js/modules/layers/selectors.js
+++ web/js/modules/layers/selectors.js
@@ -41,7 +41,7 @@ export function addLayer(id, spec = {}, layersParam, layerConfig, overlayLength,
   def.max = spec.max || undefined;
   def.squash = spec.squash || undefined;
   def.disabled = spec.disabled || undefined;
-  def.count = spec.count || undefined;
+  def.count = spec.count || def.count || undefined;
 
   if (Array.isArray(spec.bandCombo)) {
     def.bandCombo = {
