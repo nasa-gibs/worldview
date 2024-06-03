b6a986c250a1d77d6f7b4c5dfd44ec85671f2a4c
PatchesMaps <patrick.moulden@gmail.com>
2024-06-03 10:40:23 -0400

    change tile state to error on error

--- web/js/ol/lookupimagetile.js
+++ web/js/ol/lookupimagetile.js
@@ -42,7 +42,7 @@ class LookupImageTile extends OlImageTile {
 LookupImageTile.prototype.getImage = function() {
   return this.canvas_;
 };
-LookupImageTile.prototype.load = function() {
+LookupImageTile.prototype.load = async function() {
   if (this.state === OlTileState.IDLE) {
     this.state = OlTileState.LOADING;
     const that = this;
@@ -130,65 +130,67 @@ LookupImageTile.prototype.load = function() {
     if (lookupCount > 1 && lookupCount < 25) {
       imageProcessed = true;
       const pixelsToDisplay = getPixelColorsToDisplay(this.lookup_);
-      fetch(this.src_)
-        .then((response) => response.arrayBuffer())
-        .then((buffer) => {
-          // decode the buffer PNG file
-          const decodedPNG = UPNG.decode(buffer);
-          const { width, height } = decodedPNG;
-
-          // Create an array buffer matching the pixel dimensions of the provided image
-          const bufferSize = height * width * 4;
-          const arrBuffer = new Uint32Array(bufferSize);
-
-          // Extract the colormap values. This is an array of integers representing rgba values.
-          // Used in sets of 4 (i.e. colorMapArr[0] = r, colorMapArr[1] = b, etc.)
-          // colorMapArr assumes a max of 256 colors
-          const colorMapArr = getColormap(decodedPNG.tabs.PLTE);
-
-          // Extract the pixel data. This is an array of integers corresponding to the colormap
-          // i.e. if pixelData[0] == 5, this pixel is the color of the 5th entry in the colormap
-          const pixelData = decodedPNG.data;
-
-          // iterate through the pixelData, drawing each pixel using the appropriate color
-          for (let i = 0; i < pixelData.length; i += 1) {
-            const arrBuffIndex = i * 4;
-            const lookupIndex = pixelData[i] * 4;
-
-            // Determine desired RGBA for this pixel
-            const r = colorMapArr[lookupIndex];
-            const g = colorMapArr[lookupIndex + 1];
-            const b = colorMapArr[lookupIndex + 2];
-            const a = 255;
-            // Concatentate to 'r,g,b,a' string & check if that color is in the pixelsToDisplay array
-            const rgbaStr = `${r},${g},${b},${a}`;
-            const drawThisColor = pixelsToDisplay[rgbaStr];
-
-            // If the intended color exists in pixelsToDisplay obj, draw that color, otherwise draw transparent
-            if (drawThisColor !== undefined) {
-              arrBuffer[arrBuffIndex + 0] = r;
-              arrBuffer[arrBuffIndex + 1] = g;
-              arrBuffer[arrBuffIndex + 2] = b;
-              arrBuffer[arrBuffIndex + 3] = a;
-            } else {
-            // console.log('drawThisColor undefined, rgbaStr:', rgbaStr);
-              arrBuffer[arrBuffIndex] = 0;
-              arrBuffer[arrBuffIndex + 1] = 0;
-              arrBuffer[arrBuffIndex + 2] = 0;
-              arrBuffer[arrBuffIndex + 3] = 0;
-            }
+      try {
+        const res = await fetch(this.src_)
+        if (!res.ok) throw new Error(`Failed to fetch image: ${res.status} ${res.statusText}`);
+        const buffer = await res.arrayBuffer();
+        // decode the buffer PNG file
+        const decodedPNG = UPNG.decode(buffer);
+        const { width, height } = decodedPNG;
+  
+        // Create an array buffer matching the pixel dimensions of the provided image
+        const bufferSize = height * width * 4;
+        const arrBuffer = new Uint32Array(bufferSize);
+  
+        // Extract the colormap values. This is an array of integers representing rgba values.
+        // Used in sets of 4 (i.e. colorMapArr[0] = r, colorMapArr[1] = b, etc.)
+        // colorMapArr assumes a max of 256 colors
+        const colorMapArr = getColormap(decodedPNG.tabs.PLTE);
+  
+        // Extract the pixel data. This is an array of integers corresponding to the colormap
+        // i.e. if pixelData[0] == 5, this pixel is the color of the 5th entry in the colormap
+        const pixelData = decodedPNG.data;
+  
+        // iterate through the pixelData, drawing each pixel using the appropriate color
+        for (let i = 0; i < pixelData.length; i += 1) {
+          const arrBuffIndex = i * 4;
+          const lookupIndex = pixelData[i] * 4;
+  
+          // Determine desired RGBA for this pixel
+          const r = colorMapArr[lookupIndex];
+          const g = colorMapArr[lookupIndex + 1];
+          const b = colorMapArr[lookupIndex + 2];
+          const a = 255;
+          // Concatentate to 'r,g,b,a' string & check if that color is in the pixelsToDisplay array
+          const rgbaStr = `${r},${g},${b},${a}`;
+          const drawThisColor = pixelsToDisplay[rgbaStr];
+  
+          // If the intended color exists in pixelsToDisplay obj, draw that color, otherwise draw transparent
+          if (drawThisColor !== undefined) {
+            arrBuffer[arrBuffIndex + 0] = r;
+            arrBuffer[arrBuffIndex + 1] = g;
+            arrBuffer[arrBuffIndex + 2] = b;
+            arrBuffer[arrBuffIndex + 3] = a;
+          } else {
+          // console.log('drawThisColor undefined, rgbaStr:', rgbaStr);
+            arrBuffer[arrBuffIndex] = 0;
+            arrBuffer[arrBuffIndex + 1] = 0;
+            arrBuffer[arrBuffIndex + 2] = 0;
+            arrBuffer[arrBuffIndex + 3] = 0;
           }
-
-          // Encode the image, creating a new PNG file
-          const encodedBufferImage = UPNG.encode([arrBuffer], decodedPNG.width, decodedPNG.height, decodedPNG.depth);
-          const blob = new Blob([encodedBufferImage], { type: 'image/png' });
-          const dataURL = `${URL.createObjectURL(blob)}`;
-          this.image_.src = dataURL;
-          this.image_.addEventListener('load', onImageLoad);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+        }
+  
+        // Encode the image, creating a new PNG file
+        const encodedBufferImage = UPNG.encode([arrBuffer], decodedPNG.width, decodedPNG.height, decodedPNG.depth);
+        const blob = new Blob([encodedBufferImage], { type: 'image/png' });
+        const dataURL = `${URL.createObjectURL(blob)}`;
+        this.image_.src = dataURL;
+        this.image_.addEventListener('load', onImageLoad);  
+      } catch (error) {
+        that.state = OlTileState.ERROR;
+        that.changed();
+        console.error('Error:', error);
+      }
     } else {
       this.image_.src = this.src_;
       this.image_.addEventListener('load', onImageLoad);
