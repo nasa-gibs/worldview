520e138ab0415e23feaee2528414b579c6d9235c
PatchesMaps <patrick.moulden@gmail.com>
2024-09-03 11:30:22 -0400

    fix: Update isWithinRanges function to return false when ranges are null.
    This prevents imagery from being shown before the CMR requests resolve.

--- web/js/map/granule/granule-layer-builder.js
+++ web/js/map/granule/granule-layer-builder.js
@@ -153,7 +153,7 @@ export default function granuleLayerBuilder(cache, store, createLayerWMTS) {
    * @returns {boolean} - true if date is within a range
   */
   const isWithinRanges = (date, ranges) => {
-    if (!ranges) return true;
+    if (!ranges) return false;
 
     return ranges.some(([start, end]) => date >= new Date(start) && date <= new Date(end));
   };
