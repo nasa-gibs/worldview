4a163b5a4e67454aa1baa5b95eab1351a4098064
mwong4 <min.m.wong@nasa.gov>
2025-09-19 09:58:45 -0400

    Update layer-picker-test.spec.js

--- e2e/features/layers/layer-picker-test.spec.js
+++ e2e/features/layers/layer-picker-test.spec.js
@@ -155,8 +155,8 @@ test('Disabling coverage filter updates list', async () => {
   } = selectors
   await availableFilterCheckbox.click()
   await expect(availableFilterCheckboxInput).not.toBeChecked()
-  await expect(layersSearchRow).toHaveCount(17)
-  await expect(layerResultsCountText).toContainText('Showing 17 out of')
+  await expect(layersSearchRow).toHaveCount(19)
+  await expect(layerResultsCountText).toContainText('Showing 19 out of')
 })
 
 test('Finding layer by ID with search', async () => {
