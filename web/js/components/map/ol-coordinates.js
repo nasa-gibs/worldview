4e091edc2b551e6b2d0d7bd59357e2b21f1d37f2
Victor Gonzalez Leon <victor.a.gonzalez-leon@nasa.gov>
2026-07-22 18:27:27 -0400

    Remove unused React imports

--- web/js/components/map/ol-coordinates.js
+++ web/js/components/map/ol-coordinates.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
