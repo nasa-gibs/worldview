6740823c90c827ac8d4fe09c23deff8df5ca22bd
christof-wittreich <christof.wittreich@nasa.gov>
2026-07-30 15:33:13 -0400

    Modernized layer comps

--- web/js/components/layer/product-picker/search/search-layers-list.js
+++ web/js/components/layer/product-picker/search/search-layers-list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withSearch } from '@elastic/react-search-ui';
@@ -11,55 +11,71 @@ import {
   clearSingleRecentLayer as clearSingleRecentLayerAction,
 } from '../../../../modules/product-picker/actions';
 import RecentLayersInfo from '../browse/recent-layers-info';
+import usePrevious from '../../../../util/customHooks';
 
 /*
  * A scrollable list of layers
- * @class LayerList
- * @extends React.Component
  */
-class SearchLayerList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visibleItems: [],
-      hasMoreItems: true,
-      nextIndex: 0,
-      firstLoadAutoSelect: false,
-    };
-    this.loadMoreItems = this.loadMoreItems.bind(this);
-  }
+function SearchLayerList(props) {
+  const {
+    selectedLayer,
+    results,
+    selectLayer,
+    recentLayerMode,
+    isMobile,
+    clearSingleRecentLayer,
+  } = props;
 
-  static getDerivedStateFromProps(props, state) {
-    if (props.selectedLayer) {
-      return { firstLoadAutoSelect: true };
-    }
-    return null;
-  }
+  const [visibleItems, setVisibleItems] = useState([]);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
+  const [nextIndex, setNextIndex] = useState(0);
+  const [firstLoadAutoSelect, setFirstLoadAutoSelect] = useState(false);
+
+  const scrollParentSelector = '.layer-list-container.search .simplebar-content-wrapper';
+  const scrollParentRef = useRef(document.querySelector(scrollParentSelector));
+  const isMountedRef = useRef(false);
+
+  const prevResults = usePrevious(results);
+
+  useEffect(() => {
+    if (!selectedLayer) return;
+    setFirstLoadAutoSelect(true);
+  }, [selectedLayer]);
 
   /**
    * Handle selecting/showing metadata when there is only a single search result
    */
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    const { selectedLayer, results } = this.props;
-    const { firstLoadAutoSelect } = this.state;
+  useEffect(() => {
+    if (!isMountedRef.current) {
+      isMountedRef.current = true;
+      return;
+    }
     const selectedLayerInResults = selectedLayer &&
       (results || []).find((l) => l.id === selectedLayer.id);
 
     // Clear metadata when item no longer in list of results
     if (!selectedLayerInResults && selectedLayer) {
-      this.showLayerMetadata(null);
+      showLayerMetadata(null);
     }
     // Select first item in list on initial load
     if (!selectedLayer && results && results.length && !firstLoadAutoSelect) {
-      this.setState({ firstLoadAutoSelect: true }, () => {
-        const { id } = results[0];
-        this.showLayerMetadata(id);
-      });
+      setFirstLoadAutoSelect(true);
     }
-    if (prevProps.results !== results) {
-      this.loadMoreItems(0, prevProps);
+    if (prevResults !== results) {
+      loadMoreItems();
     }
-  }
+  }, [selectedLayer, results]);
+
+  useEffect(() => {
+    if (!firstLoadAutoSelect || !results[0]) return;
+    const { id } = results[0];
+    showLayerMetadata(id);
+  }, [firstLoadAutoSelect]);
+
+  useEffect(() => {
+    if (!hasMoreItems) return;
+    loadMoreItems();
+  }, [hasMoreItems]);
 
   /**
    * Loads metadata for layer (if not previously loaded) and
@@ -68,12 +84,7 @@ class SearchLayerList extends React.Component {
    * @param {string} layerId - the layer id to show metadata for
    * @return {void}
    */
-  showLayerMetadata(layerId) {
-    const {
-      results,
-      selectedLayer,
-      selectLayer,
-    } = this.props;
+  function showLayerMetadata(layerId) {
     const layer = results.find((l) => l.id === layerId);
 
     // No result found, clear the metadata detail view by passing null
@@ -90,17 +101,12 @@ class SearchLayerList extends React.Component {
     selectLayer(layer);
   }
 
-  loadMoreItems(page, prevProps) {
-    const { results } = this.props;
-    const { visibleItems, nextIndex, hasMoreItems } = this.state;
-
+  function loadMoreItems() {
     // If results changed, reset
-    if (prevProps) {
-      this.setState({
-        visibleItems: [],
-        hasMoreItems: true,
-        nextIndex: 0,
-      }, this.loadMoreItems);
+    if (!hasMoreItems) {
+      setVisibleItems([]);
+      setHasMoreItems(true);
+      setNextIndex(0);
       return;
     }
 
@@ -109,20 +115,15 @@ class SearchLayerList extends React.Component {
       const newNextIndex = prevIndex + 50;
       const moreItems = results.slice(prevIndex, newNextIndex);
       const newItems = [...visibleItems, ...moreItems];
-      this.setState({
-        visibleItems: newItems,
-        nextIndex: newNextIndex,
-        hasMoreItems: results.length > newItems.length,
-      });
+      setVisibleItems(newItems);
+      setNextIndex(newNextIndex);
+      setHasMoreItems(results.length > newItems.length);
     } else {
-      this.setState({
-        hasMoreItems: false,
-      });
+      setHasMoreItems(false);
     }
   }
 
-  renderNoResults() {
-    const { recentLayerMode } = this.props;
+  function renderNoResults() {
     return recentLayerMode
       ? (<RecentLayersInfo />)
       : (
@@ -133,50 +134,41 @@ class SearchLayerList extends React.Component {
       );
   }
 
-  render() {
-    const { visibleItems, hasMoreItems } = this.state;
-    const {
-      results, isMobile, clearSingleRecentLayer, recentLayerMode,
-    } = this.props;
-    const scrollParentSelector = '.layer-list-container.search .simplebar-content-wrapper';
-    this.scrollParent = this.scrollParent || document.querySelector(scrollParentSelector);
-
-    return !results.length
-      ? this.renderNoResults()
-      : (
-        <InfiniteScroll
-          pageStart={0}
-          loadMore={this.loadMoreItems}
-          hasMore={hasMoreItems}
-          useWindow={false}
-          getScrollParent={() => this.scrollParent}
-        >
-          <div className="product-outer-list-case layers-all">
-            {visibleItems.map((layer) => (isMobile && recentLayerMode
-              ? (
-                <SwipeToDelete
-                  key={layer.id}
-                  item={layer}
-                  deleteSwipe={0.33}
-                  onDelete={() => clearSingleRecentLayer(layer)}
-                >
-                  <SearchLayerRow
-                    layer={layer}
-                    showLayerMetadata={(id) => this.showLayerMetadata(id)}
-                  />
-                </SwipeToDelete>
-              )
-              : (
+  return !results.length
+    ? renderNoResults()
+    : (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadMoreItems}
+        hasMore={hasMoreItems}
+        useWindow={false}
+        getScrollParent={scrollParentRef.current}
+      >
+        <div className="product-outer-list-case layers-all">
+          {visibleItems.map((layer) => (isMobile && recentLayerMode
+            ? (
+              <SwipeToDelete
+                key={layer.id}
+                item={layer}
+                deleteSwipe={0.33}
+                onDelete={() => clearSingleRecentLayer(layer)}
+              >
                 <SearchLayerRow
-                  key={layer.id}
                   layer={layer}
-                  showLayerMetadata={(id) => this.showLayerMetadata(id)}
+                  showLayerMetadata={(id) => showLayerMetadata(id)}
                 />
-              )))}
-          </div>
-        </InfiniteScroll>
-      );
-  }
+              </SwipeToDelete>
+            )
+            : (
+              <SearchLayerRow
+                key={layer.id}
+                layer={layer}
+                showLayerMetadata={(id) => showLayerMetadata(id)}
+              />
+            )))}
+        </div>
+      </InfiniteScroll>
+    );
 }
 
 SearchLayerList.propTypes = {
